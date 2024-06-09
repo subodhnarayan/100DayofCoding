b70b8ee6924867a6996416d873a81bf6ec0b64a9
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-06-09 23:28:14 +0530

    Create day40.cpp

---
+++ day40.cpp
@@ -0,0 +1,53 @@
+// Convert array into Zig-Zag fashion
+// EasyAccuracy: 58.28%Submissions: 138K+Points: 2
+// Solve today's problem-> Be the comment of the day-> Win a GfG T-Shirt!
+// Daily rewards up for grabs!
+
+// banner
+// Given an array arr of distinct elements of size n, the task is to rearrange the elements of the array in a zig-zag fashion so that the converted array should be in the below form:
+
+// arr[0] < arr[1]  > arr[2] < arr[3] > arr[4] < . . . . arr[n-2] < arr[n-1] > arr[n].
+
+// Note: Modify the given arr[] only, If your transformation is correct, the output will be 1 else the output will be 0.
+
+// Examples
+
+// Input: n = 7, arr[] = {4, 3, 7, 8, 6, 2, 1}
+// Output: 1
+// Explanation:  After modification the array will look like 3 < 7 > 4 < 8 > 2 < 6 > 1, the checker in the driver code will produce 1.
+// Input: n = 5, arr[] = {4, 7, 3, 8, 2}
+// Output: 1
+// Explanation: After modification the array will look like 4 < 7 > 3 < 8 > 2 hence output will be 1.
+// Expected Time Complexity: O(n)
+// Expected Auxiliary Space: O(1)
+
+// Constraints:
+// 1 <= n <= 106
+// 0 <= arri <= 109
+
+class Solution
+{
+public:
+    void zigZag(int n, vector<int> &arr)
+    {
+        bool first = true;
+        for (int i = 0; i < arr.size() - 1; i++)
+        {
+            if (first)
+            {
+                if (arr[i] > arr[i + 1])
+                {
+                    swap(arr[i], arr[i + 1]);
+                }
+            }
+            else
+            {
+                if (arr[i] < arr[i + 1])
+                {
+                    swap(arr[i], arr[i + 1]);
+                }
+            }
+            first = !first;
+        }
+    }
+};
