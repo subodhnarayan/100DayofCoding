c3b54db3b4ef1f651d05c1b30ec14bc175c03be3
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-09 23:51:52 +0530

    Update day66.cpp

--- day66.cpp
+++ day66.cpp
@@ -112,3 +112,23 @@ public:
 // 1 <= customers.length <= 105
 // 1 <= arrivali, timei <= 104
 // arrivali <= arrivali+1
+
+class Solution
+{
+public:
+    double averageWaitingTime(vector<vector<int>> &customers)
+    {
+        int N = customers.size();
+        double serviceTime = customers[0][0] + customers[0][1];
+        double totalWait = serviceTime - customers[0][0];
+
+        for (int i = 1; i < N; i++)
+        {
+            if (serviceTime < customers[i][0])
+                serviceTime = customers[i][0];
+            serviceTime += customers[i][1];
+            totalWait += (serviceTime - customers[i][0]);
+        }
+        return totalWait / N;
+    }
+};
