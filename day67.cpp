269cd40ebe11f7c9f6548a085cd838deacb4e25c
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-11 00:07:22 +0530

    Update day67.cpp

--- day67.cpp
+++ day67.cpp
@@ -65,3 +65,71 @@ int maxSquare(int n, int m, vector<vector<int>> mat)
     solve(mat, 0, 0, dp, ans);
     return ans;
 }
+
+Letcode POTD 11/7/2024
+
+1598. Crawler Log Folder
+Solved
+Easy
+Topics
+Companies
+Hint
+The Leetcode file system keeps a log each time some user performs a change folder operation.
+
+The operations are described below:
+
+"../" : Move to the parent folder of the current folder. (If you are already in the main folder, remain in the same folder).
+"./" : Remain in the same folder.
+"x/" : Move to the child folder named x (This folder is guaranteed to always exist).
+You are given a list of strings logs where logs[i] is the operation performed by the user at the ith step.
+
+The file system starts in the main folder, then the operations in logs are performed.
+
+Return the minimum number of operations needed to go back to the main folder after the change folder operations.
+
+Example 1:
+
+Input: logs = ["d1/","d2/","../","d21/","./"]
+Output: 2
+Explanation: Use this change folder operation "../" 2 times and go back to the main folder.
+Example 2:
+
+Input: logs = ["d1/","d2/","./","d3/","../","d31/"]
+Output: 3
+Example 3:
+
+Input: logs = ["d1/","../","../","../"]
+Output: 0
+
+Constraints:
+
+1 <= logs.length <= 103
+2 <= logs[i].length <= 10
+logs[i] contains lowercase English letters, digits, '.', and '/'.
+logs[i] follows the format described in the statement.
+Folder names consist of lowercase English letters and digits.
+
+int minOperations(vector<string> &logs)
+{
+    int count = 0;
+    int n = logs.size();
+    for (int i = 0; i < n; i++)
+    {
+        if (logs[i] == "../")
+        {
+            if (count > 0)
+                count--;
+        }
+        else if (logs[i] == "./")
+        {
+            count = count;
+        }
+        else
+        {
+            count++;
+        }
+    }
+    return count;
+}
+}
+;
