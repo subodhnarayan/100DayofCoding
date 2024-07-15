94ce0f5ad4b8b7091f35991d104ba58297b5c05d
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-15 22:44:30 +0530

    gfgcode

--- day72.cpp
+++ day72.cpp
@@ -1,20 +1,43 @@
-//GFG POTD 15/7/2024
+// GFG POTD 15/7/2024
 Smallest number
 
-// Difficulty: MediumAccuracy: 38.74%Submissions: 55K+Points: 4
-// Given two integers s and d. The task is to find the smallest number such that the sum of its digits is s and the number of digits in the number are d. Return a string that is the smallest possible number. If it is not possible then return -1.
+    // Difficulty: MediumAccuracy: 38.74%Submissions: 55K+Points: 4
+    // Given two integers s and d. The task is to find the smallest number such that the sum of its digits is s and the number of digits in the number are d. Return a string that is the smallest possible number. If it is not possible then return -1.
 
-// Examples:
+    // Examples:
 
-// Input: s = 9, d = 2
-// Output: 18 
-// Explanation: 18 is the smallest number possible with the sum of digits = 9 and total digits = 2.
-// Input: s = 20, d = 3 
-// Output: 299 
-// Explanation: 299 is the smallest number possible with the sum of digits = 20 and total digits = 3.
-// Expected Time Complexity: O(d)
-// Expected Auxiliary Space: O(1)
+    // Input: s = 9, d = 2
+    // Output: 18
+    // Explanation: 18 is the smallest number possible with the sum of digits = 9 and total digits = 2.
+    // Input: s = 20, d = 3
+    // Output: 299
+    // Explanation: 299 is the smallest number possible with the sum of digits = 20 and total digits = 3.
+    // Expected Time Complexity: O(d)
+    // Expected Auxiliary Space: O(1)
 
-// Constraints:
-// 1 ≤ s ≤ 100
-// 1 ≤ d ≤ 6
+    // Constraints:
+    // 1 ≤ s ≤ 100
+    // 1 ≤ d ≤ 6
+
+    class Solution
+{
+public:
+    string smallestNumber(int s, int d)
+    {
+        string ans = "";
+        int i = 1;
+        while (i <= d)
+        {
+            int num = (s - (d - i) * 9) <= 0 ? 0 : s - (d - i) * 9;
+            if (num > 9)
+                return "-1";
+            if (num == 0 && i == 1)
+                num = 1;
+
+            ans += (num + '0');
+            i++;
+            s -= num;
+        }
+        return ans;
+    }
+};
