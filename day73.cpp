9408281b9692108de714774de63f8b776edb8e1e
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-16 23:51:27 +0530

    Update day73.cpp

--- day73.cpp
+++ day73.cpp
@@ -1,8 +1,8 @@
-//GFG POTD 16/7/2024
-// Remaining String
-// Difficulty: EasyAccuracy: 17.06%Submissions: 59K+Points: 2
-// Given a string s without spaces, a character ch and an integer count. Your task is to return the substring that remains after the character ch has appeared count number of times.
-// Note:  Assume upper case and lower case alphabets are different. “”(Empty string) should be returned if it is not possible, or the remaining substring is empty.
+// GFG POTD 16/7/2024
+//  Remaining String
+//  Difficulty: EasyAccuracy: 17.06%Submissions: 59K+Points: 2
+//  Given a string s without spaces, a character ch and an integer count. Your task is to return the substring that remains after the character ch has appeared count number of times.
+//  Note:  Assume upper case and lower case alphabets are different. “”(Empty string) should be returned if it is not possible, or the remaining substring is empty.
 
 // Examples:
 
@@ -19,4 +19,25 @@
 // Expected Time Complexity: O(|s|)
 // Expected Auxiliary Space: O(1)
 
-
+class Solution
+{
+public:
+    string printString(string s, char ch, int count)
+    {
+        string ans = "";
+        int n = s.size();
+        int i = n - 1;
+        for (int i = 0; i < n; i++)
+        {
+            if (s[i] == ch)
+            {
+                count--;
+            }
+            if (count == 0)
+            {
+                return s.substr(i + 1);
+            }
+        }
+        return "";
+    }
+};
