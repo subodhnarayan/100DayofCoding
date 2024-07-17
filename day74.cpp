8e42303b82721959fcb8b5945006a25913705ecd
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-17 23:25:07 +0530

    Update day74.cpp

--- day74.cpp
+++ day74.cpp
@@ -103,3 +103,27 @@ public:
 // to_delete.length <= 1000
 // to_delete contains distinct values between 1 and 1000.
 
+class Solution
+{
+public:
+    bool set[1001] = {};
+    void dfs(TreeNode *&root, bool flag, vector<TreeNode *> &res)
+    {
+        if (root == nullptr)
+            return;
+        dfs(root->left, set[root->val], res);
+        dfs(root->right, set[root->val], res);
+        if (!set[root->val] && flag)
+            res.push_back(root);
+        if (set[root->val])
+            root = nullptr;
+    }
+    vector<TreeNode *> delNodes(TreeNode *root, vector<int> &to_delete)
+    {
+        vector<TreeNode *> res;
+        for (int num : to_delete)
+            set[num] = true;
+        dfs(root, true, res);
+        return res;
+    }
+};
