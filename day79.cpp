fc96c1c5e6c1c143289c500783a5656a4990faa5
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-22 23:54:05 +0530

    Update day79.cpp

--- day79.cpp
+++ day79.cpp
@@ -61,39 +61,58 @@ class Solution{
     }
 };
 
+// Leetcode POTD 22/07/2024
+// 2418. Sort the People
+// Solved
+// Easy
+// Topics
+// Companies
+// Hint
+// You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
 
-2418. Sort the People
-Solved
-Easy
-Topics
-Companies
-Hint
-You are given an array of strings names, and an array heights that consists of distinct positive integers. Both arrays are of length n.
+// For each index i, names[i] and heights[i] denote the name and height of the ith person.
 
-For each index i, names[i] and heights[i] denote the name and height of the ith person.
-
-Return names sorted in descending order by the people's heights.
+// Return names sorted in descending order by the people's heights.
 
  
 
-Example 1:
+// Example 1:
 
-Input: names = ["Mary","John","Emma"], heights = [180,165,170]
-Output: ["Mary","Emma","John"]
-Explanation: Mary is the tallest, followed by Emma and John.
-Example 2:
+// Input: names = ["Mary","John","Emma"], heights = [180,165,170]
+// Output: ["Mary","Emma","John"]
+// Explanation: Mary is the tallest, followed by Emma and John.
+// Example 2:
 
-Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
-Output: ["Bob","Alice","Bob"]
-Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
+// Input: names = ["Alice","Bob","Bob"], heights = [155,185,150]
+// Output: ["Bob","Alice","Bob"]
+// Explanation: The first Bob is the tallest, followed by Alice and the second Bob.
  
 
-Constraints:
+// Constraints:
 
-n == names.length == heights.length
-1 <= n <= 103
-1 <= names[i].length <= 20
-1 <= heights[i] <= 105
-names[i] consists of lower and upper case English letters.
-All the values of heights are distinct.
+// n == names.length == heights.length
+// 1 <= n <= 103
+// 1 <= names[i].length <= 20
+// 1 <= heights[i] <= 105
+// names[i] consists of lower and upper case English letters.
+// All the values of heights are distinct.
 
+class Solution {
+public:
+    vector<string> sortPeople(vector<string>& names, vector<int>& heights) {
+       int n = names.size();
+        unordered_map<int, string> mapping;
+
+        for (int i = 0; i < n; ++i) {
+            mapping[heights[i]] = names[i];
+        }
+
+        sort(heights.rbegin(), heights.rend());
+
+        for (int i = 0; i < n; ++i) {
+            names[i] = mapping[heights[i]];
+        }
+
+        return names;
+    }
+};
