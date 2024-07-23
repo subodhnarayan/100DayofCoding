54c5bd3740d53738e4316ccfee4efde6a159d5f8
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-23 23:58:31 +0530

    push2

--- day80.cpp
+++ day80.cpp
@@ -1,5 +1,4 @@
-//GFG PoTD 23/7/2024
-
+// GFG PoTD 23/7/2024
 
 // Given two BSTs, return elements of merged BSTs in sorted form.
 
@@ -11,7 +10,7 @@
 //      /   \
 //     3     6
 //    / \
-//   2   4  
+//   2   4
 // BST2:
 //         2
 //       /   \
@@ -25,7 +24,7 @@
 // Input:
 // BST1:
 //        12
-//      /   
+//      /
 //     9
 //    / \    
 //   6   11
@@ -43,3 +42,60 @@
 // Constraints:
 // 1 ≤ Number of Nodes ≤ 105
 
+class Solution
+{
+public:
+    // Function to return a list of integers denoting the node
+    // values of both the BST in a sorted order.
+    void inorder(Node *root, vector<int> &ans)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        inorder(root->left, ans);
+        ans.push_back(root->data);
+        inorder(root->right, ans);
+    }
+
+    vector<int> merge(Node *root1, Node *root2)
+    {
+        // what we can do is traverse the tree and parallely we can compare if we find small
+        // er then we will puss that in ans vector and make next pointer
+        vector<int> ans1;
+        vector<int> ans2;
+        inorder(root1, ans1);
+        inorder(root2, ans2);
+
+        int n1 = ans1.size();
+        int n2 = ans2.size();
+        int i = 0, j = 0;
+
+        vector<int> ans;
+        while (i < n1 && j < n2)
+        {
+            if (ans1[i] >= ans2[j])
+            {
+                ans.push_back(ans2[j]);
+                j++;
+            }
+            else
+            {
+                ans.push_back(ans1[i]);
+                i++;
+            }
+        }
+
+        while (i < n1)
+        {
+            ans.push_back(ans1[i]);
+            i++;
+        }
+        while (j < n2)
+        {
+            ans.push_back(ans2[j]);
+            j++;
+        }
+        return ans;
+    }
+};
