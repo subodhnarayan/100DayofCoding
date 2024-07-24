e16aaceb0ab4094c20f356029a7fe73f5a230203
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-24 23:37:38 +0530

    Update day81.cpp

--- day81.cpp
+++ day81.cpp
@@ -46,4 +46,32 @@
 // 1 ≤ Number of nodes ≤ 105
 // 1 ≤ Data of a node ≤ 105
 
-
+class Solution
+{
+public:
+    // Function to check whether a Binary Tree is BST or not.
+    void inorder(Node *root, vector<int> &ans)
+    {
+        if (root == NULL)
+        {
+            return;
+        }
+        inorder(root->left, ans);
+        ans.push_back(root->data);
+        inorder(root->right, ans);
+    }
+    bool isBST(Node *root)
+    {
+        vector<int> ans;
+        inorder(root, ans);
+        int n = ans.size();
+        for (int i = 1; i < n; i++)
+        {
+            if (ans[i - 1] == ans[i] || ans[i - 1] > ans[i])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+};
