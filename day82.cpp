e2fcfbf44a0b8fc34f7036b845c751569f7138cf
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-25 23:38:02 +0530

    Update day82.cpp

--- day82.cpp
+++ day82.cpp
@@ -32,3 +32,24 @@ Expected Auxillary Space: O(n)
 Constraints:
 1 ≤ nums.size() ≤ 105
 1 ≤ nums[i] ≤ 105
+
+
+class Solution {
+  public:
+  Node* help(vector<int> & nums,int start,int end){
+      if(start>end){
+          return NULL;
+      }
+      int mid=(start+end)/2;
+      Node* root=new Node(nums[mid]);
+      root->left=help(nums,start,mid-1);
+      root->right=help(nums,mid+1,end);
+      return root;
+  }
+    Node* sortedArrayToBST(vector<int>& nums) {
+        int start=0;
+        int end=nums.size()-1;
+        return help(nums,start,end);
+    }
+};
+
