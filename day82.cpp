6a840bf5062a2a99b8dc1659cad803df45d57620
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-07-25 23:39:16 +0530

    Update day82.cpp

--- day82.cpp
+++ day82.cpp
@@ -80,3 +80,67 @@ Constraints:
 
 1 <= nums.length <= 5 * 104
 -5 * 104 <= nums[i] <= 5 * 104
+
+
+class Solution {
+public:
+    void merge(vector<int> &nums,int start,int mid,int end){
+        vector<int> temp;
+        int left=start;
+        int right=mid+1;
+        while(left<=mid && right<=end){
+            if(nums[left]<=nums[right]){
+                temp.push_back(nums[left]);
+                left++;
+            }
+            else{
+                temp.push_back(nums[right]);
+                right++;
+            }
+        }
+
+        while(left<=mid){
+              temp.push_back(nums[left]);
+            left++;
+        }
+
+        while(right<=end){
+            temp.push_back(nums[right]);
+                right++;
+        }
+
+        for(int i=start;i<=end;i++){
+            nums[i]=temp[i-start];
+        }
+    }
+    void Mergesort(vector<int> &nums,int start,int end){
+        if(start>=end){
+            return;
+        }
+        int mid=(start+end)/2;
+        Mergesort(nums,start,mid);  //including mid
+        Mergesort(nums,mid+1,end);   //excluded mid
+        merge(nums,start,mid,end);
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        // sort(nums.begin(),nums.end());
+        // int end=nums.size()-1;
+        // Mergesort(nums,0,end);
+        // return nums;
+         vector<int> counting(2 * 50000 + 1, 0);
+        for (int num : nums) {
+            // we add 5 * 10^4 because for smallest possible element -5 * 10^4 index must be 0
+            counting[num + 50000]++;
+        }
+        int write_ind = 0;
+        for (int number_ind = 0; number_ind < counting.size(); ++number_ind) {
+            int freq = counting[number_ind];
+            while (freq != 0) {
+                nums[write_ind] = number_ind - 50000;
+                write_ind++;
+                freq--;
+            }
+        }
+        return nums;  tc=0(n) nd sc=0(1)
+    }
+};
