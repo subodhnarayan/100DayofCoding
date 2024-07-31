8fb8507662cd46cf608793174e0e3ecfc0f9e096
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-01 00:01:53 +0530

    Update day90.cpp

--- day90.cpp
+++ day90.cpp
@@ -1,1 +1,22 @@
-dfgdfg
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -GFg - POTD - 30 / 07 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+Longest Common Prefix of Strings
+Difficulty: EasyAccuracy: 29.52%Submissions: 279K+Points: 2
+Given an array of strings arr. Return the longest common prefix among all strings present in the array. If there's no prefix common in all the strings, return "-1".
+
+Examples :
+
+Input: arr[] = ["geeksforgeeks", "geeks", "geek", "geezer"]
+Output: gee
+Explanation: "gee" is the longest common prefix in all the given strings.
+Input: arr[] = ["hello", "world"]
+Output: -1
+Explanation: There's no common prefix in the given strings.
+Expected Time Complexity: O(n*min(|arri|))
+Expected Space Complexity: O(min(|arri|))
+
+Constraints:
+1 ≤ |arr| ≤ 103
+1 ≤ |arr[i]| ≤ 103
+
+
+
