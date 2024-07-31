1d2cb1d9d4bcbe07f786e3444fd9894fecb4bb13
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-01 00:04:40 +0530

    Update day90.cpp

--- day90.cpp
+++ day90.cpp
@@ -80,3 +80,28 @@ Constraints:
 1 <= books.length <= 1000
 1 <= thicknessi <= shelfWidth <= 1000
 1 <= heighti <= 1000
+
+
+class Solution {
+public:
+    int minHeightShelves(vector<vector<int>>& books, int shelfWidth) {
+        int n = books.size();
+        vector<int> dp(n + 1, INT_MAX);
+        dp[0] = 0;  // Base case: no books require 0 height
+        
+        for (int i = 1; i <= n; ++i) {
+            int total_width = 0;
+            int max_height = 0;
+            for (int j = i; j > 0; --j) {
+                total_width += books[j-1][0];
+                if (total_width > shelfWidth) {
+                    break;
+                }
+                max_height = std::max(max_height, books[j-1][1]);
+                dp[i] = std::min(dp[i], dp[j-1] + max_height);
+            }
+        }
+        
+        return dp[n];
+    }
+};
