04ffdd1c15cbf683a3cd417c49eff55bbc69f0d4
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-01 23:37:00 +0530

    Update day91.cpp

--- day91.cpp
+++ day91.cpp
@@ -25,3 +25,52 @@ Constraints:
 1 <= matrix.size(), matrix[0].size() <= 100
 0 <= matrix[i][j]<= 100
 
+
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
+         vector<int> ans;
+    if (matrix.empty() || matrix[0].empty()) {
+        return ans;
+    }
+
+    int top = 0;
+    int bottom = matrix.size() - 1;
+    int left = 0;
+    int right = matrix[0].size() - 1;
+
+    while (left <= right && top <= bottom) {
+        // Traverse from left to right along the top row
+        for (int i = left; i <= right; ++i) {
+            ans.push_back(matrix[top][i]);
+        }
+        top++;
+
+        // Traverse from top to bottom along the right column
+        if (top <= bottom) {
+            for (int i = top; i <= bottom; ++i) {
+                ans.push_back(matrix[i][right]);
+            }
+            right--;
+        }
+
+        // Traverse from right to left along the bottom row
+        if (left <= right && top <= bottom) {
+            for (int i = right; i >= left; --i) {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+        }
+
+        // Traverse from bottom to top along the left column
+        if (left <= right && top <= bottom) {
+            for (int i = bottom; i >= top; --i) {
+                ans.push_back(matrix[i][left]);
+            }
+            left++;
+        }
+    }
+
+    return ans;
+    }
+};
