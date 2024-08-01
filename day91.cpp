61d9ea50d336f1563d0091c338016567ad8e918c
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-01 23:38:54 +0530

    Update day91.cpp

--- day91.cpp
+++ day91.cpp
@@ -1,4 +1,4 @@
- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -GFg - POTD - 30 / 07 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+ -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -GFg - POTD - 1 / 08 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
  Spirally traversing a matrix
 Difficulty: MediumAccuracy: 35.2%Submissions: 269K+Points: 4
@@ -72,5 +72,60 @@ class Solution {
     }
 
     return ans;
+    }
+};
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -Leetcode - POTD - 1 / 08 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+2678. Number of Senior Citizens
+Solved
+Easy
+Topics
+Companies
+Hint
+You are given a 0-indexed array of strings details. Each element of details provides information about a given passenger compressed into a string of length 15. The system is such that:
+
+The first ten characters consist of the phone number of passengers.
+The next character denotes the gender of the person.
+The following two characters are used to indicate the age of the person.
+The last two characters determine the seat allotted to that person.
+Return the number of passengers who are strictly more than 60 years old.
+
+ 
+
+Example 1:
+
+Input: details = ["7868190130M7522","5303914400F9211","9273338290F4010"]
+Output: 2
+Explanation: The passengers at indices 0, 1, and 2 have ages 75, 92, and 40. Thus, there are 2 people who are over 60 years old.
+Example 2:
+
+Input: details = ["1313579440F2036","2921522980M5644"]
+Output: 0
+Explanation: None of the passengers are older than 60.
+ 
+
+Constraints:
+
+1 <= details.length <= 100
+details[i].length == 15
+details[i] consists of digits from '0' to '9'.
+details[i][10] is either 'M' or 'F' or 'O'.
+The phone numbers and seat numbers of the passengers are distinct.
+
+class Solution {
+public:
+    int countSeniors(vector<string>& details) {
+       int n=details.size();
+       int count=0;
+       for(int i=0;i<n;i++){
+        string str=details[i];
+        int age=(str[11]-'0')*10+(str[12]-'0');
+        if((age)>60){
+            count++;
+        }
+        
+       }
+       return count;
     }
 };
