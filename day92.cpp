d652051af70e8594647cb050c8558e1ea6e6cb27
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-02 22:19:03 +0530

    Update day92.cpp

--- day92.cpp
+++ day92.cpp
@@ -21,3 +21,116 @@ Expected Space Complexity: O(|str1|*|str2|)
 Constraints:
 1 ≤ str1.length(), str2.length() ≤ 100
 Both the strings are in lowercase.
+
+class Solution {
+  public:
+  int solve(string &str1,string &str2,int i,int j,vector<vector<int>>&dp){
+      if(i==str1.length()&&j!=str2.length()){
+          return str2.length()-j;
+      }
+      
+      if(j==str2.length()&&i!=str1.length()){
+          return str1.length()-i;
+      }
+      
+      if(i==str1.length()&&j==str2.length()){
+          return 0;
+      }
+      if(dp[i][j]!=-1){
+          return dp[i][j];
+      }
+      
+      int ans=0;
+      if(str1[i]==str2[j]){
+          return solve(str1,str2,i+1,j+1,dp);
+      }else{
+          // insert
+          int insert=solve(str1,str2,i,j+1,dp)+1;
+          // remove
+          int remove=solve(str1,str2,i+1,j,dp)+1;
+         // replace
+         int replace=solve(str1,str2,i+1,j+1,dp)+1;
+         
+         dp[i][j]=min(insert,min(remove,replace));
+      }
+      return dp[i][j];
+      
+      
+  }
+    int editDistance(string str1, string str2) {
+        // Code here
+         int n=str1.length();
+        int m=str2.length();
+        vector<vector<int>>dp(n,vector<int>(m,-1));
+        int ans=solve(str1,str2,0,0,dp);
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int T;
+    cin >> T;
+    while (T--) {
+        string s, t;
+        cin >> s >> t;
+        Solution ob;
+        int ans = ob.editDistance(s, t);
+        cout << ans << "\n";
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -leetcode- POTD - 2 / 08 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+Code
+Testcase
+Testcase
+Test Result
+2134. Minimum Swaps to Group All 1's Together II
+Solved
+Medium
+Topics
+Companies
+Hint
+A swap is defined as taking two distinct positions in an array and swapping the values in them.
+
+A circular array is defined as an array where we consider the first element and the last element to be adjacent.
+
+Given a binary circular array nums, return the minimum number of swaps required to group all 1's present in the array together at any location.
+
+ 
+
+Example 1:
+
+Input: nums = [0,1,0,1,1,0,0]
+Output: 1
+Explanation: Here are a few of the ways to group all the 1's together:
+[0,0,1,1,1,0,0] using 1 swap.
+[0,1,1,1,0,0,0] using 1 swap.
+[1,1,0,0,0,0,1] using 2 swaps (using the circular property of the array).
+There is no way to group all 1's together with 0 swaps.
+Thus, the minimum number of swaps required is 1.
+Example 2:
+
+Input: nums = [0,1,1,1,0,0,1,1,0]
+Output: 2
+Explanation: Here are a few of the ways to group all the 1's together:
+[1,1,1,0,0,0,0,1,1] using 2 swaps (using the circular property of the array).
+[1,1,1,1,1,0,0,0,0] using 2 swaps.
+There is no way to group all 1's together with 0 or 1 swaps.
+Thus, the minimum number of swaps required is 2.
+Example 3:
+
+Input: nums = [1,1,0,0,1]
+Output: 0
+Explanation: All the 1's are already grouped together due to the circular property of the array.
+Thus, the minimum number of swaps required is 0.
+ 
+
+Constraints:
+
+1 <= nums.length <= 105
+nums[i] is either 0 or 1.
