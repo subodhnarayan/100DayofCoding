f040874503238dc4e36877da1cce5b79a5fe61ab
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-02 22:20:15 +0530

    Update day92.cpp

--- day92.cpp
+++ day92.cpp
@@ -134,3 +134,37 @@ Constraints:
 
 1 <= nums.length <= 105
 nums[i] is either 0 or 1.
+
+class Solution {
+public:
+    int minSwaps(vector<int>& nums) {
+        int n = nums.size();
+        int totalOnes = 0;
+        
+        // Count total number of 1's and create a doubled array
+        vector<int> doubledNums(2 * n);
+        for (int i = 0; i < n; i++) {
+            totalOnes += nums[i];
+            doubledNums[i] = doubledNums[i + n] = nums[i];
+        }
+        
+        // Edge cases
+        if (totalOnes == 0 || totalOnes == n) return 0;
+        
+        // Create cumulative sum array
+        vector<int> cumulativeSum(2 * n + 1, 0);
+        for (int i = 0; i < 2 * n; i++) {
+            cumulativeSum[i + 1] = cumulativeSum[i] + doubledNums[i];
+        }
+        
+        int maxOnesInWindow = 0;
+        
+        // Check all possible windows of size totalOnes
+        for (int i = 0; i <= n; i++) {
+            int onesInWindow = cumulativeSum[i + totalOnes] - cumulativeSum[i];
+            maxOnesInWindow = max(maxOnesInWindow, onesInWindow);
+        }
+        
+        return totalOnes - maxOnesInWindow;
+    }
+};
