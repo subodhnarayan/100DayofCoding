bb4475b3319771d4dccddcf40eb46d01eb5f970c
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-05 23:25:22 +0530

    Update day95.cpp

--- day95.cpp
+++ day95.cpp
@@ -69,7 +69,7 @@ class Solution {
     }
 };
 
--- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -GFg - POTD - 6 / 08 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+-- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -Leetcode - POTD - 6 / 08 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 2053. Kth Distinct String in an Array
 Solved
@@ -114,3 +114,32 @@ Constraints:
 1 <= arr[i].length <= 5
 arr[i] consists of lowercase English letters.
 
+
+class Solution {
+public:
+    string kthDistinct(vector<string>& arr, int k) {
+       unordered_map<string, bool> map;
+        int count = 0;
+
+        // Count occurrences of each string
+        for (const string& item : arr) {
+            if (map.find(item) != map.end()) {
+                map[item] = false;  // Mark as not distinct
+            } else {
+                map[item] = true;   // Mark as distinct
+            }
+        }
+
+        // Find the k-th distinct string
+        for (const string& item : arr) {
+            if (map[item]) {  // Check if it's distinct
+                count++;
+                if (count == k) {
+                    return item;
+                }
+            }
+        }
+
+        return "";  // Return empty string if not found 
+    }
+};
