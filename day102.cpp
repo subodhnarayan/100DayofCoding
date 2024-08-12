996796fda39a6ba1597e405d49bd7d7b37254291
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-12 23:39:30 +0530

    Update day102.cpp

--- day102.cpp
+++ day102.cpp
@@ -19,3 +19,15 @@ Constraints:
 1 <= arr1[i] <= 106
 1 <= arr2[i] <= 106
 
+class Solution {
+  public:
+    int SumofMiddleElements(vector<int> &arr1, vector<int> &arr2) {
+        int n1=arr1.size();
+        int n2=arr2.size();
+          for(int i=0; i<arr2.size(); i++){
+            arr1.push_back(arr2[i]);
+        }
+        sort(arr1.begin(), arr1.end());
+        return arr1[arr1.size()/2] + arr1[(arr1.size()/2)-1];
+    }
+};
