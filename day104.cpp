8ff6f5cc01af0000697649860a897ae5ecd23cee
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-14 23:54:36 +0530

    Update day104.cpp

--- day104.cpp
+++ day104.cpp
@@ -19,3 +19,25 @@ Constraints:
 1<= str1.size(), str2.size()<=1000
 Both strings may contain upper and lower case alphabets.
 
+class Solution {
+  public:
+    int longestCommonSubstr(string str1, string str2) {
+    int n=str1.length(), m=str2.length();
+        
+        vector<vector<int>>dp(n+1, vector<int>(m+1, 0));
+        
+        int maxi=0;
+        
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                if(str1[i]==str2[j]){
+                    dp[i+1][j+1]=1+dp[i][j];
+                    maxi=max(maxi, dp[i+1][j+1]);
+                }
+            }
+        }
+        return maxi;
+
+    }
+};
+
