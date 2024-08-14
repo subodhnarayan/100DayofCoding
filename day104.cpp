d4b30b20f8735fc96c3b458103438ebeacddbac4
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-14 23:52:48 +0530

    Create day104.cpp

---
+++ day104.cpp
@@ -0,0 +1,18 @@
+Longest Common Substring
+Difficulty: MediumAccuracy: 42.69%Submissions: 232K+Points: 4
+You are given two strings str1 and str2. Your task is to find the length of the longest common substring among the given strings.
+
+Examples:
+
+Input: str1 = "ABCDGH", str2 = "ACDGHR"
+Output: 4
+Explanation: The longest common substring is "CDGH" which has length 4.
+Input: str1 = "ABC", str2 = "ACB"
+Output: 1
+Explanation: The longest common substrings are "A", "B", "C" all having length 1.
+Expected Time Complexity: O(n*m).
+Expected Auxiliary Space: O(n*m).
+
+Constraints:
+1<= str1.size(), str2.size()<=1000
+Both strings may contain upper and lower case alphabets.
