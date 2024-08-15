2328a8e1e1047e200b3da53455f3cfb410f15076
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-15 14:35:23 +0530

    Update day105.cpp

--- day105.cpp
+++ day105.cpp
@@ -80,7 +80,7 @@ class Solution {
 
 
 
-    860. Lemonade Change
+860. Lemonade Change
 Easy
 Topics
 Companies
@@ -118,3 +118,34 @@ Constraints:
 bills[i] is either 5, 10, or 20.
 
 
+
+class Solution {
+public:
+    bool lemonadeChange(vector<int>& bills) {
+         int five_dollars = 0, ten_dollars = 0;
+        
+        for (int x : bills) {
+            if (x == 5) {
+                five_dollars++;
+            } else if (x == 10) {
+                if (five_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars++;
+                } else {
+                    return false;
+                }
+            } else {
+                if (five_dollars > 0 && ten_dollars > 0) {
+                    five_dollars--;
+                    ten_dollars--;
+                } else if (five_dollars > 2) {
+                    five_dollars -= 3;
+                } else {
+                    return false;
+                }
+            }
+        }
+        
+        return true;
+    }
+};
