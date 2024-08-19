04dc420aa169755968a6ee883efc68f897ff06c4
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-19 21:19:36 +0530

    day109

---
+++ dat109.cpp
@@ -0,0 +1,164 @@
+- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -GFG - POTD - 19 / 08 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+
+Kth Smallest
+Difficulty: MediumAccuracy: 35.17%Submissions: 623K+Points: 4
+Given an array arr[] and an integer k where k is smaller than the size of the array, the task is to find the kth smallest element in the given array. It is given that all array elements are distinct.
+
+Follow up: Don't solve it using the inbuilt sort function.
+
+Examples :
+
+Input: arr[] = [7, 10, 4, 3, 20, 15], k = 3
+Output:  7
+Explanation: 3rd smallest element in the given array is 7.
+Input: arr[] = [7, 10, 4, 20, 15], k = 4 
+Output:  15
+Explanation: 4th smallest element in the given array is 15.
+Expected Time Complexity: O(n+(max_element) )
+Expected Auxiliary Space: O(max_element)
+Constraints:
+1 <= arr.size <= 106
+1<= arr[i] <= 106
+1 <= k <= n
+
+
+
+
+
+
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function template for C++
+
+class Solution {
+  public:
+    // arr : given array
+    // k : find kth smallest element and return using this function
+    int partition(vector<int>& arr, int low, int high) {
+        int pivot = arr[high];
+        int i = low;
+        for (int j = low; j < high; j++) {
+            if (arr[j] <= pivot) {
+                swap(arr[i], arr[j]);
+                i++;
+            }
+        }
+        swap(arr[i], arr[high]);
+        return i;
+    }
+
+    int quickSelect(vector<int>& arr, int low, int high, int k) {
+        if (low <= high) {
+            int pivotIndex = partition(arr, low, high);
+            if (pivotIndex == k) {
+                return arr[pivotIndex];
+            } else if (pivotIndex > k) {
+                return quickSelect(arr, low, pivotIndex - 1, k);
+            } else {
+                return quickSelect(arr, pivotIndex + 1, high, k);
+            }
+        }
+        return -1;
+    }
+
+    int kthSmallest(vector<int> &arr, int k) {
+        int n = arr.size();
+        return quickSelect(arr, 0, n - 1, k - 1);
+        // code here
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int k;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        k = crr[0];
+        int n = arr.size();
+        Solution ob;
+        cout << ob.kthSmallest(arr, k) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+
+
+
+
+
+650. 2 Keys Keyboard
+Solved
+Medium
+Topics
+Companies
+Hint
+There is only one character 'A' on the screen of a notepad. You can perform one of two operations on this notepad for each step:
+
+Copy All: You can copy all the characters present on the screen (a partial copy is not allowed).
+Paste: You can paste the characters which are copied last time.
+Given an integer n, return the minimum number of operations to get the character 'A' exactly n times on the screen.
+
+ 
+
+Example 1:
+
+Input: n = 3
+Output: 3
+Explanation: Initially, we have one character 'A'.
+In step 1, we use Copy All operation.
+In step 2, we use Paste operation to get 'AA'.
+In step 3, we use Paste operation to get 'AAA'.
+Example 2:
+
+Input: n = 1
+Output: 0
+ 
+
+Constraints:
+
+1 <= n <= 1000
+
+
+
+class Solution {
+public:
+    int minSteps(int n) {
+        int result = 0;
+        for (int d = 2; d * d <= n; ++d) {
+            while (n % d == 0) {
+                result += d;
+                n /= d;
+            }
+        }
+        if (n > 1) {
+            result += n;
+        }
+        return result;
+    }
+};
