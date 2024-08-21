6cfff9b4ee49478d5edbf45d3cd5a9387d63bb5e
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-21 23:52:51 +0530

    Update day110.cpp

--- day110.cpp
+++ day110.cpp
@@ -27,3 +27,42 @@ Constraint:
 Expected Time Complexity: O(N + E), where N is the number of nodes and E is the edges
 Expected Space Complexity: O(N)
 
+class Solution {
+  public:
+    vector<int> shortestPath(vector<vector<int>>& edges, int N,int M, int src){
+     vector<int> ans(N, -1);
+        vector< vector<pair<int, int>> > adj(N);
+        
+        for(int i=0; i<M; i++){
+            int u = edges[i][0];
+            int v = edges[i][1];
+            adj[u].push_back({v, 1});
+            adj[v].push_back({u, 1});
+        }
+  
+        vector<int> vis(N, 0);
+        vis[src]=1;
+        ans[src]=0;
+        queue<pair<int, int>> q;
+        q.push({src, 0});
+        
+        while(!q.empty()){
+            
+            int node = q.front().first;
+            int len = q.front().second;
+            q.pop();
+            
+            for(auto it : adj[node]){
+                if(!vis[it.first]){
+                    vis[it.first] = 1;
+                    ans[it.first] = len+1;
+                    q.push({it.first, len+1});
+                }
+                else{
+                    ans[it.first] = min(ans[it.first] , len+1);
+                }
+            }
+        }
+        return ans; 
+    }
+};
