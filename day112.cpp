62deee5b4167ca8ffaa74438717fcc3716b042ac
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-23 23:48:32 +0530

    Update day112.cpp

--- day112.cpp
+++ day112.cpp
@@ -84,3 +84,45 @@ Each fraction (input and output) has the format ±numerator/denominator. If the 
 The input only contains valid irreducible fractions, where the numerator and denominator of each fraction will always be in the range [1, 10]. If the denominator is 1, it means this fraction is actually an integer in a fraction format defined above.
 The number of given fractions will be in the range [1, 10].
 The numerator and denominator of the final result are guaranteed to be valid and in the range of 32-bit int.
+
+
+
+class Solution {
+public:
+    string fractionAddition(string expression) {
+         int numerator = 0, denominator = 1;
+        int i = 0, n = expression.size();
+
+        while (i < n) {
+            int sign = 1;
+            if (expression[i] == '+' || expression[i] == '-') {
+                if (expression[i] == '-') sign = -1;
+                i++;
+            }
+
+            int num = 0;
+            while (i < n && isdigit(expression[i])) {
+                num = num * 10 + (expression[i] - '0');
+                i++;
+            }
+            num *= sign;
+
+            i++;
+
+            int den = 0;
+            while (i < n && isdigit(expression[i])) {
+                den = den * 10 + (expression[i] - '0');
+                i++;
+            }
+
+            numerator = numerator * den + num * denominator;
+            denominator *= den;
+
+            int gcdVal = gcd(abs(numerator), denominator);
+            numerator /= gcdVal;
+            denominator /= gcdVal;
+        }
+
+        return to_string(numerator) + "/" + to_string(denominator);
+    }
+};
