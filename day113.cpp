0059161a405748f75408d1f544450f6cfbb9035b
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-24 23:51:33 +0530

    Update day113.cpp

--- day113.cpp
+++ day113.cpp
@@ -19,3 +19,22 @@ Constraints:
 1 ≤ W ≤ 1000
 1 ≤ wt[i] ≤ 1000
 1 ≤ val[i] ≤ 1000
+
+
+class Solution {
+  public:
+    // Function to return max value that can be put in knapsack of capacity W.
+      int rec(int i, int W, vector<int>& wt, vector<int>& val, vector<vector<int>>& mem) {
+        if(i>=val.size()) return 0;
+        if(mem[i][W]!=-1) return mem[i][W];
+        int take=0;
+        if(W>=wt[i])
+            take=val[i]+rec(i+1,W-wt[i],wt,val,mem);
+        int not_take=rec(i+1,W,wt,val,mem);
+        return mem[i][W]=max(take,not_take);
+    }
+    int knapSack(int W, vector<int>& wt, vector<int>& val) {
+       vector<vector<int>> mem(val.size()+1, vector<int>(W+1,-1));
+        return rec(0,W,wt,val,mem);
+    }
+};
