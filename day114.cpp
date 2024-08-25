153361e4e391a74e664c7444da4631f74d318c00
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-25 23:54:24 +0530

    Update day114.cpp

--- day114.cpp
+++ day114.cpp
@@ -17,3 +17,69 @@ Expected Auxiliary Space: O(1).
 Constraints:
 1 ≤ arr.size(), brr.size() ≤ 105
 1 ≤ brr[i], arr[i] ≤ 103
+
+
+
+class Solution {
+  public:
+    int binarysearch(vector<int> brr,int key){
+      int low=0;
+      int high=brr.size()-1;
+      int idx=-1;
+      while(low<=high){
+          int mid=(low+high)/2;
+          if(brr[mid]<=key){
+              idx=mid;
+              low=mid+1;
+          }
+          else{
+          high=mid-1;
+      }
+      }
+      return idx;
+    }
+    long long countPairs(vector<int> &arr, vector<int> &brr) {
+    //   int n=arr.size();
+    //   int  m=brr.size();
+    //   long long count=0;
+    //   for(int i=0;i<n;i++){
+    //       for(int j=0;j<m;j++){
+    //           if(pow(arr[i],brr[j])>pow(brr[j],arr[i]))
+    //           {
+    //               count++;
+    //           }
+    //       }
+    //   }
+    //   return count;  brute force o(n2)
+    
+    
+       long long ans=0;
+        long long one=0;
+        long long two=0;
+        long long threefour=0;
+        sort(brr.begin(),brr.end());
+        
+        for(auto x:brr){
+            if(x==1) one++;
+            else if(x==2) two++;
+            else if(x==3 || x==4) threefour++;
+        }
+        
+        for(auto x:arr){
+            if(x!=1) {
+                ans=ans+one;
+            
+            if(x==2){
+                ans=ans-threefour;
+            }
+            if(x==3){
+                ans=ans+two;
+            }
+            int index=binarysearch(brr,x);
+            ans=ans+(brr.size()-index-1);
+        }
+        
+     }
+        return ans;
+    }
+};
