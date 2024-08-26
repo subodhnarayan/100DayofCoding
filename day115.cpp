3dc1f1bd24c85c909fd1ee8eb45add43be0612c3
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-26 23:46:25 +0530

    Update day115.cpp

--- day115.cpp
+++ day115.cpp
@@ -86,3 +86,21 @@ The height of the n-ary tree is less than or equal to 1000.
  
 
 Follow up: Recursive solution is trivial, could you do it iteratively?
+
+
+
+class Solution {
+public:
+   vector<int> ans;
+   void postOrder(Node *root){
+     if(root==NULL) return;
+     for(Node *child:root->children){
+        postOrder(child);
+     }
+     ans.push_back(root->val);
+   }
+    vector<int> postorder(Node* root) {
+       postOrder(root);
+       return ans;
+    }
+};
