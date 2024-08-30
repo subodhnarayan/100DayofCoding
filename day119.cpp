9b2e1936c4bffcbe9781c9b9e520906bf8de07af
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-08-30 23:30:43 +0530

    Update day119.cpp

--- day119.cpp
+++ day119.cpp
@@ -21,3 +21,71 @@ Expected Auxiliary Space: O(n2)
 Constraints:
 1 ≤ n ≤ 10
 
+
+
+class Solution{
+public:
+
+    bool issafe(int row,int col,vector<vector<int>> &board,int n){
+        int crow=row;
+        int ccol=col;
+        
+        while(row>=0 && col>=0){
+            if(board[row][col]==1) return false;
+            row--;
+            col--;
+        }
+        
+        row=crow;
+        col=ccol;
+        while(col>=0){
+            if(board[row][col]==1) return false;
+            col--;
+        }
+        
+    
+        row=crow;
+        col=ccol;
+        
+         while(row<n && col>=0){
+            if(board[row][col]==1) return false;
+            row++;
+            col--;
+        }
+        
+        return true;
+        
+    }
+    
+    void soln(int col,vector<vector<int>> &board, vector<vector<int>> &ans,int n){
+        if(col==n){
+            vector<int> res;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(board[i][j]==1){
+                        res.push_back(j+1);
+                    }
+                }
+            }
+            ans.push_back(res);
+            return;
+        }
+        
+        for(int row=0;row<n;row++){
+         if(issafe(row,col,board,n)){
+             board[row][col]=1;
+             soln(col+1,board,ans,n);
+             board[row][col]=0;
+         }
+            
+        }
+    }
+    vector<vector<int>> nQueen(int n) {
+        vector<vector<int>> ans;
+        vector<std::vector<int>> board(n, std::vector<int>(n, 0));
+        soln(0,board,ans,n);
+        sort(ans.begin(),ans.end());
+        return ans;
+       // getsoln(0,chesbod,ans,0);
+    }
+};
