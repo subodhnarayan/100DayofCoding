27f26846afec0ab5d0c4c1b23500a68ca8a51910
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-01 23:41:16 +0530

    Update day121.cpp

--- day121.cpp
+++ day121.cpp
@@ -1,45 +1,23 @@
 - -- -- -- -- -- -- -- -- -- -- -- -- -- -GFG - POTD - 1 / 09 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
-2022. Convert 1D Array Into 2D Array
-Solved
-Easy
-Topics
-Companies
-Hint
-You are given a 0-indexed 1-dimensional (1D) integer array original, and two integers, m and n. You are tasked with creating a 2-dimensional (2D) array with  m rows and n columns using all the elements from original.
+Max sum path in two arrays
+Difficulty: MediumAccuracy: 30.9%Submissions: 68K+Points: 4
+Given two sorted arrays of distinct integers arr1 and arr2. Each array may have some elements in common with the other array. Find the maximum sum of a path from the beginning of any array to the end of any array. You can switch from one array to another array only at the common elements.
 
-The elements from indices 0 to n - 1 (inclusive) of original should form the first row of the constructed 2D array, the elements from indices n to 2 * n - 1 (inclusive) should form the second row of the constructed 2D array, and so on.
+Note:  When we switch from one array to other,  we need to consider the common element only once in the result.
 
-Return an m x n 2D array constructed according to the above procedure, or an empty 2D array if it is impossible.
+Examples : 
 
- 
-
-Example 1:
-
-
-Input: original = [1,2,3,4], m = 2, n = 2
-Output: [[1,2],[3,4]]
-Explanation: The constructed 2D array should contain 2 rows and 2 columns.
-The first group of n=2 elements in original, [1,2], becomes the first row in the constructed 2D array.
-The second group of n=2 elements in original, [3,4], becomes the second row in the constructed 2D array.
-Example 2:
-
-Input: original = [1,2,3], m = 1, n = 3
-Output: [[1,2,3]]
-Explanation: The constructed 2D array should contain 1 row and 3 columns.
-Put all three elements in original into the first row of the constructed 2D array.
-Example 3:
-
-Input: original = [1,2], m = 1, n = 1
-Output: []
-Explanation: There are 2 elements in original.
-It is impossible to fit 2 elements in a 1x1 2D array, so return an empty 2D array.
- 
-
+Input: arr1 = [2, 3, 7, 10, 12] , arr2 = [1, 5, 7, 8]
+Output: 35
+Explanation: The path will be 1+5+7+10+12 = 35, where 1 and 5 come from arr2 and then 7 is common so we switch to arr1 and add 10 and 12.
+Input: arr1 = [1, 2, 3] , arr2[] = [3, 4, 5]
+Output: 15
+Explanation: The path will be 1+2+3+4+5=15.
+Expected Time Complexity: O(m + n)
+Expected Auxiliary Space: O(1)
 Constraints:
-
-1 <= original.length <= 5 * 104
-1 <= original[i] <= 105
-1 <= m, n <= 4 * 104
+1 <= arr1.size(), arr2.size() <= 104
+1 <= arr1[i], arr2[i] <= 105
 
 
 
@@ -65,6 +43,7 @@ class Solution {
         return ans;
     }
 };
+
 
 
 
