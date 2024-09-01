1a4fe5790ec921ae84be6b9ff002a07f9a0efaaa
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-01 23:36:24 +0530

    Update day121.cpp

--- day121.cpp
+++ day121.cpp
@@ -41,3 +41,28 @@ Constraints:
 1 <= original[i] <= 105
 1 <= m, n <= 4 * 104
 
+
+
+
+class Solution {
+  public:
+    int maxPathSum(vector<int> &arr1, vector<int> &arr2) {
+         int i=0, j=0,sum1=0,sum2=0,ans =0;
+        while(i<arr1.size() && j<arr2.size()){
+            if(arr1[i]<arr2[j])sum1+=arr1[i++];
+            else if(arr2[j]<arr1[i])sum2+=arr2[j++];
+            else if(arr1[i]==arr2[j]){
+                ans+=max(sum1,sum2)+arr1[i];
+                i++;
+                j++;
+                sum1=0;
+                sum2=0;
+            }
+        }
+        while(i<arr1.size())sum1+=arr1[i++];
+        while(j<arr2.size())sum2+=arr2[j++];
+        ans+=max(sum1,sum2);
+        return ans;
+    }
+};
+
