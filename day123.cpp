537bbf8f04c25a5a0765aa8ed2493dabc3925685
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-03 23:19:06 +0530

    Update day123.cpp

--- day123.cpp
+++ day123.cpp
@@ -19,3 +19,27 @@ Expected Space Complexity: O(|str1|*|str2|)
 Constraints:
 1 ≤ |str1|, |str2| ≤ 1000
 All the characters are lowercase English alphabets
+
+
+class Solution{
+		
+
+	public:
+	int minOperations(string str1, string str2) 
+	{ 
+	    int m = str1.length(), n = str2.length();
+    vector<vector<int>> c(m + 1, vector<int>(n + 1, 0));
+
+    for (int i = 1; i <= m; i++) {
+        for (int j = 1; j <= n; j++) {
+            if (str1[i - 1] == str2[j - 1])
+                c[i][j] = c[i - 1][j - 1] + 1;
+            else
+                c[i][j] = max(c[i - 1][j], c[i][j - 1]);
+        }
+    }
+    
+    int lcsL = c[m][n];
+    return (n - lcsL) + (m - lcsL);
+	} 
+};
