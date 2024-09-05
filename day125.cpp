9a0bc8c5478d822c15fddfe34ced55beb3c86725
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-05 23:59:47 +0530

    Update day125.cpp

--- day125.cpp
+++ day125.cpp
@@ -23,6 +23,7 @@ Constraints:
 
 
 
+
 2028. Find Missing Observations
 Solved
 Medium
@@ -63,3 +64,27 @@ Constraints:
 m == rolls.length
 1 <= n, m <= 105
 1 <= rolls[i], mean <= 6
+class Solution {
+public:
+    vector<int> missingRolls(vector<int>& rolls, int mean, int n) {
+       int nNumsSum = 0;
+        for(int i : rolls) nNumsSum += i;
+
+        int m=rolls.size();
+        int totalLength = n+m;
+
+        int mNumsSum = mean*totalLength - nNumsSum;     // formula to Find mean -> (nNumsSum + X) / (n+m) = mean
+                                                        // So, X = mean * (n+m) - nNumsSum;
+
+        if(n*6 < mNumsSum || mNumsSum<n) return {};     // for (n*6 < mNumsSum) see Constraints
+        
+        int rem = mNumsSum % n;
+        int q   = mNumsSum / n;
+
+        vector<int> res(n,q);
+        for(int i=1; i<=rem; i++)
+            res[i]++;
+
+        return res; 
+    }
+};
