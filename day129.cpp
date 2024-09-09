026f0cc1f0ce009abaa089f4da8bde7fb50ea875
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-09 23:18:13 +0530

    Update day129.cpp

--- day129.cpp
+++ day129.cpp
@@ -18,4 +18,30 @@ Constraints:
 1 <= arr.size() <= 106
 0 <= arr[i] <= 2
 
-
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        int zc=0,oc=0,tc=0;
+        for(int i=0;i<arr.size();i++){
+            if(arr[i]==0){
+                zc++;
+            }
+            else if(arr[i]==1){
+                oc++;
+            }
+            else{
+                tc++;
+            }
+        }
+        
+        for(int i=0;i<zc;i++){
+            arr[i]=0;
+        }
+        for(int i=zc;i<zc+oc;i++){
+            arr[i]=1;
+        }
+        for(int i=zc+oc;i<zc+oc+tc;i++){
+            arr[i]=2;
+        }
+    }
+};
