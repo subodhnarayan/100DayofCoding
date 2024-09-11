e2fac1c9d75af754d6bba43f6acb83387f8e7732
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-11 23:54:56 +0530

    Update day131.cpp

--- day131.cpp
+++ day131.cpp
@@ -96,3 +96,21 @@ It can be shown we cannot convert 3 to 4 in less than 3 steps. Hence, we return 
 Constraints:
 
 0 <= start, goal <= 109
+
+
+
+class Solution {
+public:
+    int minBitFlips(int start, int goal) {
+        int xorResult = start ^ goal;
+        int ans = 0;
+        
+        
+        while (xorResult > 0) {
+            ans += xorResult & 1; 
+            xorResult >>= 1;
+        }
+        
+        return ans;
+    }
+};
