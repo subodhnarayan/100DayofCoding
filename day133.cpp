237ebf09a9a77257e63c4c34a7477f70992f4d74
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-13 23:34:44 +0530

    Update day133.cpp

--- day133.cpp
+++ day133.cpp
@@ -39,3 +39,19 @@ Constraints:
 1 ≤ Data of a node ≤ 105
 
 
+
+class Solution {
+  public:
+    // Function to convert a binary tree into its mirror tree.
+    void mirror(Node* node) {
+        // code here
+        if(node==NULL) return ;
+        Node *temp=node->right;
+        node->right=node->left;
+        node->left=temp;
+        mirror(node->left);
+        mirror(node->right);
+    }
+};
+
+
