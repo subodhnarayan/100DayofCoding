ff2a400b2044dd0508c293f578cb38ad7172c77e
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-19 23:50:41 +0530

    Update day139.cpp

--- day139.cpp
+++ day139.cpp
@@ -84,3 +84,28 @@ Constraints:
 expression consists of digits and the operator '+', '-', and '*'.
 All the integer values in the input expression are in the range [0, 99].
 The integer values in the input expression do not have a leading '-' or '+' denoting the sign.
+
+
+
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string expression) {
+         vector<int> res;
+        for (int i = 0; i < expression.size(); ++i) {
+            char oper = expression[i];
+            if (oper == '+' || oper == '-' || oper == '*') {
+                vector<int> s1 = diffWaysToCompute(expression.substr(0, i));
+                vector<int> s2 = diffWaysToCompute(expression.substr(i + 1));
+                for (int a : s1) {
+                    for (int b : s2) {
+                        if (oper == '+') res.push_back(a + b);
+                        else if (oper == '-') res.push_back(a - b);
+                        else if (oper == '*') res.push_back(a * b);
+                    }
+                }
+            }
+        }
+        if (res.empty()) res.push_back(stoi(expression));
+        return res;
+    }
+};
