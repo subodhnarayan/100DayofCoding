57a8391a070712f12748fb704df8e9a4855aad48
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-22 23:11:04 +0530

    Update day142.cpp

--- day142.cpp
+++ day142.cpp
@@ -20,3 +20,33 @@ Constraints:
 1 ≤ |str| ≤ 106
 str contains lower case English alphabets
 
+
+class Solution {
+  public:
+    int lps(string str) {
+        // Your code goes here
+           int p=0;
+        int s=1;
+        int cnt=0;
+        int pos=1;
+        int n=str.size();
+        while(p<n && s<n)
+        {
+            if(str[p]==str[s])
+            {
+                cnt++;
+                p++;
+                s++;
+            }
+            else
+            {
+                cnt=0;
+                pos++;
+                s=pos;
+                p=0;
+                
+            }
+        }
+        return cnt;
+    }
+};
