596924d438a0f342ae3c8af48e97a468500440d2
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-24 23:45:24 +0530

    Update day144.cpp

--- day144.cpp
+++ day144.cpp
@@ -72,11 +72,7 @@ class Solution
 
 
 
-
-Code
-Testcase
-Test Result
-Test Result
+-- -- -- -- -- -- -- -- -- -- -- -- -- -Leetcode - POTD - 24 / 09 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
 3043. Find the Length of the Longest Common Prefix
 Medium
 Topics
@@ -117,35 +113,3 @@ Constraints:
 1 <= arr1[i], arr2[i] <= 108
 
 
-class Solution {
-public:
-    int longestCommonPrefix(vector<int>& arr1, vector<int>& arr2) {
-          unordered_map<string, int> prefixMap;
-        
-        // Step 1: Build the prefix map for arr1
-        for (int num : arr1) {
-            string strNum = to_string(num);
-            string prefix = "";
-            for (char ch : strNum) {
-                prefix += ch;
-                prefixMap[prefix]++;
-            }
-        }
-        
-        int maxLength = 0;
-        
-        // Step 2: Check for common prefixes in arr2
-        for (int num : arr2) {
-            string strNum = to_string(num);
-            string prefix = "";
-            for (char ch : strNum) {
-                prefix += ch;
-                if (prefixMap.find(prefix) != prefixMap.end()) {
-                    maxLength = max(maxLength, static_cast<int>(prefix.length()));
-                }
-            }
-        }
-        
-        return maxLength;
-    }
-};
