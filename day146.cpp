e0b99ba89a4b6f137b244c48daa0e465a410c828
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-09-26 23:51:05 +0530

    Update day146.cpp

--- day146.cpp
+++ day146.cpp
@@ -78,3 +78,24 @@ Constraints:
 
 0 <= start < end <= 109
 At most 1000 calls will be made to book.
+
+
+class MyCalendar {
+    map<int, int> intervals;
+public:
+    MyCalendar() {
+        
+    }
+    
+    bool book(int start, int end) {
+        auto next = intervals.lower_bound(start); // Find next interval
+        if (next != intervals.end() && next->first < end) {
+            return false; // Overlaps with next interval
+        }
+        if (next != intervals.begin() && prev(next)->second > start) {
+            return false; // Overlaps with previous interval
+        }
+        intervals[start] = end; // No overlap, add interval
+        return true;
+    }
+};
