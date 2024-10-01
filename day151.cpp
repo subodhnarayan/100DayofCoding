cdf6f3e600bd5d441effcdbe0d083d9f5a78fd22
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-01 23:18:02 +0530

    Update day151.cpp

--- day151.cpp
+++ day151.cpp
@@ -134,3 +134,30 @@ arr.length == n
 n is even.
 -109 <= arr[i] <= 109
 1 <= k <= 105
+
+
+class Solution {
+public:
+    bool canArrange(vector<int>& arr, int k) {
+         vector<int> freq(k, 0);
+        for (int num : arr) {
+            int rem = num % k;
+            if (rem < 0) {
+                rem += k;
+            }
+            freq[rem]++;
+        }
+        
+        if (freq[0] % 2 != 0) {
+            return false;
+        }
+        
+        for (int i = 1; i <= k / 2; i++) {
+            if (freq[i] != freq[k - i]) {
+                return false;
+            }
+        }
+        
+        return true;
+    }
+};
