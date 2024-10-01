06bdd43011304982233842bf0ae1e2720f8ec2a5
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-01 23:17:16 +0530

    Update day151.cpp

--- day151.cpp
+++ day151.cpp
@@ -24,3 +24,72 @@ where n is the size of L1 and m is the size of L2
 Constraints:
 1 <= number of nodes <= 9
 0 <= node->data <= 9
+
+
+class solution {
+  public:
+//   string mult(string a,string b){
+//       string ans="";
+//       int carr=0;
+//       int as=a.size()-1;
+//       int bs=b.size()-1;
+//       while(as>=0 && bs>=0){
+//           int mul=((a[as]-'0')*(b[bs]-'0'))+carr;
+//               carr=mul/10;
+//               ans=to_string(mul%10)+ans;
+//               as--,bs--;
+//       }
+//       while(as>=0){
+//           int mul=(a[as]-'0')+carr;
+//               carr=mul/10;
+//               ans=to_string(mul%10)+ans;
+//               as--;
+//           }
+
+//       }
+//       while(bs>=0){
+//           int mul=(b[bs]-'0')+carr;
+//               carr=mul/10;
+//               ans=to_string(mul%10)+ans;
+//               bs--;
+          
+//       }
+//       if(carr>0){
+//           ans=to_string(carr)+ans;
+//       }
+//       return ans;
+//   }
+    long long multiplyTwoLists(Node *first, Node *second) {
+    //     long long fir = 0;
+    // long long sec = 0;
+    // int mod = 10000000;  // Use an integer, not a floating-point number
+
+    // // Build the number from the first list
+    // while (first) {
+    //     fir = ((fir * 10) % mod + first->data) % mod;
+    //     first = first->next;
+    // }
+
+    // // Build the number from the second list
+    // while (second) {
+    //     sec = ((sec * 10) % mod + second->data) % mod;
+    //     second = second->next;
+    // }
+
+    // // Return the product modulo `mod`
+    // return (fir * sec) % mod;
+    string p1="";
+        string p2="";
+        while(first!=NULL){
+            p1.push_back(first->data+'0');
+            first=first->next;
+        }
+         while(second!=NULL){
+            p2.push_back(second->data+'0');
+            second=second->next;
+        }
+        long long n1=stoll(p1);
+        long long n2=stoll(p2);
+        return (n1*n2)%1000000007;
+    }
+};
