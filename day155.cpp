9199ac3712eff9de3237b3e5fddca6d5003b3255
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-05 23:50:53 +0530

    Update day155.cpp

--- day155.cpp
+++ day155.cpp
@@ -27,3 +27,17 @@ Expected Auxiliary Space: O(1)
 Constraints
 1 <= arr.size() <= 106
 1 <= arr[i] <= 108
+
+
+class Solution {
+  public:
+    long long findSmallest(vector<int> &arr) {
+        // Your code goes here.
+         int ans=1;
+        for(int x:arr){
+            if(x>ans) return ans;
+            ans += x;
+        }
+        return ans;
+    }
+};
