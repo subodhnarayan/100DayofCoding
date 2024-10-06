cc8ef945ca1ec589c8a3f8d1d677765857aedfda
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-06 23:58:34 +0530

    Update day156.cpp

--- day156.cpp
+++ day156.cpp
@@ -113,3 +113,63 @@ Constraints:
 1 <= sentence1.length, sentence2.length <= 100
 sentence1 and sentence2 consist of lowercase and uppercase English letters and spaces.
 The words in sentence1 and sentence2 are separated by a single space.
+
+
+
+class Solution {
+public:
+   vector<string> convert(string sentence){
+        sentence += " ";
+        vector<string> ans;
+
+        string word = "";
+        for(int i=0;i<sentence.size();i++){
+            if(sentence[i]==' '){
+                ans.push_back(word);
+                word="";
+            }
+            else{
+                word += sentence[i];
+            }
+        }
+
+        return ans;
+    }
+    bool areSentencesSimilar(string x, string y) {
+           if (x.length() < y.length()) {
+            swap(x, y);
+        }
+
+        // first store the words from sentences
+        vector<string> vx, vy;
+        vx = convert(x);
+        vy = convert(y);
+
+        int l = 0;
+        // we will try to match words from prefix part
+        for(int i = 0; i < vy.size(); i++) {
+            if(vx[i] == vy[i]) {
+                l++;
+            }
+            else {
+                break;
+            }
+        }
+
+        int ind = vx.size() - 1, r=vy.size();
+        // we will try to match words from suffix part
+        for(int i=vy.size()-1; i>=0; i--){
+            if(vy[i] == vx[ind]){
+                ind--;
+                r=i;
+            }
+            else{
+                break;
+            }
+        }
+
+        // if they overlaps
+        return r <= l;
+    }
+};
+
