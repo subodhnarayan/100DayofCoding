5569ef27f3278972d3c79457bb6370978ec2a2be
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-07 23:57:17 +0530

    Update day157.cpp

--- day157.cpp
+++ day157.cpp
@@ -28,3 +28,45 @@ Expected Auxiliary Space: O(1)
 
 Constraints:
 1 <= number of nodes, data of nodes <= 105
+
+
+
+
+/*
+Structure of linked list is as
+struct Node
+{
+    int data;
+    struct Node* npx;
+
+    Node(int x){
+        data = x;
+        npx = NULL;
+    }
+};
+
+Utility function to get XOR of two Struct Node pointer
+use this function to get XOR of two pointers
+struct Node* XOR (struct Node *a, struct Node *b)
+{
+    return (struct Node*) ((uintptr_t) (a) ^ (uintptr_t) (b));
+}
+*/
+
+// function should insert the data to the front of the list
+struct Node *insert(struct Node *head, int data) {
+    // Code here
+       Node *temp=new Node(data);
+    temp->npx=head;
+    return temp;
+}
+
+vector<int> getList(struct Node *head) {
+    // Code here
+       vector<int>ans;
+    Node *temp1=head;
+    while(temp1!=NULL){
+    ans.push_back(temp1->data);
+    temp1=temp1->npx;}
+    return ans;
+}
