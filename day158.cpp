83c622c1fe1c84cdcec569c705680da54d58bfd5
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-08 23:54:07 +0530

    Update day158.cpp

--- day158.cpp
+++ day158.cpp
@@ -98,3 +98,20 @@ n == s.length
 n is even.
 s[i] is either '[' or ']'.
 The number of opening brackets '[' equals n / 2, and the number of closing brackets ']' equals n / 2.
+
+
+class Solution {
+public:
+    int minSwaps(string s) {
+        int size = 0;
+        int n = s.size();
+        for (int i = 0; i < n; i++) {
+            char ch = s[i];
+            if (ch == '[')
+                size++;
+            else if (size > 0) 
+                size--;
+        }
+        return (size + 1) / 2;
+    }
+};
