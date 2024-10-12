780d006c9de8195c82a49a085954ecbc7d9b6d3e
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-12 23:43:41 +0530

    Update day162.cpp

--- day162.cpp
+++ day162.cpp
@@ -93,3 +93,31 @@ Constraints:
 1 <= intervals.length <= 105
 intervals[i].length == 2
 1 <= lefti <= righti <= 106
+
+
+class Solution {
+public:
+    int minGroups(vector<vector<int>>& intervals) {
+        vector<int> start_t,end_t;
+         for (const auto& interval : intervals) {
+            start_t.push_back(interval[0]);
+            end_t.push_back(interval[1]);
+        }
+        sort(start_t.begin(),start_t.end());
+        sort(end_t.begin(),end_t.end());
+
+        int  ptr=0,group=0;
+
+        for(int start:start_t){
+            if(start>end_t[ptr]){
+                ptr++;
+            }
+            else{
+                group++;
+            }
+        }
+
+        return group;
+
+    }
+};
