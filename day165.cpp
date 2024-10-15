908f57f8abb0019bc242ca665a9523ec34767acc
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-15 22:38:38 +0530

    Update day165.cpp

--- day165.cpp
+++ day165.cpp
@@ -18,3 +18,64 @@ Constraints:
 1 <= arr.size() <= 106
 -105 <= arr[i] <= 105
 -105 <= tar <= 105
+
+
+class Solution {
+  public:
+    // Function to count the number of subarrays which adds to the given sum.
+    int subArraySum(vector<int>& arr, int tar) {
+        // int n=arr.size();
+        // int count=0;
+        // for(int i=0;i<n;i++){
+        //         int sum=0;
+        //         for(int k=i;k<n;k++){
+        //             sum+=arr[k];
+        //             if(sum==tar){
+        //                 count++;
+        //                 break;
+        //             }
+        //         }
+        // }
+        // return count;  Tc=0(n2)
+        
+        int n=arr.size();
+        unordered_map<int,int> mp;
+        int sum=0,ans=0;
+        for(int x:arr){
+            sum+=x;
+            if(sum==tar) ans++;
+            if(mp.find(sum-tar)!=mp.end()){
+                ans+=mp[sum-tar];
+            }
+            mp[sum]++;
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    string ts;
+    getline(cin, ts);
+    int t = stoi(ts);
+    while (t--) {
+
+        vector<int> arr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        string ks;
+        getline(cin, ks);
+        int tar = stoi(ks);
+        Solution obj;
+        int res = obj.subArraySum(arr, tar);
+        cout << res << endl;
+    }
+    return 0;
+}
+/
