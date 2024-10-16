04d5c4020b976569ae998d704721fa24a1774531
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-16 23:56:09 +0530

    Update day166.cpp

--- day166.cpp
+++ day166.cpp
@@ -20,3 +20,29 @@ Constraints:
 1 ≤ arr[i] ≤ arr.size()
 
 
+class Solution {
+  public:
+    bool checkSorted(vector<int> &arr) {
+              bool ascending=1;
+
+        bool descending =1;
+
+        for(int i=1;i<arr.size()-1;i++){
+
+            if(arr[i]>arr[i+1]){
+
+                ascending=0;
+
+            }
+
+            if(arr[i]<arr[i+1]){
+
+                descending=0;
+
+            }
+
+        }
+
+        return ascending||descending;
+    }
+};
