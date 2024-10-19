bab45bd1e99b60a9c4030729e8d07817a57ebac0
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-19 23:56:55 +0530

    Update day169.cpp

--- day169.cpp
+++ day169.cpp
@@ -1,4 +1,21 @@
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -GFG - POTD - 19 / 10 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+
+fficulty: EasyAccuracy: 19.32%Submissions: 84K+Points: 2
+A string str is given to represent a positive number. The task is to round str to the nearest multiple of 10.  If you have two multiples equally apart from str, choose the smallest element among them.
+
+Examples:
+
+Input: str = 29 
+Output: 30
+Explanation: Close multiples are 20 and 30, and 30 is the nearest to 29. 
+Input: str = 15
+Output: 10
+Explanation: 10 and 20 are equally distant multiples from 20. The smallest of the two is 10.
+Expected Time Complexity: O(n).
+Expected Auxiliary Space: O(1).
+
+Constraints:
+
 class Solution {
   public:
     string roundToNearest(string str) {
