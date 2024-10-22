82f61eb11c8c6b5a93e4c49283e29af3840cdd29
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-22 23:52:40 +0530

    Update day172.cpp

--- day172.cpp
+++ day172.cpp
@@ -25,3 +25,21 @@ Explanation: The possible sub-array have same equal number of occurrences of x a
 Constraints: 
 1 <= arr.size() <= 106
 1 <= arr[i], x, y<=106
+
+
+class Solution {
+  public:
+    int sameOccurrence(vector<int>& arr, int x, int y) {
+        // code here
+         map<int,int>mp;
+        mp[0]=1;
+        int count=0,res=0;
+        for(auto it:arr){
+            if(it==x)count++;
+            if(it==y)count--;
+            if(mp[count])res+=mp[count];
+            mp[count]+=1;
+        }
+        return res;
+    }
+};
