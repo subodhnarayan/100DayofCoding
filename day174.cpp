f571a74e24dcfc7853142d88031ff3f125c8bf23
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-24 23:20:17 +0530

    Update day174.cpp

--- day174.cpp
+++ day174.cpp
@@ -84,3 +84,17 @@ Constraints:
 
 The number of nodes in each tree is in the range [0, 100].
 Each tree will have unique node values in the range [0, 99].
+
+class Solution {
+public:
+   bool checker(TreeNode* node1, TreeNode* node2) {
+        if (!node1 && !node2) return true;
+        if (!node1 || !node2 || node1->val != node2->val) return false;
+
+        return (checker(node1->left, node2->left) || checker(node1->left, node2->right)) &&
+               (checker(node1->right, node2->right) || checker(node1->right, node2->left));
+    }
+    bool flipEquiv(TreeNode* root1, TreeNode* root2) {
+        return checker(root1, root2);
+    }
+};
