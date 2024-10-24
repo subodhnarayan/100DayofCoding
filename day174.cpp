68a7c7351b3664f0dc557a772f2dd14d846d875e
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-24 23:19:17 +0530

    Update day174.cpp

--- day174.cpp
+++ day174.cpp
@@ -21,6 +21,28 @@ Expected Auxiliary Space: O(n)
 Constraints:
 1 ≤ arr.size() ≤ 105
 1 ≤ arr[i] ≤ 106
+
+
+
+class Solution {
+  public:
+    vector<int> modifyAndRearrangeArray(vector<int> &arr) {
+        int n = arr.size();
+        vector<int>ans(n);
+        int i = 0,j = n-1;
+        for(int k = 0;k<n;k++) {
+            if(k+1<n && arr[k]!=0 && arr[k+1]!=0 && arr[k] == arr[k+1]) {
+                ans[i++] = 2*arr[k];
+                arr[k+1] = 0;
+            }
+            else if(arr[k]!=0) {
+                ans[i++] = arr[k];
+            }
+            else ans[j--] = arr[k];
+        }
+        return ans;
+    }
+};
  
 
  -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -Leetcode - POTD - 24 / 10 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
