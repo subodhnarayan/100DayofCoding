575fe1e88784ff92509ce5458d7e25c010282110
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-29 23:30:12 +0530

    Update day179.cpp

--- day179.cpp
+++ day179.cpp
@@ -1,1 +1,19 @@
 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -GFG - POTD - 29 / 10 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- --
+Quick Sort on Linked List
+Difficulty: MediumAccuracy: 71.8%Submissions: 30K+Points: 4
+You are given a Linked List. Sort the given Linked List using quicksort. 
+
+Examples:
+
+Input: Linked list: 1->6->2
+Output: 1->2->6
+
+Explanation:
+After sorting the nodes, we have 1, 2 and 6.
+Input: Linked list: 1->9->3->8
+Output: 1->3->8->9
+
+Explanation:
+After sorting the nodes, we have 1, 3, 8 and 9. 
+Constraints:
+1<= size of linked list <= 105
