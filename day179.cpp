a52eb95aae9b69e5e0af221de27946d78aabfd05
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-10-29 23:30:50 +0530

    Update day179.cpp

--- day179.cpp
+++ day179.cpp
@@ -17,3 +17,35 @@ Explanation:
 After sorting the nodes, we have 1, 3, 8 and 9. 
 Constraints:
 1<= size of linked list <= 105
+
+
+class Solution {
+  public:
+   struct Node* Partition(struct Node* head,struct Node* last){
+       struct Node* i=head;
+       struct Node* j=head->next;
+       while(j!=last){
+           if(j->data<head->data){
+               i=i->next;
+               swap(i->data,j->data);
+           }
+           j=j->next;
+       }
+       swap(i->data,head->data);
+       return i;
+       
+   }
+   
+   void QuickSort(struct Node* head,struct Node* last){
+       if(head==last || head->next==last){
+           return;
+       }
+       struct Node* p=Partition(head,last);
+       QuickSort(head,p);
+       QuickSort(p->next,last);
+   }
+    struct Node* quickSort(struct Node* head) {
+        QuickSort(head,NULL);
+        return head;
+    }
+};
