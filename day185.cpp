71834e241885e47f462b9fb00a2db300c6c84848
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-04 23:49:43 +0530

    Update day185.cpp

--- day185.cpp
+++ day185.cpp
@@ -89,3 +89,24 @@ Constraints:
 
 1 <= word.length <= 2 * 105
 word consists only of lowercase English letters.
+
+class Solution {
+public:
+    string compressedString(string word) {
+        string comp = "";
+        int cnt = 1, n = word.size();
+        char ch = word[0];
+        for(int i=1;i<n;i++){
+            if(word[i] == ch && cnt < 9)cnt++;
+            else{
+                comp.push_back(cnt+'0');
+                comp.push_back(ch);
+                ch = word[i];
+                cnt = 1;
+            }
+        }
+        comp.push_back(cnt+'0');
+        comp.push_back(ch);
+        return comp;
+    }
+};
