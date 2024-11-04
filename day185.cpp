4b63c841e5aea4c78a8a0e6898217806abd2f255
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-04 23:47:36 +0530

    Update day185.cpp

--- day185.cpp
+++ day185.cpp
@@ -21,3 +21,27 @@ Explanation: There is no triplet with sum 0.
 Constraints:
 3 <= arr.size() <= 103
 -104 <= arr[i] <= 104
+
+
+class Solution {
+  public:
+    vector<vector<int>> findTriplets(vector<int> &nums) {
+        vector<vector<int>> res;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            unordered_map<int,unordered_set<int>> mp;
+            
+            for(int j=i+1;j<nums.size();j++)
+            mp[nums[j]].insert(j);
+            
+            for(int j=i+1;j<nums.size();j++)
+                if(mp.find(((-1) * nums[i])-nums[j])!=mp.end())
+                    for(auto it : mp[((-1) * nums[i])-nums[j]])
+                        if(it>i && it>j)
+                            res.push_back({i,j,it});
+        }
+
+        return res;
+    }
+};
