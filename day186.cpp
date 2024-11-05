2c4a9f4a5012b27868c73078f74f66087ed40c3f
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-05 22:42:17 +0530

    Update day186.cpp

--- day186.cpp
+++ day186.cpp
@@ -19,3 +19,21 @@ Output:
 Constraints:
 1 ≤ mat.size() ≤ 1000
 1 <= mat[][] <= 100
+
+
+void rotate(vector<vector<int> >& mat) {
+    // Your code goes here
+     int n = mat.size();
+    for(int i=0; i < n/2; i++)
+    {
+        for(int j=i; j < (n-1-i); j++)
+        {
+            int temp = mat[i][j];
+            mat[i][j] = mat[n-j-1][i];
+            mat[n-j-1][i] = mat[n-i-1][n-j-1];
+            mat[n-i-1][n-j-1] = mat[j][n-i-1];
+            mat[j][n-i-1] = temp;
+        }
+    }
+    return;
+}
