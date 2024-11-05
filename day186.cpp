dbcd3040a4e137417bec3a59d824fc31d62ecc1f
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-05 22:44:11 +0530

    Update day186.cpp

--- day186.cpp
+++ day186.cpp
@@ -85,3 +85,18 @@ Constraints:
 2 <= s.length <= 105
 s has an even length.
 s[i] is either '0' or '1'.
+
+
+class Solution {
+public:
+    int minChanges(string s) {
+        int n = s.length();
+        int cnt = 0;
+        for (int i = 0; i < n-1; i=i + 2) {
+            if (s[i] != s[i + 1]) {
+                cnt++;
+            }else continue;
+        }
+        return cnt;
+    }
+};
