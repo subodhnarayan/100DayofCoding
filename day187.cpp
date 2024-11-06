621381497ce764196a867c9e3ad43ab7b6cf0c83
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-06 21:23:17 +0530

    Update day187.cpp

--- day187.cpp
+++ day187.cpp
@@ -22,3 +22,29 @@ Explanation: There is 1 leaf, resulting in leaf path of 12.
 Constraints:
 1 ≤ number of nodes ≤ 31
 1 ≤ node->data ≤ 100
+
+class Solution {
+  public:
+     void treePathsSumfun(Node* root,int &sum,int temp){
+         if(root->left==NULL&&root->right==NULL){
+             temp=temp*10+root->data;
+             sum+=temp;
+            //  cout<<temp<<endl;
+             return;
+         }
+         
+         if(root->left)
+         treePathsSumfun(root->left,sum,temp*10+root->data);
+         if(root->right)
+         treePathsSumfun(root->right,sum,temp*10+root->data);
+         
+         return;
+         
+     }
+    int treePathsSum(Node *root) {
+        // code here.
+            int sum=0;
+         treePathsSumfun(root,sum,0);
+         return sum;
+    }
+};
