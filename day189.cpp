265b79ff3ea96bb616f9b4daee584f7215a68a78
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-08 23:54:15 +0530

    Update day189.cpp

--- day189.cpp
+++ day189.cpp
@@ -19,4 +19,20 @@ Explanation: No matter how many times we repeat s1, we can't get a string such t
 Constraints:
 1 ≤ |s1|, |s2| ≤ 105
 
-
+class Solution {
+  public:
+    int minRepeats(string& s1, string& s2) {
+        // code here
+          int ans = 1 , n1 = s1.size() ;string s = s1;
+        while(n1<s2.size()){
+            ans++;
+            n1+=s1.size();
+            s+=s1;
+        }
+        if(s.find(s2)!=string::npos)return ans;
+        ans++;
+        s+=s1;
+        if(s.find(s2)!=string::npos)return ans;
+        return -1;
+    }
+};
