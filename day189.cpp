c6550281e21bd2a14e0687ed045ba83fcc0acef3
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-08 23:55:58 +0530

    Update day189.cpp

--- day189.cpp
+++ day189.cpp
@@ -86,3 +86,22 @@ nums.length == n
 1 <= maximumBit <= 20
 0 <= nums[i] < 2maximumBit
 nums​​​ is sorted in ascending order.
+
+
+
+class Solution {
+public:
+    vector<int> getMaximumXor(vector<int>& nums, int maximumBit) {
+        int n = nums.size(),xorr = nums[0],maxxorr = pow(2,maximumBit)-1;
+        for(int i=1;i<n;i++)xorr ^= nums[i];
+        vector<int>ans(n);
+        for(int i=0;i<n;i++){
+            ans[i] = xorr^maxxorr;
+            xorr ^= nums[n-1-i];
+        }
+        return ans;
+    }
+};
+
+
+
