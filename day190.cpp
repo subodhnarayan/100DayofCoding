3b8666111b1d86cc53f84004265a6805314fc8b0
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-09 22:47:18 +0530

    Update day190.cpp

--- day190.cpp
+++ day190.cpp
@@ -17,3 +17,63 @@ Explanation: The minimum sum is formed by numbers 9 and 4.
 Constraints:
 1 ≤ arr.size() ≤ 106
 0 ≤ arr[i] ≤ 9
+
+
+
+
+class Solution {
+  public:
+    string minSum(vector<int> &arr) {
+        sort(arr.begin(),arr.end());
+        string num1="";
+        string num2="";
+        for(int i=0;i<arr.size();i++){
+            if(i%2==0){
+                num1+='0'+arr[i];
+                
+            }else{
+                num2+='0'+arr[i];
+                
+            }
+        }
+        
+        int n=num1.size();
+        int m=num2.size();
+        int i=n-1;
+        int j=m-1;
+        int carry=0;
+        int d1,d2;
+        string ans="";
+        int temp;
+        while(i>-1||j>-1||carry){
+            
+            if(i>-1){
+             d1=num1[i]-'0';
+            }else{
+                d1=0;
+            }
+            if(j>-1){
+            d2=num2[j]-'0';
+            }else{
+                d2=0;
+            }
+            temp=(d1+d2+carry);
+            int n=temp%10;
+            
+            ans+=n+'0';
+            carry=temp/10;
+            i--;
+            j--;
+            
+        }
+        
+        int k=0;
+        reverse(ans.begin(),ans.end());
+        for(k=0;k<ans.size();k++){
+            if(ans[k]!='0'){
+                break;
+            }
+        }
+        return ans.substr(k);
+    }
+};
