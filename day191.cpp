fc194ab52fe2bafd41dcff0539164aed2e975bbf
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-10 23:58:35 +0530

    Update day191.cpp

--- day191.cpp
+++ day191.cpp
@@ -20,3 +20,54 @@ Constraints:
 1  <=  a.size(), b.size()  <=  105
 -109  <=  a[i] , b[i]  <=  109
 
+class Solution {
+  public:
+    // a,b : the arrays
+    // Function to return a list containing the union of the two arrays.
+    vector<int> findUnion(vector<int> &a, vector<int> &b) {
+        // Your code here
+        // return vector with correct order of elements
+        vector<int> ans;
+    int i = 0, j = 0;
+    
+    while (i < a.size() && j < b.size()) {
+        if (a[i] < b[j]) {
+            if (ans.empty() || ans.back() != a[i]) {  // Avoid duplicates
+                ans.push_back(a[i]);
+            }
+            i++;
+        }
+        else if (a[i] > b[j]) {
+            if (ans.empty() || ans.back() != b[j]) {  // Avoid duplicates
+                ans.push_back(b[j]);
+            }
+            j++;
+        }
+        else { // a[i] == b[j]
+            if (ans.empty() || ans.back() != a[i]) {  // Avoid duplicates
+                ans.push_back(a[i]);
+            }
+            i++;
+            j++;
+        }
+    }
+    
+    // Add remaining elements from array a, if any
+    while (i < a.size()) {
+        if (ans.empty() || ans.back() != a[i]) {
+            ans.push_back(a[i]);
+        }
+        i++;
+    }
+
+    // Add remaining elements from array b, if any
+    while (j < b.size()) {
+        if (ans.empty() || ans.back() != b[j]) {
+            ans.push_back(b[j]);
+        }
+        j++;
+    }
+    
+    return ans;
+    }
+};
