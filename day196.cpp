e4f4a2f2188b688d884173fdf2a28cbe420b5307
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-15 21:38:40 +0530

    Update day196.cpp

--- day196.cpp
+++ day196.cpp
@@ -1,1 +1,24 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 15 / 11 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+class Solution {
+  public:
+    // Function returns the second
+    // largest elements
+    int getSecondLargest(vector<int> &arr) {
+        
+        int n=arr.size();
+        int flar=-1;
+        int slar=-1;
+        
+        for(int i=0;i<n;i++){
+            if(arr[i]>flar){
+                slar=flar;
+                flar=arr[i];
+            }
+            else if(arr[i]>slar && arr[i]!=flar){
+                slar=arr[i];
+            }
+        }
+        return slar;
+    }
+};
