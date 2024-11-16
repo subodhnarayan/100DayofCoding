f43bdd0268c173064eba8226c9d9c75fa6fb0d6a
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-16 23:42:56 +0530

    Update day197.cpp

--- day197.cpp
+++ day197.cpp
@@ -1,1 +1,20 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 16 / 11 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+class Solution {
+  public:
+    void pushZerosToEnd(vector<int>& arr) {
+        vector<int> ans;
+        int n=arr.size();
+        for(int i=0;i<n;i++){
+            if(arr[i]!=0){
+                ans.push_back(arr[i]);
+            }
+        }
+        for(int i=0;i<ans.size();i++){
+            arr[i]=ans[i];
+        }
+        for(int i=ans.size();i<arr.size();i++){
+            arr[i]=0;
+        }
+    }
+};
