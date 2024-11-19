6a18e07c3f48e3635423b8b846d787267ed4bd3d
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-19 23:26:40 +0530

    Update day200.cpp

--- day200.cpp
+++ day200.cpp
@@ -19,3 +19,30 @@ Explanation: The next permutation of the given array is {3, 4, 5, 1, 2}.
 Constraints:
 1 ≤ arr.size() ≤ 105
 1 ≤ arr[i] ≤ 105
+
+class Solution {
+  public:
+    void nextPermutation(vector<int>& arr) {
+        //next_permutation(arr.begin(), arr.end());
+         int n=arr.size();
+        if(n==1)return;
+        for(int i=n-2;i>=0;i--){
+            if(arr[i]<arr[i+1]){
+                int ele = arr[i];
+                int idx=i;
+                int k = INT_MAX;
+                for(int j=i+1;j<n;j++){
+                    if(arr[j]<k and arr[j]>arr[i]){
+                        k=arr[j];
+                        idx = j;
+                    }
+                }
+                swap(arr[i],arr[idx]);
+                sort(arr.begin()+i+1, arr.end());
+                
+                return;
+            }
+        }
+        sort(arr.begin(), arr.end());
+    }
+};
