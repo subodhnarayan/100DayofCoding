3c1e0b85d1cf84c016de35e793c928ed8c5f43fd
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-22 23:53:52 +0530

    Update day203.cpp

--- day203.cpp
+++ day203.cpp
@@ -92,3 +92,29 @@ m == matrix.length
 n == matrix[i].length
 1 <= m, n <= 300
 matrix[i][j] is either 0 or 1.
+
+
+class Solution {
+public:
+    int maxEqualRowsAfterFlips(vector<vector<int>>& matrix) {
+         unordered_map<string, int> count;
+        
+        for (const auto& row : matrix) {
+            string key;
+            bool flip = row[0] == 1;
+            
+            for (int num : row) {
+                key += flip ? ('0' + (1 - num)) : ('0' + num);
+            }
+            
+            count[key]++;
+        }
+        
+        int maxCount = 0;
+        for (const auto& pair : count) {
+            maxCount = max(maxCount, pair.second);
+        }
+        
+        return maxCount;
+    }
+};
