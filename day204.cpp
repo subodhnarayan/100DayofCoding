a5fbb9040ed1558c15f2dc277fb2584bb53cba8b
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-23 23:53:18 +0530

    Update day204.cpp

--- day204.cpp
+++ day204.cpp
@@ -118,3 +118,36 @@ m == box.length
 n == box[i].length
 1 <= m, n <= 500
 box[i][j] is either '#', '*', or '.'.
+
+
+
+class Solution {
+public:
+    vector<vector<char>> rotateTheBox(vector<vector<char>>& box) {
+        int m = box.size();
+        int n = box[0].size();
+        
+        for (auto& row : box) {
+            int dropPos = n - 1;
+            
+            for (int currPos = n - 1; currPos >= 0; currPos--) {
+                if (row[currPos] == '*') {
+                    dropPos = currPos - 1;
+                }
+                else if (row[currPos] == '#') {
+                    swap(row[dropPos], row[currPos]);
+                    dropPos--;
+                }
+            }
+        }
+        
+        vector<vector<char>> rotatedBox(n, vector<char>(m));
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                rotatedBox[j][m - 1 - i] = box[i][j];
+            }
+        }
+        
+        return rotatedBox;
+    }
+};
