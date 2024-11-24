2901f1a2750cc0bf21091014520683f48a379377
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-24 23:05:52 +0530

    Update day205.cpp

--- day205.cpp
+++ day205.cpp
@@ -1,4 +1,21 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 24 / 11 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+Kadane's Algorithm
+Difficulty: MediumAccuracy: 36.28%Submissions: 1MPoints: 4
+Given an integer array arr[]. You need to find the maximum sum of a subarray.
 
+Examples:
+
+Input: arr[] = [2, 3, -8, 7, -1, 2, 3]
+Output: 11
+Explanation: The subarray {7, -1, 2, 3} has the largest sum 11.
+Input: arr[] = [-2, -4]
+Output: -2
+Explanation: The subarray {-2} has the largest sum -2.
+Input: arr[] = [5, 4, 1, 7, 8]
+Output: 25
+Explanation: The subarray {5, 4, 1, 7, 8} has the largest sum 25.
+Constraints:
+1 ≤ arr.size() ≤ 105
+-109 ≤ arr[i] ≤ 104
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 24 / 11 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
