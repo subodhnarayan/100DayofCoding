bea250741fdc61ce03d2b12936076a1b625a89f2
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-24 23:07:08 +0530

    Update day205.cpp

--- day205.cpp
+++ day205.cpp
@@ -72,3 +72,24 @@ Constraints:
 n == matrix.length == matrix[i].length
 2 <= n <= 250
 -105 <= matrix[i][j] <= 105
+
+
+class Solution {
+public:
+    long long maxMatrixSum(vector<vector<int>>& matrix) {
+         long long sum=0;
+        int minAbs=INT_MAX;
+        bool negOdd=0;
+        for(auto& row: matrix){
+            for(int x: row){
+                minAbs=min(minAbs, abs(x));
+                if (x<0){
+                    sum-=x;
+                    negOdd=!negOdd;
+                }
+                else sum+=x;
+            }
+        }
+        return sum-2*(negOdd)*minAbs;
+    }
+};
