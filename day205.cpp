3a6ea1b578667a9ec814bcccc1461d780eedf9b1
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-24 23:06:17 +0530

    Update day205.cpp

--- day205.cpp
+++ day205.cpp
@@ -18,4 +18,19 @@ Constraints:
 1 ≤ arr.size() ≤ 105
 -109 ≤ arr[i] ≤ 104
 
+
+class Solution {
+  public:
+    // Function to find the sum of contiguous subarray with maximum sum.
+    int maxSubarraySum(vector<int> &arr) {
+        int n=arr.size();
+        int max_sum=arr[0],curr_sum=arr[0];
+        for(int i=1;i<n;i++){
+            curr_sum=max(arr[i],curr_sum+arr[i]);
+            max_sum=max(curr_sum,max_sum);
+        }
+        return max_sum;
+    }
+};
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 24 / 11 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
