d6bb8b64b142c9339d67bcb7f4e1db971a03bb5b
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-11-29 23:53:57 +0530

    Update day211.cpp

--- day211.cpp
+++ day211.cpp
@@ -21,6 +21,72 @@ Constraints:
 1 ≤s1.size(), s2.size()≤ 106
 
 
+
+class Solution {
+  public:
+   string find(string &str1,string &str2){
+      
+       int n=str1.size(),m=str2.size();
+      
+      int i=n-1,j=m-1;
+      int carry=0;
+      string st="";
+      while(i>=0&&j>=0){
+          
+          int sum=(str1[i]-'0')+(str2[j]-'0')+carry;
+          
+          if(sum==2){
+              st+="0";
+              carry=1;
+          }
+        else if(sum==3){
+              st+="1";
+              carry=1;
+          }
+         
+          else {
+              st+=to_string(sum);
+              carry=0;
+          }
+          i--;j--;
+      }
+      
+      while(i>=0){
+            int sum=(str1[i]-'0')+carry;
+          
+            if(sum==2){
+              st+="0";
+              carry=1;
+          }
+             else if(sum==3){
+              st+="1";
+              carry=1;
+          }
+          else {
+              st+=to_string(sum);
+              carry=0;
+          }
+          i--;
+      }
+      if(carry)st+=to_string(carry);
+      
+      while(st.back()=='0'){
+          st.pop_back();
+      }
+      reverse(st.begin(),st.end());
+      return st;
+      
+  }
+    string addBinary(string& s1, string& s2) {
+        
+        int n=s1.size(),m=s2.size();
+        
+        if(n>m)return find(s1,s2);
+        else return find(s2,s1);
+    }
+};
+
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 29 / 11 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 
