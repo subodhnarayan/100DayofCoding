dc3ad833dd7abb5c0f96076c7bba45a1f2300f57
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-03 23:50:15 +0530

    Update day215.cpp

--- day215.cpp
+++ day215.cpp
@@ -17,5 +17,39 @@ Explanation: Add 2 a's at front of above string to make it palindrome : "aaaacec
 Constraints:
 1 <= s.size() <= 106
 
+
+class Solution {
+  public:
+    int minChar(string& s) {
+        // Write your code here
+        string rev = s; 
+        reverse(rev.begin(), rev.end()); 
+        string complete = s + '$' + rev; 
+
+        int n = complete.length();
+        int i = 0, j = 1;
+        vector<int> helperArr(n, 0); 
+
+      
+        while (j < n) {
+            if (complete[i] == complete[j]) {
+                helperArr[j++] = ++i; 
+            } else if (i != 0) {
+                i = helperArr[i - 1]; 
+            } else {
+                j++; 
+            }
+        }
+
+        // The LPS value at the end tells us the length of the longest palindrome suffix
+        int longest_palindrome_suffix = helperArr[n - 1];
+
+        // The result is the number of characters to be added at the beginning
+        return s.length() - longest_palindrome_suffix;
+
+
+    }
+};
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 03 / 11 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
