82a897aa979ab97476e48604cd566617a980d883
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-05 23:56:49 +0530

    Update day217.cpp

--- day217.cpp
+++ day217.cpp
@@ -1,6 +1,20 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 05 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
+Sort 0s, 1s and 2s
+Difficulty: EasyAccuracy: 50.58%Submissions: 725K+Points: 2
+Given an array arr[] containing only 0s, 1s, and 2s. Sort the array in ascending order.
 
+Examples:
+
+Input: arr[] = [0, 1, 2, 0, 1, 2]
+Output: [0, 0, 1, 1, 2, 2]
+Explanation: 0s 1s and 2s are segregated into ascending order.
+Input: arr[] = [0, 1, 1, 0, 1, 2, 1, 2, 0, 0, 0, 1]
+Output: [0, 0, 0, 0, 0, 1, 1, 1, 1, 1, 2, 2]
+Explanation: 0s 1s and 2s are segregated into ascending order.
+Constraints:
+1 <= arr.size() <= 106
+0 <= arr[i] <= 2
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 05 / 11 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
