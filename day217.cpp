ccb66aa48c04db4f5097d8f6d1be50ad66d3a268
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-05 23:57:16 +0530

    Update day217.cpp

--- day217.cpp
+++ day217.cpp
@@ -16,5 +16,39 @@ Constraints:
 1 <= arr.size() <= 106
 0 <= arr[i] <= 2
 
+
+class Solution {
+  public:
+    void sort012(vector<int>& arr) {
+        // code here
+        int countZero = 0;
+        int countOne = 0;
+        int countTwo = 0;
+        
+        for(int i=0; i<arr.size(); i++) {
+            
+            if(arr[i] == 0) countZero++;
+            else if(arr[i] == 1) countOne++;
+            else countTwo++;
+        }
+        
+        for(int i=0; i<arr.size(); i++) {
+            if(countZero != 0) {
+                arr[i] = 0;
+                countZero--;
+            }
+            else if(countOne != 0) {
+                arr[i] = 1;
+                countOne--;
+            }
+            else {
+                arr[i]=2;
+                countTwo--;
+            }
+        }
+    }
+};
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 05 / 11 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
 
