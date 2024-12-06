1fdad748de2db7ae02a22ed163eb4c783589b736
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-06 23:53:25 +0530

    Update day218.cpp

--- day218.cpp
+++ day218.cpp
@@ -21,6 +21,45 @@ Constraints:
 0 ≤ citations[i] ≤ 106
 
 
+class Solution {
+  public:
+    // Function to find hIndex
+    bool check(vector<int>&arr, int t){
+        int cnt=0;
+        int n=arr.size();
+        for(int i=0; i<n; i++){
+            if(arr[i]>=t) cnt++;
+        }
+        if(cnt>=t) return 1;
+        else return 0;
+    }
+    int hIndex(vector<int>& citations) {
+        // code here
+         int maxi=-1e9, mini=1e9;
+      int  n=citations.size();
+        
+        for(int i=0; i<n; i++){
+            maxi=max(maxi, citations[i]);
+            mini=min(mini, citations[i]);
+        }
+        
+        int lo=min(0, mini), hi=max(maxi, n);
+        int ans=0;
+        
+        while(lo<=hi){
+            int mid=lo+(hi-lo)/2;
+            if(check(citations, mid)){
+                ans=mid;
+                lo=mid+1;
+            }else{
+                hi=mid-1;
+            }
+        }
+        return ans;
+    }
+};
+
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 05 / 11 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 2554. Maximum Number of Integers to Choose From a Range I
