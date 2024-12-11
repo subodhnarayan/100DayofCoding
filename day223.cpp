303301fe9110229ae3e559d45f9643b0cb18ebab
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-11 23:55:19 +0530

    Update day223.cpp

--- day223.cpp
+++ day223.cpp
@@ -26,6 +26,58 @@ Constraints:
 0 <= a[i], b[i] <= 107
 
 
+class Solution {
+  public:
+    void mergeArrays(vector<int>& a, vector<int>& b) {
+        
+        //approach using extra space
+    //   vector<int> ans;
+    //   int m=a.size();
+    //   int n=b.size();
+    //   for(int i=0;i<a.size();i++){
+    //       ans.push_back(a[i]);
+    //   }
+    //   for(int i=0;i<n;i++){
+    //       ans.push_back(b[i]);
+    //   }
+    //   sort(ans.begin(),ans.end());
+    //   for(int i=0;i<m;i++){
+    //       a[i]=ans[i];
+    //   }
+    //   for(int i=0;i<n;i++){
+    //       b[i]=ans[m+i];
+    //   }
+    
+    int m=a.size();
+    int n=b.size();
+    
+    int gp=ceil((double)(m+n)/2.0);
+    
+    while(gp>0){
+        int i=0,j=gp;
+        
+        while(j<m+n){
+            
+        
+        if(j<m && a[i]>a[j]){
+            swap(a[i],a[j]);
+        }
+        else if(i<m && j>=m && a[i]>b[j-m]){
+            swap(a[i],b[j-m]);
+        }
+        else if(i>=m && j>=m && b[i-m]>b[j-m]){
+            swap(b[i-m],b[j-m]);
+        }
+        i++;
+        j++;
+    }
+    
+    if(gp==1) break;
+    gp=ceil((double)gp/2.0);
+    }
+    
+    }
+};
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 10 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 
