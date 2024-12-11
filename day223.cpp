6de6fd31807a034d6e1493bdbc5ec6201d411158
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-11 23:56:33 +0530

    Update day223.cpp

--- day223.cpp
+++ day223.cpp
@@ -124,3 +124,24 @@ Constraints:
 
 1 <= nums.length <= 105
 0 <= nums[i], k <= 105
+
+
+class Solution {
+public:
+    int maximumBeauty(vector<int>& nums, int k) {
+        
+        sort(nums.begin(),nums.end());
+
+        int ans=0;
+        int left=0;
+
+        for(int right=0;right<nums.size();right++){
+            while(nums[right]-nums[left]>2*k){
+                left++;
+            }
+
+            ans=max(ans,right-left+1);
+        }
+        return ans;
+    }
+};
