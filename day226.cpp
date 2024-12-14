f0d9e304ed7926f5c0933b061d65981b0bd52508
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-14 22:35:07 +0530

    Update day226.cpp

--- day226.cpp
+++ day226.cpp
@@ -60,3 +60,46 @@ class Solution {
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 14 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        // int n=nums.size();
+        // for(int i=0;i<n;i++){
+        //     if(nums[i]==target){
+        //         return i;
+        //     }
+        // }
+        // return -1;  //TC=O(N)
+
+        int n=nums.size();
+        int low=0;
+        int high=n-1;
+        int ind=-1;
+        while(low<=high){
+            int mid=(low+high)/2; 
+
+            if(nums[mid]==target) return mid;
+
+            //left side sorted
+            if(nums[mid]>=nums[low]){
+             if(nums[low]<=target and target<=nums[mid]){
+                high=mid-1;
+             }
+             else{
+                low=mid+1;
+             }
+            }
+            //right side sorted
+            else{
+                 if(nums[mid]<=target and target<=nums[high]){
+                low=mid+1;
+             }
+             else{
+                high=mid-1;
+             }
+            }
+        }
+        return -1;
+    }
+};
