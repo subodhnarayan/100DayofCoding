c9bbf74356879b30fd7550983f4f90efca42c295
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-14 22:34:38 +0530

    Update day226.cpp

--- day226.cpp
+++ day226.cpp
@@ -21,6 +21,42 @@ Constraints:
 1 ≤ key ≤ 106
 
 
+class Solution {
+  public:
+    int search(vector<int>& arr, int key) {
+        //brute force approach
+    //     for(int i=0;i<arr.size();i++){
+    //     if(arr[i]==key){
+    //         return i;
+    //     }
+    // }
+    // return -1; tc=0(N)
+    
+    int start=0;
+    int end=arr.size()-1;
+    while(start<=end){
+        int mid=(start+end)/2;
+        if(arr[mid]==key) return mid;
+        if(arr[start]<=arr[mid]){
+            if(arr[start]<=key && key<=arr[mid]){
+               end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        else{
+            if(arr[mid]<=key && key<=arr[end]){
+               start=mid+1;
+            }
+            else{
+                end=mid-1;
+            }
+        }
+    }
+    return -1;
+    }
+};
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 14 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
