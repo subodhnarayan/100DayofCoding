97b0840d3f07e6563dfca2d53d58a1272c711859
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-17 00:00:08 +0530

    Update day228.cpp

--- day228.cpp
+++ day228.cpp
@@ -100,3 +100,27 @@ Constraints:
 1 <= k <= 10
 1 <= multiplier <= 5
 
+
+class Solution {
+public:
+    vector<int> getFinalState(vector<int>& nums, int k, int multiplier) {
+          vector<pair<int, int>> heap;
+        for (int i = 0; i < nums.size(); i++) {
+            heap.push_back({nums[i], i});
+        }
+
+        make_heap(heap.begin(), heap.end(), greater<>());
+
+        while (k--) {
+            pop_heap(heap.begin(), heap.end(), greater<>());
+            auto [value, i] = heap.back();
+            heap.pop_back();
+
+            nums[i] *= multiplier;
+            heap.push_back({nums[i], i});
+            push_heap(heap.begin(), heap.end(), greater<>());
+        }
+
+        return nums;
+    }
+};
