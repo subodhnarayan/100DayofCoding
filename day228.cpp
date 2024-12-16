c18cf06d7d4d38268ada75a85ae4887216731d6e
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-16 23:58:59 +0530

    Update day228.cpp

--- day228.cpp
+++ day228.cpp
@@ -19,7 +19,30 @@ Constraints:
 0 <= a[i], b[i] < 10
 
 
-
+class Solution {
+  public:
+    int kthElement(vector<int>& a, vector<int>& b, int k) {
+         int s1 = a.size();
+        int s2 = b.size();
+        int i = 0;
+        int j = 0;
+        int ans = -1;
+        while ( i < s1 && j < s2 ) {
+            if ( a[ i ] < b[ j ] )
+                ans = a[i++];
+            else
+                ans = b[j++];
+            if ( --k == 0 ) return ans;
+        }
+        while ( i++ < s1 ) {
+            if ( --k == 0 ) return a[ i - 1 ];
+        }
+        while ( j++ < s2 ) {
+            if ( --k == 0 ) return b[ j - 1 ];
+        }
+        return -1;
+    }
+};
 
 
 
