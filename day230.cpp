62a88b908e8a9def1c57ab3009563faa4f289793
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-18 23:25:24 +0530

    Update day230.cpp

--- day230.cpp
+++ day230.cpp
@@ -160,3 +160,22 @@ Constraints:
 
 1 <= prices.length <= 500
 1 <= prices[i] <= 1000
+
+
+
+class Solution {
+public:
+    vector<int> finalPrices(vector<int>& prices) {
+         const int n=prices.size();
+        int stack[500], top=0;
+        stack[top]=n-1;
+        vector<int> ans=prices;
+        for(int i=n-2; i>=0; i--){
+            while(top>=0 && prices[i]<prices[stack[top]])
+                top--;
+            if (top>=0) ans[i]-=prices[stack[top]];
+            stack[++top]=i;
+        }
+        return ans;
+    }
+};
