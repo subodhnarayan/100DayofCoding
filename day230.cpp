8110edbbec55e2166062b376304abfd870b29390
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-18 23:24:22 +0530

    Update day230.cpp

--- day230.cpp
+++ day230.cpp
@@ -31,6 +31,94 @@ Constraints:
 1 <= k <= 10
 
 
+Time Complexity: O(n log n)Auxiliary Space: O(1)
+
+
+//{ Driver Code Starts
+// Initial function template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int findPages(vector<int> &arr, int k) {
+        int N=arr.size();
+        int M=k;
+        if(M>N)
+        return -1;
+        int start=0,end=0,ans,mid;
+        for(int i=0;i<N;i++)
+        {
+            start=max(arr[i],start);
+            end=end+arr[i];
+        }
+        while(start<=end)
+        {
+            mid=start+(end-start)/2;
+            int count=1;
+            int page=0;
+            for(int i=0;i<N;i++)
+            {
+                page=page+arr[i];
+                if(page>mid)
+                {
+                    count++;
+                    page=arr[i];
+                }
+            }
+            if(count<=M)
+            {
+                ans=mid;
+                end=mid-1;
+            }
+            else{
+                start=mid+1;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int test_case;
+    cin >> test_case;
+    cin.ignore();
+    while (test_case--) {
+
+        int d;
+        vector<int> arr, brr, crr;
+        string input;
+        getline(cin, input);
+        stringstream ss(input);
+        int number;
+        while (ss >> number) {
+            arr.push_back(number);
+        }
+        getline(cin, input);
+        ss.clear();
+        ss.str(input);
+        while (ss >> number) {
+            crr.push_back(number);
+        }
+        d = crr[0];
+        int n = arr.size();
+        Solution ob;
+        int ans = ob.findPages(arr, d);
+        cout << ans << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 18 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 
