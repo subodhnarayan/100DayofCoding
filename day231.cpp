575fbb13f5415584ef47fad55c3846113629c881
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-19 22:20:34 +0530

    Update day231.cpp

--- day231.cpp
+++ day231.cpp
@@ -85,3 +85,17 @@ n == arr.length
 1 <= n <= 10
 0 <= arr[i] < n
 All the elements of arr are unique.
+
+
+class Solution {
+public:
+    int maxChunksToSorted(vector<int>& arr) {
+        int runningSum = 0, expectedSum = 0, chunks = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            runningSum += arr[i];
+            expectedSum += i;
+            if (runningSum == expectedSum) chunks++;
+        }
+        return chunks; 
+    }
+};
