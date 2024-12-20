cb17ded614ef52eaab516511583e6bd4b22d8107
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-20 22:48:20 +0530

    Update day235.cpp

--- day235.cpp
+++ day235.cpp
@@ -20,6 +20,84 @@ Constraints:
 1 <= n, m <= 1000
 0 <= mat[i][j]<= 100
 
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution {
+  public:
+    vector<int> spirallyTraverse(vector<vector<int> > &matrix) {
+        vector<int> ans;
+        int row=matrix.size();
+        int col=matrix[0].size();
+        int top=0;
+        int bottom=row-1;
+        int left=0;
+        int right=col-1;
+        while(left<=right && top<=bottom)
+        {
+            for(int i=left;i<=right;i++)
+            {
+               ans.push_back(matrix[top][i]);
+            }
+            top++;
+            for(int j=top;j<=bottom;j++)
+            {
+                ans.push_back(matrix[j][right]);
+            }
+            right--;
+            if(top<=bottom){
+            for(int i=right;i>=left;i--)
+            {
+                ans.push_back(matrix[bottom][i]);
+            }
+            bottom--;
+            }
+            if(left<=right)
+            {
+            for(int j=bottom;j>=top;j--)
+            {
+                ans.push_back(matrix[j][left]);
+            }
+            left++;
+            }
+        }
+        return ans;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int r, c;
+        cin >> r >> c;
+        vector<vector<int>> matrix(r);
+
+        for (int i = 0; i < r; i++) {
+            matrix[i].assign(c, 0);
+            for (int j = 0; j < c; j++) {
+                cin >> matrix[i][j];
+            }
+        }
+
+        Solution ob;
+        vector<int> result = ob.spirallyTraverse(matrix);
+        for (int i = 0; i < result.size(); ++i)
+            cout << result[i] << " ";
+        cout << endl;
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
+
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 20 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
