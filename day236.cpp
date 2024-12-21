972cbd14142148fe9be35093a031592a956e3b9b
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-21 21:48:07 +0530

    Update day236.cpp

--- day236.cpp
+++ day236.cpp
@@ -1,6 +1,26 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 21 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
+Rotate by 90 degree
+Difficulty: EasyAccuracy: 56.88%Submissions: 99K+Points: 2
+Given a square matrix mat[][] of size n x n. The task is to rotate it by 90 degrees in an anti-clockwise direction without using any extra space. 
 
+Examples:
+
+Input: mat[][] = [[1, 2, 3],
+                [4, 5, 6]
+                [7, 8, 9]]
+Output: Rotated Matrix:
+[3, 6, 9]
+[2, 5, 8]
+[1, 4, 7]
+Input: mat[][] = [[1, 2],
+                [3, 4]]
+Output: Rotated Matrix:
+[2, 4]
+[1, 3]
+Constraints:
+1 ≤ n ≤ 102
+0 <= mat[i][j] <= 103
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 21 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
