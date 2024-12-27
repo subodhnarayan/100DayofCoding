41435369f5ad9f24f13bcab25a9d86bb363fd3e7
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-27 20:23:00 +0530

    Update day242.cpp

--- day242.cpp
+++ day242.cpp
@@ -78,3 +78,22 @@ Constraints:
 
 2 <= values.length <= 5 * 104
 1 <= values[i] <= 1000
+
+class Solution {
+public:
+    int maxScoreSightseeingPair(vector<int>& values) {
+         int n = values.size();
+        if (n < 2) return values[0] + values[1] + 0 - 1; 
+        vector<int> suffixMax(n);
+        suffixMax[n - 1] = values[n - 1] - (n - 1);
+        for (int i = n - 2; i >= 0; i--) {
+            suffixMax[i] = max(suffixMax[i + 1], values[i] - i);
+        }
+        int maxScore = INT_MIN;
+        for (int i = 0; i < n - 1; i++) {
+            maxScore = max(maxScore, values[i] + i + suffixMax[i + 1]);
+        }
+
+        return maxScore;
+    }
+};
