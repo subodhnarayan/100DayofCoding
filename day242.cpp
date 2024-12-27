58a2f607f2f0b06fcb53ea7e925087f0713657cf
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-27 20:21:40 +0530

    Update day242.cpp

--- day242.cpp
+++ day242.cpp
@@ -31,6 +31,21 @@ Constraints:
 1 <= values[i] <= 1000
 
 
+class Solution {
+  public:
+    int countPairs(vector<int> &arr, int target) {
+        int count=0;
+        unordered_map<int,int> u;
+        for(int x:arr)
+        {
+            if(u.find(target-x)!=u.end())
+            count+=u[target-x];
+            u[x]++;
+        }
+        return count;
+    }
+};
+
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 28 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
