48914f9035c43d2a50179f516b079aab3f62bc3a
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-28 22:28:30 +0530

    Update day243.cpp

--- day243.cpp
+++ day243.cpp
@@ -22,5 +22,28 @@ Constraints:
 3 <= arr.size() <= 103
 -104 <= arr[i] <= 104
 
+class Solution {
+  public:
+    vector<vector<int>> findTriplets(vector<int> &nums) {
+         vector<vector<int>> res;
+        
+        for(int i=0;i<nums.size();i++)
+        {
+            unordered_map<int,unordered_set<int>> mp;
+            
+            for(int j=i+1;j<nums.size();j++)
+            mp[nums[j]].insert(j);
+            
+            for(int j=i+1;j<nums.size();j++)
+                if(mp.find(((-1) * nums[i])-nums[j])!=mp.end())
+                    for(auto it : mp[((-1) * nums[i])-nums[j]])
+                        if(it>i && it>j)
+                            res.push_back({i,j,it});
+        }
+
+        return res;
+    }
+};
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 29 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
