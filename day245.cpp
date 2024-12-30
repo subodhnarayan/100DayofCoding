9917043891ab8f6197fb1ea46d24e606daf277c1
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-30 15:56:20 +0530

    Update day245.cpp

--- day245.cpp
+++ day245.cpp
@@ -25,5 +25,21 @@ Constraints:
 
 
 
+class Solution {
+  public:
+    // Function to return the count of number of elements in union of two arrays.
+    int findUnion(vector<int>& a, vector<int>& b) {
+        set<int> st;
+        for(int i=0;i<a.size();i++){
+            st.insert(a[i]);
+        }
+        for(int i=0;i<b.size();i++){
+            st.insert(b[i]);
+        }
+        return st.size();
+    }
+    
+};
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 30 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
