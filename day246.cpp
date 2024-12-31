fda220a4121e6469a5b3387063658c22e965dae5
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2024-12-31 21:01:18 +0530

    Update day246.cpp

--- day246.cpp
+++ day246.cpp
@@ -20,6 +20,39 @@ Constraints:
 0 <= arr[i] <= 105
 
 
+
+class Solution {
+  public:
+
+    // Function to return length of longest subsequence of consecutive integers.
+    int longestConsecutive(vector<int>& nums) {
+       
+       int n=nums.size();
+        int longest=1;
+        if(n==0){
+            return 0;
+        }
+        unordered_set<int>st;
+        for(int i=0;i<n;i++){
+            st.insert(nums[i]);
+        }
+        for(auto it:st){
+            if(st.find(it-1)==st.end()){
+                int cnt=1;
+                int x=it;
+            
+            while(st.find(x+1)!=st.end()){
+                x=x+1;
+                cnt=cnt+1;
+            }
+            longest=max(longest,cnt);
+        }
+        }
+        return longest;
+
+    }
+};
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 31 / 12 / 2024 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 
