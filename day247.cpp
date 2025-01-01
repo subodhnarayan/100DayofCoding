b75996d7c5cc9de7b9065c08211b1d0e8c2f7706
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-01 21:50:29 +0530

    Update day247.cpp

--- day247.cpp
+++ day247.cpp
@@ -94,3 +94,31 @@ Constraints:
 
 2 <= s.length <= 500
 The string s consists of characters '0' and '1' only.
+
+
+
+
+class Solution {
+public:
+    int maxScore(string inputStr) {
+        int onesCount = 0;
+        int zerosCount = 0;
+        int bestScore = INT_MIN;
+
+        for (int i = 0; i < inputStr.size() - 1; i++) {
+            if (inputStr[i] == '1') {
+                onesCount++;
+            } else {
+                zerosCount++;
+            }
+            
+            bestScore = max(bestScore, zerosCount - onesCount);
+        }
+        
+        if (inputStr[inputStr.size() - 1] == '1') {
+            onesCount++;
+        }
+        
+        return bestScore + onesCount;
+    }
+};
