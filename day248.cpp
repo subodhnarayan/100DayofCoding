8f48bdcada607fa5520b38fc679cea093a67abef
@MeSubodh <88626001+subodhnarayan@users.noreply.github.com>
2025-01-02 22:47:44 +0530

    Coding day of 248

---
+++ day248.cpp
@@ -0,0 +1,115 @@
+-- -- -- -- -- -- -- -- -GFG - POTD - 02 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+Subarrays with sum K
+Difficulty: MediumAccuracy: 49.74%Submissions: 57K+Points: 4
+Given an unsorted array of integers, find the number of continuous subarrays having sum exactly equal to a given number k.
+
+Examples:
+
+Input: arr = [10, 2, -2, -20, 10], k = -10
+Output: 3
+Explaination: Subarrays: arr[0...3], arr[1...4], arr[3...4] have sum exactly equal to -10.
+Input: arr = [9, 4, 20, 3, 10, 5], k = 33
+Output: 2
+Explaination: Subarrays: arr[0...2], arr[2...4] have sum exactly equal to 33.
+Input: arr = [1, 3, 5], k = 0
+Output: 0
+Explaination: No subarray with 0 sum.
+Constraints:
+
+1 ≤ arr.size() ≤ 105
+-103 ≤ arr[i] ≤ 103
+-107 ≤ k ≤ 107
+
+
+class Solution {
+  public:
+    int countSubarrays(vector<int> &arr, int k) {
+        // code here
+        map<int,int> mp;
+        mp[0] = 1;
+        int sum = 0, count = 0;
+        
+        for(int i=0; i<arr.size(); i++)
+        {
+            sum += arr[i];
+            
+            int rem = sum-k;
+            
+            if(mp.find(rem) != mp.end()){
+                count += mp[rem];
+            }
+            mp[sum]++;
+        }
+        return count;
+    }
+};
+-- -- -- -- -- -- -- -- -LeetCode - POTD - 02 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+2559. Count Vowel Strings in Ranges
+Solved
+Medium
+Topics
+Companies
+Hint
+You are given a 0-indexed array of strings words and a 2D array of integers queries.
+
+Each query queries[i] = [li, ri] asks us to find the number of strings present in the range li to ri (both inclusive) of words that start and end with a vowel.
+
+Return an array ans of size queries.length, where ans[i] is the answer to the ith query.
+
+Note that the vowel letters are 'a', 'e', 'i', 'o', and 'u'.
+
+ 
+
+Example 1:
+
+Input: words = ["aba","bcb","ece","aa","e"], queries = [[0,2],[1,4],[1,1]]
+Output: [2,3,0]
+Explanation: The strings starting and ending with a vowel are "aba", "ece", "aa" and "e".
+The answer to the query [0,2] is 2 (strings "aba" and "ece").
+to query [1,4] is 3 (strings "ece", "aa", "e").
+to query [1,1] is 0.
+We return [2,3,0].
+Example 2:
+
+Input: words = ["a","e","i"], queries = [[0,2],[0,1],[2,2]]
+Output: [3,2,1]
+Explanation: Every string satisfies the conditions, so we return [3,2,1].
+ 
+
+Constraints:
+
+1 <= words.length <= 105
+1 <= words[i].length <= 40
+words[i] consists only of lowercase English letters.
+sum(words[i].length) <= 3 * 105
+1 <= queries.length <= 105
+0 <= li <= ri < words.length
+
+
+class Solution {
+public:
+    vector<int> vowelStrings(vector<string>& words,
+                             vector<vector<int>>& queries) {
+        vector<int> ans(queries.size());
+        unordered_set<char> vowels{'a', 'e', 'i', 'o', 'u'};
+        vector<int> prefixSum(words.size());
+        int sum = 0;
+        for (int i = 0; i < words.size(); i++) {
+            string currentWord = words[i];
+            if (vowels.count(currentWord[0]) &&
+                vowels.count(currentWord[currentWord.size() - 1])) {
+                sum++;
+            }
+            prefixSum[i] = sum;
+        }
+
+        for (int i = 0; i < queries.size(); i++) {
+            vector<int> currentQuery = queries[i];
+            ans[i] =
+                prefixSum[currentQuery[1]] -
+                (currentQuery[0] == 0 ? 0 : prefixSum[currentQuery[0] - 1]);
+        }
+
+        return ans;
+    }
+};
