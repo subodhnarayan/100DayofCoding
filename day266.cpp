9c37b2d46d14dd7db0d309c2c4dfb6907d340c60
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-20 00:00:11 +0530

    Update day266.cpp

--- day266.cpp
+++ day266.cpp
@@ -1,5 +1,26 @@
--- -- -- -- -- -- -- -- -GFG - POTD - 19 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+-- -- -- -- -- -- -- -- -GFG - POTD - 20 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+Rotate a Linked List
+Difficulty: MediumAccuracy: 39.95%Submissions: 276K+Points: 4
+Given the head of a singly linked list, your task is to left rotate the linked list k times.
 
+Examples:
+
+Input: head = 10 -> 20 -> 30 -> 40 -> 50, k = 4
+Output: 50 -> 10 -> 20 -> 30 -> 40
+Explanation:
+Rotate 1: 20 -> 30 -> 40 -> 50 -> 10
+Rotate 2: 30 -> 40 -> 50 -> 10 -> 20
+Rotate 3: 40 -> 50 -> 10 -> 20 -> 30
+Rotate 4: 50 -> 10 -> 20 -> 30 -> 40
+
+Input: head = 10 -> 20 -> 30 -> 40 , k = 6
+Output: 30 -> 40 -> 10 -> 20 
+ 
+Constraints:
+
+1 <= number of nodes <= 105
+0 <= k <= 109
+0 <= data of node <= 109
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 20 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
