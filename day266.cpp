8d0d910f45738899a9c37070ba9a08c201de9861
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-20 00:00:38 +0530

    Update day266.cpp

--- day266.cpp
+++ day266.cpp
@@ -22,6 +22,33 @@ Constraints:
 0 <= k <= 109
 0 <= data of node <= 109
 
+
+class Solution {
+  public:
+    Node* rotate(Node* head, int k) {
+        // Your code here
+        if(k==0)return head;
+        int len=1;
+        Node* temp=head;
+        while(temp->next){
+            temp=temp->next;
+            len++;
+        }
+        k=k%len;
+        if(k==0)
+        {
+            return head;
+        }
+            temp->next=head;
+            Node* nwt=head;
+            for(int i=1;i<k;i++)
+            nwt=nwt->next;
+            head=nwt->next;
+            nwt->next=NULL;
+            return head;
+    }
+};
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 20 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 407. Trapping Rain Water II
