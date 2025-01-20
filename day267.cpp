1b48309b55570e9f65ae158c36535d7df2ca24df
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-20 22:58:07 +0530

    Update day267.cpp

--- day267.cpp
+++ day267.cpp
@@ -95,3 +95,30 @@ arr.length == m * n
 1 <= arr[i], mat[r][c] <= m * n
 All the integers of arr are unique.
 All the integers of mat are unique.
+
+
+class Solution {
+public:
+    int firstCompleteIndex(vector<int>& arr, vector<vector<int>>& mat) {
+         int rows = mat.size(), cols = mat[0].size();
+        unordered_map<int, pair<int, int>> positionMap;
+        vector<int> rowCount(rows, cols), colCount(cols, rows);
+
+        for (int r = 0; r < rows; ++r) {
+            for (int c = 0; c < cols; ++c) {
+                positionMap[mat[r][c]] = {r, c};
+            }
+        }
+
+        for (int idx = 0; idx < arr.size(); ++idx) {
+            int val = arr[idx];
+            auto [row, col] = positionMap[val];
+
+            if (--rowCount[row] == 0 || --colCount[col] == 0) {
+                return idx;
+            }
+        }
+
+        return -1;
+    }
+};
