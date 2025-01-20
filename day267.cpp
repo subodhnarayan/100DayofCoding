150bdac1e4d20bb40681d0134b763f15b213ed7f
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-20 22:56:22 +0530

    Update day267.cpp

--- day267.cpp
+++ day267.cpp
@@ -18,7 +18,42 @@ Constraints:
 1 <= no. of nodes<= 103
 0 <= node->data <= 105
 
-
+class Solution {
+  public:
+    Node* sortedMerge(Node* head1, Node* head2) {
+        if(head1==NULL) return head2;
+        if(head2==NULL) return head1;
+        int m=0;
+        int n=0;
+       
+        Node * res=new Node(0);
+        Node * ans=res;
+        while(head1!=NULL && head2!=NULL){
+            if(head1->data>head2->data){
+                ans->next=new Node(head2->data);
+                ans=ans->next;
+                head2=head2->next;
+            }
+            else{
+               ans->next=new Node(head1->data);
+                ans=ans->next;
+                head1=head1->next;
+            }
+        }
+        while(head1!=NULL){
+            ans->next=new Node(head1->data);
+            ans=ans->next;
+            head1=head1->next;
+        }
+        while(head2!=NULL){
+            ans->next=new Node(head2->data);
+            ans=ans->next;
+            head2=head2->next;
+        }
+        return res->next;
+        
+    }
+};
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 21 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
