5b238f69e65d8f7080d639e2f64653f1c9cfec8a
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-23 23:25:30 +0530

    Update day270.cpp

--- day270.cpp
+++ day270.cpp
@@ -49,3 +49,31 @@ n == grid[i].length
 1 <= m <= 250
 1 <= n <= 250
 grid[i][j] == 0 or 1
+
+
+
+class Solution {
+public:
+    int countServers(vector<vector<int>>& grid) {
+        vector<int> Rows(grid.size());
+        vector<int> Col(grid[0].size());
+        
+        // Calculate row and column sums
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                Rows[i] += grid[i][j];
+                Col[j] += grid[i][j];
+            }
+        }
+        
+        int ans = 0;
+        for (int i = 0; i < grid.size(); i++) {
+            for (int j = 0; j < grid[0].size(); j++) {
+                if (grid[i][j] == 1 && (Rows[i] > 1 || Col[j] > 1)) {
+                    ans++;
+                }
+            }
+        }
+        return ans;
+    }
+};
