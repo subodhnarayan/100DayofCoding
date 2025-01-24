fd32ed75da54e8a715d17e97e1d9049a13cc0210
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-24 23:46:32 +0530

    Update day271.cpp

--- day271.cpp
+++ day271.cpp
@@ -29,7 +29,24 @@ Constraints:
 
 Time Complexity: O(n)Auxiliary Space: O(1)
 
-
+class Solution {
+  public:
+    // Function to check if the linked list has a loop.
+    bool detectLoop(Node* head) {
+        // your code here
+        Node* fast=head;
+        Node* slow=head;
+        
+        while(fast!=NULL && fast->next!=NULL){
+            slow=slow->next;
+            fast=fast->next->next;
+            
+            if(fast==slow) return true;
+        }
+        
+        return false;
+    }
+};
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 24 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
