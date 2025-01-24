32124f346b4144acd0fba715c2d54628d689ffbd
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-24 23:47:37 +0530

    Update day271.cpp

--- day271.cpp
+++ day271.cpp
@@ -90,3 +90,40 @@ graph[i] is sorted in a strictly increasing order.
 The graph may contain self-loops.
 The number of edges in the graph will be in the range [1, 4 * 104].
 
+class Solution {
+public:
+    int n;
+    vector<int> degreein;
+    vector< vector<int> > rGraph;
+    vector<int> ans;
+    
+    void init(vector<vector<int>>& G){
+        n = G.size();
+        degreein = vector<int>(n,0);
+        rGraph.resize(n, vector<int>(0) );
+        
+        for (int u=0; u<n; u++){
+            for (int v:G[u]){
+                rGraph[v].push_back(u);
+                degreein[u]++;
+            }
+        }
+    }
+    vector<int> eventualSafeNodes(vector<vector<int>>& graph) {
+        init(graph);
+        queue<int> que;
+        for (int i=0; i<n; i++){
+            if (degreein[i]==0) que.push(i);
+        }
+        while (que.size()){
+            int u = que.front(); que.pop();
+            ans.push_back(u);
+            for (int v:rGraph[u]){
+                degreein[v]--;
+                if (degreein[v]==0) que.push(v);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
