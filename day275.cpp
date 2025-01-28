85c98b649b01458afc373014da596b34f93b74f9
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-28 22:55:32 +0530

    Create day275.cpp

---
+++ day275.cpp
@@ -0,0 +1,26 @@
+-- -- -- -- -- -- -- -- -GFG - POTD - 28 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
+
+Permutations of a String
+Difficulty: MediumAccuracy: 34.65%Submissions: 294K+Points: 4
+Given a string s, which may contain duplicate characters, your task is to generate and return an array of all unique permutations of the string. You can return your answer in any order.
+
+Examples:
+
+Input: s = "ABC"
+Output: ["ABC", "ACB", "BAC", "BCA", "CAB", "CBA"]
+Explanation: Given string ABC has 6 unique permutations.
+Input: s = "ABSG"
+Output: ["ABGS", "ABSG", "AGBS", "AGSB", "ASBG", "ASGB", "BAGS", "BASG", "BGAS", "BGSA", "BSAG", "BSGA", "GABS", "GASB", "GBAS", "GBSA", "GSAB", "GSBA", "SABG", "SAGB", "SBAG", "SBGA", "SGAB", "SGBA"]
+Explanation: Given string ABSG has 24 unique permutations.
+Input: s = "AAA"
+Output: ["AAA"]
+Explanation: No other unique permutations can be formed as all the characters are same.
+Constraints:
+1 <= s.size() <= 9
+s contains only Uppercase english alphabets
+
+
+
+-- -- -- -- -- -- -- -- -LeetCode - POTD - 28 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
+
