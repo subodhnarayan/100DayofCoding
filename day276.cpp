c316f123c81abe0d5591c6b9039208c15cbd8c89
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-29 23:05:29 +0530

    Update day276.cpp

--- day276.cpp
+++ day276.cpp
@@ -19,6 +19,23 @@ Constraints:
 Either b is not zero or e > 0.
 -104 <= be <= 104
 
+class Solution {
+  public:
+    double power(double b, int e) {
+        // code here
+        if(e==0)
+            return 1;
+        if(e==1)
+            return b;
+        if(e<0)
+            return 1/power(b, -e);
+            
+        if(e%2==0)
+            return power(b*b, e/2);
+        return power(b, e-1)*b;
+    }
+};
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 29 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 
