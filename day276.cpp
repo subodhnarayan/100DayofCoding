661812aaa5aba5a58f6e293681515f3620a7f2aa
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-29 23:05:07 +0530

    Update day276.cpp

--- day276.cpp
+++ day276.cpp
@@ -1,6 +1,23 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 29 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
+Implement Pow
+Difficulty: MediumAccuracy: 52.79%Submissions: 23K+Points: 4
+Implement the function power(b, e), which calculates b raised to the power of e (i.e. be).
 
+Examples:
+
+Input: b = 3.00000, e = 5
+Output: 243.00000
+Input: b = 0.55000, e = 3
+Output: 0.16638
+Input: b = -0.67000, e = -7
+Output: -16.49971
+Constraints:
+
+-100.0 < b < 100.0
+-109 <= e <= 109
+Either b is not zero or e > 0.
+-104 <= be <= 104
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 29 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
