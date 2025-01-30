a43f8733fe8e2c26a7d3f334c369833959ea613d
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-01-30 23:06:40 +0530

    Update day277.cpp

--- day277.cpp
+++ day277.cpp
@@ -25,6 +25,117 @@ Constraints:
 
 Time Complexity: O(n!)Auxiliary Space: O(n^2)
 
+
+//{ Driver Code Starts
+// Initial Template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+// User function Template for C++
+
+class Solution {
+  public:
+  
+    bool issafe(int row,int col,vector<vector<int>> &board,int n){
+        int crow=row;
+        int ccol=col;
+        
+        while(row>=0 && col>=0){
+            if(board[row][col]==1) return false;
+            row--;
+            col--;
+        }
+        
+        row=crow;
+        col=ccol;
+        while(col>=0){
+            if(board[row][col]==1) return false;
+            col--;
+        }
+        
+    
+        row=crow;
+        col=ccol;
+        
+         while(row<n && col>=0){
+            if(board[row][col]==1) return false;
+            row++;
+            col--;
+        }
+        
+        return true;
+        
+    }
+    
+    void soln(int col,vector<vector<int>> &board, vector<vector<int>> &ans,int n){
+        if(col==n){
+            vector<int> res;
+            for(int i=0;i<n;i++){
+                for(int j=0;j<n;j++){
+                    if(board[i][j]==1){
+                        res.push_back(j+1);
+                    }
+                }
+            }
+            ans.push_back(res);
+            return;
+        }
+        
+        for(int row=0;row<n;row++){
+         if(issafe(row,col,board,n)){
+             board[row][col]=1;
+             soln(col+1,board,ans,n);
+             board[row][col]=0;
+         }
+            
+        }
+    }
+  
+    vector<vector<int>> nQueen(int n) {
+        // code here
+        vector<vector<int>> ans;
+        vector<std::vector<int>> board(n, std::vector<int>(n, 0));
+        soln(0,board,ans,n);
+        sort(ans.begin(),ans.end());
+        return ans;
+       // getsoln(0,chesbod,ans,0);
+    }
+};
+
+//{ Driver Code Starts.
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+
+        Solution ob;
+        vector<vector<int>> ans = ob.nQueen(n);
+        if (ans.size() == 0)
+            cout << -1 << "\n";
+        else {
+            sort(ans.begin(), ans.end());
+            for (int i = 0; i < ans.size(); i++) {
+                cout << "[";
+                for (int u : ans[i])
+                    cout << u << " ";
+                cout << "] ";
+            }
+            cout << endl;
+        }
+
+        cout << "~"
+             << "\n";
+    }
+    return 0;
+}
+// } Driver Code Ends
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 29 / 01 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 
