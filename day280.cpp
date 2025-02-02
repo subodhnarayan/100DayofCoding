60f0c3a0839cc95f4c0fef005e5a664b98dcf839
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-02 23:37:29 +0530

    Update day280.cpp

--- day280.cpp
+++ day280.cpp
@@ -1,5 +1,24 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 02 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
+Level order traversal
+Difficulty: EasyAccuracy: 70.31%Submissions: 224K+Points: 2
+Given a root of a binary tree with n nodes, the task is to find its level order traversal. Level order traversal of a tree is breadth-first traversal for the tree.
+
+Examples:
+
+Input: root[] = [1, 2, 3]
+
+Output: [[1], [2, 3]]
+Input: root[] = [10, 20, 30, 40, 50]
+
+Output: [[10], [20, 30], [40, 50]]
+Input: root[] = [1, 3, 2, N, N, N, 4, 6, 5]
+
+Output: [[1], [3, 2], [4], [6, 5]]
+Constraints:
+
+1 ≤ number of nodes ≤ 105
+0 ≤ node->data ≤ 109
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 02 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
