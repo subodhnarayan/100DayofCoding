7b4ac9166d4e83fa018acb25da918317a07ccc34
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-02 23:37:56 +0530

    Update day280.cpp

--- day280.cpp
+++ day280.cpp
@@ -20,5 +20,40 @@ Constraints:
 1 ≤ number of nodes ≤ 105
 0 ≤ node->data ≤ 109
 
+class Solution {
+  public:
+  
+    vector<vector<int>> levelOrder(Node *root) {
+        // Your code here
+        vector<vector<int>> ans;
+            queue<Node*>pq;
+            pq.push(root);
+            vector<int> init;
+            
+        init.push_back (root->data);
+        ans.push_back(init);
+        
+        while (!pq.empty()) {
+            
+                vector <int> temp;
+                Node * curr = pq.front();
+                pq.pop ();
+            
+            if (curr->left){
+                
+                temp.push_back(curr->left->data);
+                pq.push(curr->left);
+            }
+            if(curr->right){
+                
+                temp.push_back(curr->right->data);
+                pq.push(curr->right);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
+
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 02 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
