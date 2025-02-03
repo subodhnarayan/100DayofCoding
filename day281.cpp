d2f00213d7142fe3dd465294563ef1c06fc0ad4b
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-03 22:18:57 +0530

    Update day281.cpp

--- day281.cpp
+++ day281.cpp
@@ -100,3 +100,19 @@ Constraints:
 
 1 <= nums.length <= 50
 1 <= nums[i] <= 50
+
+
+class Solution {
+public:
+    int longestMonotonicSubarray(vector<int>& nums) {
+        if (nums.empty()) return 0;
+        int maxLen = 1, inc = 1, dec = 1;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[i] > nums[i-1]) inc++, dec = 1;      // ↗️ Case
+            else if (nums[i] < nums[i-1]) dec++, inc = 1; // ↘️ Case
+            else inc = dec = 1;                           // 🟡 Case
+            maxLen = max(maxLen, max(inc, dec));          // Update global max
+        }
+        return maxLen;
+    }
+};
