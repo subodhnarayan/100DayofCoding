3daa4f0e51835e15686fa9afcb620babdae46245
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-05 23:52:18 +0530

    Create day283.cpp

---
+++ day283.cpp
@@ -0,0 +1,28 @@
+-- -- -- -- -- -- -- -- -GFG - POTD - 04 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
+
+Mirror Tree
+Difficulty: EasyAccuracy: 72.67%Submissions: 221K+Points: 2Average Time: 20m
+Given a binary tree, convert the binary tree to its Mirror tree.
+
+Mirror of a Binary Tree T is another Binary Tree M(T) with left and right children of all non-leaf nodes interchanged.     
+
+Examples:
+
+Input: root[] = [1, 2, 3, N, N, 4]
+Output: [1, 3, 2, N, 4]
+Explanation: 
+
+In the inverted tree, every non-leaf node has its left and right child interchanged.
+Input: root[] = [1, 2, 3, 4, 5]
+Output: [1, 3, 2, N, N, 5, 4]
+Explanation:
+
+In the inverted tree, every non-leaf node has its left and right child interchanged.
+Constraints:
+1 ≤ number of nodes ≤ 105
+1 ≤ node->data ≤ 105
+
+
+
+-- -- -- -- -- -- -- -- -LeetCode - POTD - 04 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
+
