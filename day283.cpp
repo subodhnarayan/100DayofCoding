5ee28ba12d968778b65cf2a605f7c566120f1e5e
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-05 23:53:30 +0530

    Update day283.cpp

--- day283.cpp
+++ day283.cpp
@@ -71,3 +71,23 @@ Constraints:
 1 <= s1.length, s2.length <= 100
 s1.length == s2.length
 s1 and s2 consist of only lowercase English letters.
+
+
+class Solution {
+public:
+    bool areAlmostEqual(string s1, string s2) {
+        const int n=s1.size();
+        int cntD=0, i;
+        array<char, 2> D={0};
+        for(i=0; i<n && cntD<=2; i++){
+            char x=s1[i], y=s2[i];
+            if (x!=y){
+                cntD++;
+                if (cntD>2 ||(cntD==2 && (D[0]!=y || D[1]!=x )))
+                    return 0;
+                D={x, y};
+            }
+        }
+        return cntD==0 || cntD==2;
+    }
+};
