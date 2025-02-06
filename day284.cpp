8c2fd08f1559cd261249b0c777463412486881d5
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-06 23:33:18 +0530

    Update day284.cpp

--- day284.cpp
+++ day284.cpp
@@ -25,7 +25,31 @@ Constraints:
 0 ≤ nodes -> data ≤ 103
 Both the inorder and preorder arrays contain unique values.
 
-
+class Solution {
+  public:
+    int idx=0;
+  Node* Solve(vector<int>&inorder,vector<int>&preorder,int s,int e){
+      if(s>e)
+      return NULL;
+      Node* root=new Node(preorder[idx++]);
+      int k;
+      for(int l=s;l<=e;l++){
+          if(inorder[l]==root->data){
+              k=l;
+              break;
+          }
+      }
+      root->left=Solve(inorder,preorder,s,k-1);
+      root->right=Solve(inorder,preorder,k+1,e);
+      return root;
+  }
+  
+    // Function to build the tree from given inorder and preorder traversals
+    Node *buildTree(vector<int> &inorder, vector<int> &preorder) {
+        // code here
+        return Solve(inorder,preorder,0,inorder.size()-1);
+    }
+};
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 06 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
