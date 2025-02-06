ac7280363992111edce71ccd0804b976d2ff30c4
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-06 23:31:06 +0530

    Update day284.cpp

--- day284.cpp
+++ day284.cpp
@@ -34,5 +34,20 @@ Constraints:
 1 <= nums[i] <= 104
 All elements in nums are distinct.
 
+class Solution {
+public:
+    int tupleSameProduct(vector<int>& nums) {
+        unordered_map<int, int> mp;
+        int ans = 0, n = nums.size();
+        for (int i = 0; i < n; i++)
+            for (int j = i + 1; j < n; j++) {
+                int product = nums[i] * nums[j];
+                ans += 8 * mp[product];
+                mp[product]++;
+            }
+        return ans;
+    }
+};
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 06 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
