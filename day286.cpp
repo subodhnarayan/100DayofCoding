6b6fc662c2c37d92e69f5c744e8002b703ebf773
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-08 23:35:13 +0530

    Update day286.cpp

--- day286.cpp
+++ day286.cpp
@@ -142,3 +142,39 @@ Constraints:
 
 1 <= index, number <= 109
 At most 105 calls will be made in total to change and find.
+
+
+class NumberContainers {
+    std::map<int, int> indexToNum;
+    std::map<int, std::set<int>> numToIndices;
+public:
+    NumberContainers() {
+        
+    }
+    
+    void change(int index, int number) {
+         if (indexToNum.count(index)) {
+            int oldNum = indexToNum[index];
+            numToIndices[oldNum].erase(index);
+
+            // Remove the key if the set becomes empty
+            if (numToIndices[oldNum].empty()) {
+                numToIndices.erase(oldNum);
+            }
+        }
+
+        indexToNum[index] = number;
+        numToIndices[number].insert(index);
+    }
+    
+    int find(int number) {
+        return numToIndices.count(number) ? *numToIndices[number].begin() : -1;
+    }
+};
+
+/**
+ * Your NumberContainers object will be instantiated and called as such:
+ * NumberContainers* obj = new NumberContainers();
+ * obj->change(index,number);
+ * int param_2 = obj->find(number);
+ */
