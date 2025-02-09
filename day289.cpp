cd3584ba08fb1e5573828acbef5ed0cedd2ae09f
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-09 22:54:52 +0530

    Update day289.cpp

--- day289.cpp
+++ day289.cpp
@@ -20,4 +20,27 @@ Constraints:
 -104 ≤ node->data ≤ 104
 
 
+class Solution {
+    public:
+      // Function to return maximum path sum from any node in a tree.
+       pair<int,int> solve(Node *root)
+     {
+         if(root==NULL)
+         return {INT_MIN,0};
+         
+         pair<int,int> sumleft= solve(root->left);
+         pair<int,int> sumright=solve(root->right);
+         
+         int cursum=max(root->data, root->data + max(sumleft.second, sumright.second));
+         
+         int golmax=max({sumleft.first, sumright.first, cursum,root->data + sumleft.second + sumright.second});
+         
+         return {golmax,cursum};
+         
+         
+     }
+      int findMaxSum(Node *root) {
+          return max(solve(root).first,solve(root).second);
+      }
+  };
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 09 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
