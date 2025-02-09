72ac71931c2c8f9447ae925e6da80aca28b7f733
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-09 22:55:48 +0530

    Update day289.cpp

--- day289.cpp
+++ day289.cpp
@@ -78,3 +78,20 @@ Constraints:
 
 1 <= nums.length <= 105
 1 <= nums[i] <= 109
+
+
+class Solution {
+    public:
+        long long countBadPairs(vector<int>& nums) {
+            unordered_map<int, int> freq;
+            long long goodPairs = 0, n = nums.size();
+            
+            for (int i = 0; i < n; i++) {
+                int key = nums[i] - i;
+                goodPairs += freq[key];
+                freq[key]++;
+            }
+            
+            return (n * (n - 1)) / 2 - goodPairs;
+        }
+    };
