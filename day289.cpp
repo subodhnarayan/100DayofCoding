cd5d2627b5dfc2187a630e8620213ab676f3ba09
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-09 22:54:19 +0530

    Update day289.cpp

--- day289.cpp
+++ day289.cpp
@@ -1,3 +1,23 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 09 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
+Maximum path sum from any node
+Difficulty: MediumAccuracy: 42.92%Submissions: 92K+Points: 4Average Time: 45m
+Given a binary tree, the task is to find the maximum path sum. The path may start and end at any node in the tree.
+
+Examples:
+
+Input: root[] = [10, 2, 10, 20, 1, N, -25, N, N, N, N, 3, 4]
+Output: 42
+Explanation: 
+
+Max path sum is represented using green colour nodes in the above binary tree.
+Input: root[] = [-17, 11, 4, 20, -2, 10]
+Output: 31
+Explanation: 
+
+Max path sum is represented using green colour nodes in the above binary tree.
+Constraints:
+1 ≤ number of nodes ≤ 103
+-104 ≤ node->data ≤ 104
+
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 09 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
