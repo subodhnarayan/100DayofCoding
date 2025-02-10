8dfcf5122d9d8bb5b702e2f6592e4cbed0732c1f
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-10 23:17:44 +0530

    Update day290.cpp

--- day290.cpp
+++ day290.cpp
@@ -26,4 +26,27 @@ Constraints:
 -109 ≤ k ≤ 109
 
 
+class Solution {
+    public:
+       int total = 0;
+          const int mod=1e9+7;
+          void solve(Node* node, int k, unordered_map<int,int> &ump, int sum){
+              if(node==nullptr) return;
+              sum+=node->data;
+              if(ump.find(sum-k)!=ump.end()) total=(total+ump[sum-k])%mod;
+              ump[sum]++;
+              solve(node->left, k, ump, sum);
+              solve(node->right, k, ump, sum);
+              ump[sum]--;
+              if(ump[sum]==0) ump.erase(sum);
+          }
+      int sumK(Node *root, int k) {
+          // code here
+           unordered_map<int,int> ump;
+          ump[0]=1;
+          solve(root, k, ump, 0);
+          return total;
+      }
+  };
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 09 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
