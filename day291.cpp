9cec6de1cb2d81dadc8f13a58dfd6b1478927c92
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-11 22:20:00 +0530

    Update day291.cpp

--- day291.cpp
+++ day291.cpp
@@ -95,3 +95,25 @@ Constraints:
 1 <= s.length <= 1000
 1 <= part.length <= 1000
 s​​​​​​ and part consists of lowercase English letters.
+
+
+
+class Solution {
+    public:
+        string removeOccurrences(string s, string part) {
+            string resultStack;
+            int targetLength = part.size();
+            char targetEndChar = part.back();
+    
+            for (char currentChar : s) {
+                resultStack.push_back(currentChar);
+    
+                if (currentChar == targetEndChar && resultStack.size() >= targetLength) {
+                    if (resultStack.substr(resultStack.size() - targetLength) == part) {
+                        resultStack.erase(resultStack.size() - targetLength);
+                    }
+                }
+            }
+            return resultStack;
+        }
+    };
