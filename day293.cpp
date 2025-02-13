3679ab5b917819c32c760dabf4d78e8ffe1f3cf9
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-13 23:18:36 +0530

    Update day293.cpp

--- day293.cpp
+++ day293.cpp
@@ -88,3 +88,23 @@ After four operations, nums becomes equal to [33].
 At this stage, all the elements of nums are greater than 20 so we can stop.
 It can be shown that 4 is the minimum number of operations needed so that all elements of the array are greater than or equal to 20.
  
+
+class Solution {
+    public:
+        int minOperations(vector<int>& nums, int k) {
+            priority_queue<long, vector<long>, greater<long>> min_heap(nums.begin(),
+                                                                       nums.end());
+            int num_operations = 0;
+    
+            while (min_heap.top() < k) {
+                long x = min_heap.top();
+                min_heap.pop();
+                long y = min_heap.top();
+                min_heap.pop();
+                min_heap.push(min(x, y) * 2 + max(x, y));
+    
+                num_operations++;
+            }
+            return num_operations;
+        }
+    };
