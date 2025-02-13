bcce9e5a77dd7aafcaf07a05e98358cb04e32d7f
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-13 23:17:29 +0530

    Update day293.cpp

--- day293.cpp
+++ day293.cpp
@@ -20,6 +20,32 @@ Constraints:
 1 ≤ target ≤ 106
 
 
+class Solution {
+    public:
+     unordered_map<int,int> map;
+  
+   bool preorder(Node* root, int target) {
+          
+          if(root == nullptr){
+              return false;
+          }
+          if(map.find(target - root->data) != map.end()){
+              return true;
+          }
+          map[root->data] = 1;
+          if(preorder(root->left,target)) return true;;
+          if(preorder(root->right,target)) return true;
+          
+          return false;
+      }
+      bool findTarget(Node *root, int target) {
+          // your code here.
+          if(preorder(root,target)){
+              return true;
+          }
+          return false;
+      }
+  };
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 13 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
