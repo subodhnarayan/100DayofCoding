bf9d334bcbdbd47312455f01e3255d6d4232c82c
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-14 22:36:50 +0530

    Update day294.cpp

--- day294.cpp
+++ day294.cpp
@@ -112,3 +112,29 @@ The product of the stream at any point in time will fit in a 32-bit integer.
  
 
 Follow-up: Can you implement both GetProduct and Add to work in O(1) time complexity instead of O(k) time complexity?
+
+
+class ProductOfNumbers {
+    public:
+        vector<int> list;
+        int prod = 1;
+        ProductOfNumbers() {
+            
+        }
+        
+        void add(int num) {
+             if (num == 0) {
+                list.clear();
+                prod = 1;
+            } else {
+                prod *= num;
+                list.push_back(prod);
+            }
+        }
+        
+        int getProduct(int k) {
+            if (list.size() < k) return 0;
+            if (list.size() == k) return list.back();
+            return list.back() / list[list.size() - k - 1];
+        }
+    };
