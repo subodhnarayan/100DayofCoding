b6c9f5d5676e4b0b49e985f3296c6aa25a964314
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-15 23:06:25 +0530

    Update day295.cpp

--- day295.cpp
+++ day295.cpp
@@ -83,3 +83,40 @@ Hence, the punishment number of 37 is 1 + 81 + 100 + 1296 = 1478
 Constraints:
 
 1 <= n <= 1000
+
+class Solution {
+    public:
+      bool canPartition(string stringNum, int target) {
+            // Valid Partition Found
+            if (stringNum == "" && target == 0) return true;
+    
+            // Invalid Partition Found
+            if (target < 0) return false;
+    
+            // Recursively check all partitions for a valid partition
+            for (int index = 0; index < stringNum.size(); index++) {
+                string left = stringNum.substr(0, index + 1);
+                string right = stringNum.substr(index + 1);
+                int leftNum = stoi(left);
+    
+                if (canPartition(right, target - leftNum)) return true;
+            }
+            return false;
+        }
+    
+        int punishmentNumber(int n) {
+            int punishmentNum = 0;
+    
+            // Iterate through numbers in range [1, n]
+            for (int currentNum = 1; currentNum <= n; currentNum++) {
+                int squareNum = currentNum * currentNum;
+    
+                // Check if valid partition can be found and add squared number if
+                // so
+                if (canPartition(to_string(squareNum), currentNum)) {
+                    punishmentNum += squareNum;
+                }
+            }
+            return punishmentNum;
+        }
+};
