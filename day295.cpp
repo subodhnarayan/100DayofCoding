a9ff11b0657c27330644eb4f5c2e7191ceb207f0
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-15 23:05:29 +0530

    Update day295.cpp

--- day295.cpp
+++ day295.cpp
@@ -26,6 +26,21 @@ Constraints:
 1 <= n1, n2 <= 105
 
 
+class Solution {
+    public:
+      Node* LCA(Node* root, Node* n1, Node* n2) {
+          if(!root) return NULL;
+          if(root->data>n1->data && root->data>n2->data){
+              LCA(root->left,n1,n2);
+          }
+          else if(root->data<n1->data && root->data<n2->data){
+              LCA(root->right,n1,n2);
+          }
+          else{
+              return root;
+          }
+      }
+  };
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 15 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
