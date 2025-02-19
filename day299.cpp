fefcee00a32d1ced7011c4024d19323af077052a
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-19 19:48:54 +0530

    Update day299.cpp

--- day299.cpp
+++ day299.cpp
@@ -1,6 +1,5 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 19 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
-
 Merge K sorted linked lists
 Difficulty: MediumAccuracy: 57.01%Submissions: 96K+Points: 4Average Time: 60m
 Given an array arr[] of n sorted linked lists of different sizes. The task is to merge them in such a way that after merging they will be a single sorted linked list, then return the head of the merged linked list.
@@ -30,6 +29,37 @@ Constraints
 1 <= total no. of nodes <= 105
 1 <= node->data <= 103
 
+
+class Solution {
+    public:
+    
+     void merge(vector<int> &res,Node* head){
+         if(head==NULL) return;
+         Node* curr=head;
+         while(curr!=NULL){
+             res.push_back(curr->data);
+             curr=curr->next;
+         }
+         return;
+     }
+      Node* mergeKLists(vector<Node*>& arr) {
+          
+          vector<int> res;
+          for(int i=0;i<arr.size();i++){
+              merge(res,arr[i]);
+          }
+          
+          sort(res.begin(),res.end());
+          Node* ans=new Node(0);
+          Node* ret=ans;
+          for(int i=0;i<res.size();i++){
+              ret->next=new Node(res[i]);
+              ret=ret->next;
+          }
+          
+          return ans->next;
+      }
+  };
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 19 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
