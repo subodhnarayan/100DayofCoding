1485cf0cc9208dbbf6cee25ecae8760c6a6fee8c
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-19 19:48:21 +0530

    Update day299.cpp

--- day299.cpp
+++ day299.cpp
@@ -1,5 +1,36 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 19 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
 
+Merge K sorted linked lists
+Difficulty: MediumAccuracy: 57.01%Submissions: 96K+Points: 4Average Time: 60m
+Given an array arr[] of n sorted linked lists of different sizes. The task is to merge them in such a way that after merging they will be a single sorted linked list, then return the head of the merged linked list.
+
+Examples:
+
+Input: arr[] = [1 -> 2 -> 3, 4 -> 5, 5 -> 6, 7 -> 8]
+Output: 1 -> 2 -> 3 -> 4 -> 5 -> 5 -> 6 -> 7 -> 8
+Explanation:
+The arr[] has 4 sorted linked list of size 3, 2, 2, 2.
+1st list: 1 -> 2-> 3
+2nd list: 4 -> 5
+3rd list: 5 -> 6
+4th list: 7 -> 8
+The merged list will be:
+ 
+Input: arr[] = [1 -> 3, 8, 4 -> 5 -> 6]
+Output: 1 -> 3 -> 4 -> 5 -> 6 -> 8
+Explanation:
+The arr[] has 3 sorted linked list of size 2, 3, 1.
+1st list: 1 -> 3
+2nd list: 8
+3rd list: 4 -> 5 -> 6
+The merged list will be:
+
+Constraints
+1 <= total no. of nodes <= 105
+1 <= node->data <= 103
+
+
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 19 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
