75b6603e19d3b0d0ca3a30da44f173fd9fd8c66a
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-19 19:50:11 +0530

    Update day299.cpp

--- day299.cpp
+++ day299.cpp
@@ -105,3 +105,25 @@ Constraints:
 1 <= n <= 10
 1 <= k <= 100
 
+class Solution {
+    public:
+       int n2;
+        string dfs(string prefix, int n, int k) {
+            if (n == 0)
+                return prefix;
+            for (char c = 'a'; c <= 'c'; c++) {
+                if (!prefix.empty() && c == prefix.back())
+                    continue;
+                int cnt = (1 << (n2 - prefix.length() - 1));
+                if (cnt >= k)
+                    return dfs(prefix + c, n - 1, k);
+                else
+                    k -= cnt;
+            }
+            return "";
+        }
+        string getHappyString(int n, int k) {
+            n2 = n;
+            return dfs("", n, k);
+        }
+};
