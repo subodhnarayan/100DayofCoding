b7d7ac519a0e92c25d90500b4e0fcb9ae923362b
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-22 19:01:36 +0530

    Update day302.cpp

--- day302.cpp
+++ day302.cpp
@@ -53,3 +53,34 @@ class Solution {
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 21 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
+1028. Recover a Tree From Preorder Traversal
+Solved
+Hard
+Topics
+Companies
+Hint
+We run a preorder depth-first search (DFS) on the root of a binary tree.
+
+At each node in this traversal, we output D dashes (where D is the depth of this node), then we output the value of this node.  If the depth of a node is D, the depth of its immediate child is D + 1.  The depth of the root node is 0.
+
+If a node has only one child, that child is guaranteed to be the left child.
+
+Given the output traversal of this traversal, recover the tree and return its root.
+
+ 
+
+Example 1:
+
+
+Input: traversal = "1-2--3--4-5--6--7"
+Output: [1,2,5,3,4,6,7]
+Example 2:
+
+
+Input: traversal = "1-2--3---4-5--6---7"
+Output: [1,2,5,3,null,6,null,4,null,7]
+Example 3:
+
+
+Input: traversal = "1-401--349---90--88"
+Output: [1,401,null,349,88,90]
