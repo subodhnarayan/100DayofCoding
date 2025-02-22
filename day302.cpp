57165f0a741b84a09feb6379daac643cff0aa190
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-22 18:59:55 +0530

    Update day302.cpp

--- day302.cpp
+++ day302.cpp
@@ -1,5 +1,29 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 21 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
+Longest valid Parentheses
+Difficulty: HardAccuracy: 26.13%Submissions: 139K+Points: 8Average Time: 20m
+Given a string s consisting of opening and closing parenthesis '(' and ')'. Find the length of the longest valid parenthesis substring.
+
+A parenthesis string is valid if:
+
+For every opening parenthesis, there is a closing parenthesis.
+The closing parenthesis must be after its opening parenthesis.
+Examples :
+
+Input: s = "((()"
+Output: 2
+Explanation: The longest valid parenthesis substring is "()".
+Input: s = ")()())"
+Output: 4
+Explanation: The longest valid parenthesis substring is "()()".
+Input: s = "())()"
+Output: 2
+Explanation: The longest valid parenthesis substring is "()".
+Constraints:
+1 ≤ s.size() ≤ 106  
+s consists of '(' and ')' only
+
+
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 21 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
