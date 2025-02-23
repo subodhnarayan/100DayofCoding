7577aaf88792f07cd7461c90d7fc78cb8fa9f95c
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-02-23 17:51:38 +0530

    Update day303.cpp

--- day303.cpp
+++ day303.cpp
@@ -25,5 +25,29 @@ Constraints:
 
 Time Complexity: O(n)Auxiliary Space: O(n)
 
+class Solution {
+  public:
+    // Function to find the next greater element for each element of the array.
+    vector<int> nextLargerElement(vector<int>& arr) {
+        int n=arr.size();
+        stack<int> st;
+        vector<int> ans(n,-1);
+        for(int i=n-1;i>=0;i--){
+            while(!st.empty() && arr[i]>=st.top()){
+                st.pop();
+            }
+            
+            if(!st.empty()){
+                ans[i]=st.top();
+            }
+            else{
+                ans[i]=-1;
+            }
+            st.push(arr[i]);
+        }
+        return ans;
+    }
+};
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 23 / 02 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
