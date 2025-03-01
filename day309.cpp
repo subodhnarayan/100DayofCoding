99b1c377c74fdf40a2ded05b3fd68ceb0398b4ca
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-01 19:11:46 +0530

    Update day309.cpp

--- day309.cpp
+++ day309.cpp
@@ -1,4 +1,28 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 01 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
+Decode the string
+Difficulty: MediumAccuracy: 44.28%Submissions: 48K+Points: 4Average Time: 10m
+Given an encoded string s, the task is to decode it. The encoding rule is :
+
+k[encodedString], where the encodedString inside the square brackets is being repeated exactly k times. Note that k is guaranteed to be a positive integer.
+Note: The test cases are generated so that the length of the output string will never exceed 105 .
+
+Examples:
+
+Input: s = "1[b]"
+Output: "b"
+Explanation: "b" is present only one time.
+Input: s = "3[b2[ca]]"
+Output: "bcacabcacabcaca"
+Explanation:
+1. Inner substring “2[ca]” breakdown into “caca”.
+2. Now, new string becomes “3[bcaca]”
+3. Similarly “3[bcaca]” becomes “bcacabcacabcaca ” which is final result.
+Constraints:
+1 ≤ |s| ≤ 105 
+1 <= k <= 100
+s contains only lowercase english alphabets
+
+
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 01 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
