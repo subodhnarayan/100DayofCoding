1cdc88185b4b3223ce676a8160e2b740aeb66819
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-02 22:24:10 +0530

    Update day310.cpp

--- day310.cpp
+++ day310.cpp
@@ -37,5 +37,32 @@ Constraints:
 0 ≤ arr[i] ≤ 109
 
 
+class Solution {
+    public:
+      vector<int> maxOfSubarrays(vector<int>& arr, int k) {
+          // code here
+          int n = arr.size();
+          int i = 0, j = 1, maxi = 0;
+          vector<int> ans;
+          
+          while(i < k) {
+              if(arr[i] > maxi) maxi = arr[i];
+              i++;
+          }
+          ans.push_back(maxi);
+          
+          while(i < n) {
+              if(arr[j-1]==maxi)
+                  maxi = *max_element(arr.begin()+j,arr.begin()+i+1);
+              else if(arr[i]>maxi) 
+                  maxi = arr[i];
+              ans.push_back(maxi);
+              i++; j++;
+          }
+          
+          return ans;
+      }
+  };
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 02 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
