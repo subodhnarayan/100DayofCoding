a2a5e05d7370bf57f249db14ca7bffaaf6f21c4e
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-03 22:23:00 +0530

    Update day311.cpp

--- day311.cpp
+++ day311.cpp
@@ -95,3 +95,31 @@ Constraints:
 1 <= nums.length <= 105
 -106 <= nums[i] <= 106
 pivot equals to an element of nums.
+
+
+class Solution {
+    public:
+        vector<int> pivotArray(vector<int>& nums, int pivot) {
+            vector<int> result(nums.size(), 0);
+            int left = 0, right = nums.size() - 1;
+            
+            for (int i = 0, j = nums.size() - 1; i < nums.size(); ++i, --j) {
+                if (nums[i] < pivot) {
+                    result[left] = nums[i];
+                    left++;
+                }
+                
+                if (nums[j] > pivot) {
+                    result[right] = nums[j];
+                    right--;
+                }
+            }
+            
+            while (left <= right) {
+                result[left] = pivot;
+                left++;
+            }
+            
+            return result;
+        }
+};
