852aa22e8090c2b5c7e0bd63b14abd83dfbf2935
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-04 22:26:48 +0530

    Update day312.cpp

--- day312.cpp
+++ day312.cpp
@@ -23,5 +23,19 @@ Constraints:
 
 Time Complexity: O(n log n)Auxiliary Space: O(n)
 
+class Solution {
+    public:
+      int lis(vector<int>& arr) {
+          // code here
+          vector<int> ans;
+          ans.push_back(arr[0]);
+          for(int i=1;i<arr.size();i++){
+              if(arr[i]>ans.back())ans.push_back(arr[i]);
+              else ans[lower_bound(ans.begin(),ans.end(),arr[i])-ans.begin()]=arr[i];
+          }
+          return ans.size();
+      }
+  };
+
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 04 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
