00665b4a2af4e6c739e8dbc00a3df993bd4c85ab
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-05 22:51:53 +0530

    Update day313.cpp

--- day313.cpp
+++ day313.cpp
@@ -87,3 +87,18 @@ Explanation: After 2 minutes, there are 4 colored cells on the boundary and 1 in
 Constraints:
 
 1 <= n <= 105
+
+
+class Solution {
+    public:
+        long long coloredCells(int n) {
+            if(n==1) return 1;
+            long long temp=4;
+            long long ans=1;
+            for(int i=2;i<=n;i++){
+                ans=ans+temp;
+                temp=temp+4;
+            }
+            return ans;
+        }
+};
