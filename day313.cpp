daced40e6e05ea34bb50e6733d5a808ec82d1d60
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-05 22:51:00 +0530

    Update day313.cpp

--- day313.cpp
+++ day313.cpp
@@ -26,6 +26,32 @@ Constraint:
  words[i] only consists of lowercase English letters.
 
 
+ class Solution {
+    public:
+      static bool comp(string &s1,string &s2){
+          return s1.size()<s2.size();
+      }
+      int longestStringChain(vector<string>& words) {
+          // Code here
+          sort(words.begin(),words.end(),comp);
+          unordered_map<string,int> mp;
+          int ans=1;
+          for(string &w:words){
+              int length=1;
+              for(int i=0;i<w.size();i++){
+                  string pred=w.substr(0,i)+w.substr(i+1);
+                  if(mp.find(pred)!=mp.end()){
+                      length=max(length,mp[pred]+1);
+                  }
+              }
+              mp[w]=length;
+              ans=max(ans,length);
+          }
+          return ans;
+      }
+  };
+
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 05 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -- -- -
 
 
