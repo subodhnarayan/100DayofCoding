b9e2e73a37063c4e2d85261957236510248e8bb4
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-10 23:07:07 +0530

    Update day318.cpp

--- day318.cpp
+++ day318.cpp
@@ -22,4 +22,27 @@ Constraints:
 1 ≤ s1.length(), s2.length() ≤ 103
 Both the strings are in lowercase.
 
+
+class Solution {
+    public:
+      // Function to compute the edit distance between two strings
+       int dp[1005][1005];
+      int fn(string& s1, string& s2, int i , int j){
+          if(j==s2.size())return s1.size()-i;
+          if(i==s1.size())return s2.size()-j;
+          if(dp[i][j]!=-1)return dp[i][j];
+          int ans=1e5;
+          if(s1[i]==s2[j])ans=min(ans,fn(s1,s2,i+1,j+1));
+          ans=min(ans,fn(s1,s2,i+1,j)+1);
+          ans=min(ans,fn(s1,s2,i,j+1)+1);
+          ans=min(ans,fn(s1,s2,i+1,j+1)+1);
+          return dp[i][j]=ans;
+      }
+      int editDistance(string& s1, string& s2) {
+          // code here
+          memset(dp,-1,sizeof(dp));
+         return fn(s1,s2,0,0);
+      }
+  };
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 10 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
