4b249e1b4ed4bea9e3852aa006cbee9b39b3e6ca
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-11 23:24:01 +0530

    Update day319.cpp

--- day319.cpp
+++ day319.cpp
@@ -37,3 +37,35 @@ class Solution {
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 11 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
+1358. Number of Substrings Containing All Three Characters
+Solved
+Medium
+Topics
+Companies
+Hint
+Given a string s consisting only of characters a, b and c.
+
+Return the number of substrings containing at least one occurrence of all these characters a, b and c.
+
+ 
+
+Example 1:
+
+Input: s = "abcabc"
+Output: 10
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "abc", "abca", "abcab", "abcabc", "bca", "bcab", "bcabc", "cab", "cabc" and "abc" (again). 
+Example 2:
+
+Input: s = "aaacb"
+Output: 3
+Explanation: The substrings containing at least one occurrence of the characters a, b and c are "aaacb", "aacb" and "acb". 
+Example 3:
+
+Input: s = "abc"
+Output: 1
+ 
+
+Constraints:
+
+3 <= s.length <= 5 x 10^4
+s only consists of a, b or c characters.
