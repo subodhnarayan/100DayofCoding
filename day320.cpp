9fc7ea5e66fffd002afdda5c9d6b73c02a74e50a
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-12 23:42:32 +0530

    Update day320.cpp

--- day320.cpp
+++ day320.cpp
@@ -21,5 +21,27 @@ Constraints:
 2 ≤ cost.size() ≤ 105
 0 ≤ cost[i] ≤ 999
 
+class Solution {
+    public:
+    int helper(int i,int n,vector<int>&cost,vector<int>&dp){
+          if(i>=n) return 0;
+          //pay and take the one step
+          if(dp[i]!=-1) return dp[i];
+          int onestep=cost[i]+helper(i+1,n,cost,dp);
+          //pay and take two step
+          int twostep=cost[i]+helper(i+2,n,cost,dp);
+          
+          return dp[i]=min(onestep,twostep);
+          
+      }
+      int minCostClimbingStairs(vector<int>& cost) {
+          // Write your code here
+          int n=cost.size();
+          vector<int>dp(n,-1);
+          //in question is given that the it can either start from 0 index or from 1 index
+          return min(helper(0,n,cost,dp),helper(1,n,cost,dp));
+      }
+  };
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 12 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
