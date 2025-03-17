cb9447ab2a1febcff5830d44b213580677cf3d41
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-17 23:54:49 +0530

    Update day325.cpp

--- day325.cpp
+++ day325.cpp
@@ -21,6 +21,33 @@ Constraints:
 1<= arr[i] <= 200
 1<= sum <= 104
 
+class Solution {
+    public:
+      bool isSubsetSum(vector<int>& arr, int sum) {
+          // code here
+         int n=arr.size();
+          vector<bool> prev(sum+1,false);
+          vector<bool> curr(sum+1,false);
+          prev[0]=true;
+          for(int ind=1;ind<=n;ind++){
+              for(int s=0;s<=sum;s++){
+                  if(s==0){
+                      curr[s]=true;
+                      continue;
+                  }
+                  bool notTake=prev[s];
+                  bool take=false;
+                  if(s>=arr[ind-1]) take=prev[s-arr[ind-1]];
+                  bool ans=take|notTake;
+                  curr[s]=ans;
+              }
+              prev=curr;
+          }
+          return prev[sum];
+          
+      }
+};
+
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 17 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
 
