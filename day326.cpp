ac43e3bf70f5f8066c5fb57e51bd2aaeacd6fd8b
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-18 22:45:36 +0530

    Update day326.cpp

--- day326.cpp
+++ day326.cpp
@@ -101,3 +101,22 @@ Constraints:
 
 1 <= nums.length <= 105
 1 <= nums[i] <= 109
+
+class Solution {
+    public:
+        int longestNiceSubarray(vector<int>& nums) {
+            int l = 0, usedBits = 0, maxLength = 0;
+    
+            for (int r = 0; r < nums.size(); r++) {
+                while ((usedBits & nums[r]) != 0) {
+                    usedBits ^= nums[l];
+                    l++;
+                }
+    
+                usedBits |= nums[r];
+                maxLength = max(maxLength, r - l + 1);
+            }
+    
+            return maxLength;
+        }
+    };
