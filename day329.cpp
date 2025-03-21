802d9033b239469b9f7cc494e57735e0ec8d2539
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-21 23:04:30 +0530

    Update day329.cpp

--- day329.cpp
+++ day329.cpp
@@ -24,6 +24,35 @@ Constraints:
 Time Complexity: O(n)Auxiliary Space: O(1)
 
 
+class Solution {
+    public:
+      int findMaxSum(vector<int>& arr) {
+          // code here
+      int n=arr.size();
+      if (n == 0) {
+          return 0;
+      } else if (n == 1) {
+          return arr[0];
+      }
+  
+      // Create an array to store the maximum loot up to the i-th house
+      int dp[n];
+  
+      // Base cases
+      dp[0] = arr[0];
+      dp[1] = max(arr[0], arr[1]);
+  
+      // Calculate the maximum loot for each house
+      for (int i = 2; i < n; i++) {
+          dp[i] = max(dp[i - 1], dp[i - 2] + arr[i]);
+      }
+  
+      // The last element of dp[] will contain the maximum loot
+      return dp[n - 1];
+      }
+  };
+
+
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 21 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
