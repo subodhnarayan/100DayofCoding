1f3728ff33c93d9b561c7d42c711f862553bfdeb
subodhnarayan <88626001+subodhnarayan@users.noreply.github.com>
2025-03-22 22:26:47 +0530

    Update day330.cpp

--- day330.cpp
+++ day330.cpp
@@ -22,6 +22,28 @@ Constraints:
 2 ≤ arr.size() ≤ 105
 0 ≤ arr[i] ≤ 104
 
+class Solution {
+  public:
+   int solve(int idx, vector<int>& arr){
+        int n = arr.size();
+        int prev2 = 0, prev1 = arr[idx];
+        
+        for(int i = idx+1; i < n; ++i){
+            int curr = max(arr[i]+prev2, prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        return prev1;
+    }
+    int maxValue(vector<int>& arr) {
+        // your code here
+        int includeLastHouse = solve(1, arr);
+        arr.pop_back();
+        int includeFirstHouse = solve(0, arr);
+        return max(includeFirstHouse, includeLastHouse);
+    }
+};
+
 
 
 
