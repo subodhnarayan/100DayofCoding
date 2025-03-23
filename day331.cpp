c20b9c0238e22e0337abbfc46805000ef585be73
ExploreAI351 <your.email@example.com>
2025-03-23 23:03:59 +0530

    Update day331.cpp

--- day331.cpp
+++ day331.cpp
@@ -25,6 +25,28 @@ Explanation: "05" cannot be mapped to "E" because of the leading zero ("5" is di
 Constraints:
 1 ≤ digits.size() ≤ 103
 
+class Solution {
+    public:
+      int mod=1e9+7;
+      int func(string &str,int n,vector<int> &dp){
+          if(n==0 || n==1) return 1;
+          if(dp[n]!=-1) return dp[n];
+          int cnt=0;
+          if(str[n-1] >= '1') cnt = cnt + func(str,n-1,dp)%mod;
+          if(str[n-2]== '1' || str[n-2]== '2' && str[n-1] <='6') cnt=(cnt+func(str,n-2,dp)%mod)%mod;
+          return dp[n]=cnt%mod;
+         
+      }
+      int countWays(string &str) {
+          
+          int n=str.size();
+          if(str[0]=='0') return 0;
+          vector<int> dp(n+1,-1);
+          
+          return func(str,n,dp);
+      }
+  };
+
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 23 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
