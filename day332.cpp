e3df7c857a668865674ec479550d1f65e0af5d71
ExploreAI351 <your.email@example.com>
2025-03-24 22:33:00 +0530

    Update day332.cpp

--- day332.cpp
+++ day332.cpp
@@ -97,3 +97,25 @@ Constraints:
 meetings[i].length == 2
 1 <= meetings[i][0] <= meetings[i][1] <= days
 
+class Solution {
+    public:
+        int countDays(int days, vector<vector<int>>& meetings) {
+             sort(meetings.begin(), meetings.end());
+    
+            vector<vector<int>> mergedMeetings;
+            for (const auto& meeting : meetings) {
+                if (mergedMeetings.empty() || meeting[0] > mergedMeetings.back()[1]) {
+                    mergedMeetings.push_back(meeting);
+                } else {
+                    mergedMeetings.back()[1] = max(mergedMeetings.back()[1], meeting[1]);
+                }
+            }
+    
+            int meetingDaysCount = 0;
+            for (const auto& m : mergedMeetings) {
+                meetingDaysCount += (m[1] - m[0] + 1);
+            }
+    
+            return days - meetingDaysCount;
+        }
+};
