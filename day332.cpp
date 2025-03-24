a1e5e63807bd3d561e879c37b8d9344effb86cf7
ExploreAI351 <your.email@example.com>
2025-03-24 22:32:04 +0530

    Update day332.cpp

--- day332.cpp
+++ day332.cpp
@@ -22,7 +22,23 @@ Constraints:
 2 ≤ arr.size() ≤ 100
 1 ≤ arr[i] ≤ 200
 
-
+class Solution {
+    public:
+      vector<vector<int>>dp;
+      int answer(int start,int end,vector<int> &arr){
+          if(end<=start) return 0;
+          if(dp[start][end]!=INT_MAX) return dp[start][end];
+          int ans=INT_MAX;
+          for(int i=start;i<end;i++)  ans=min(ans, answer(start,i,arr) + answer(i+1,end,arr) + arr[start]*arr[i+1]*arr[end+1]);
+          return dp[start][end]=ans;
+      }
+      int matrixMultiplication(vector<int> &arr) {
+          // code here
+         int n=arr.size();
+         dp.assign(n,vector<int>(n,INT_MAX));
+         return answer(0,n-2,arr);
+      }
+  };
 
 
 
