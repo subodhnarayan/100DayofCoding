942af0fec3bfc2d712f1eed531f02f09f9b13b4a
ExploreAI351 <your.email@example.com>
2025-03-26 23:09:42 +0530

    Update day334.cpp

--- day334.cpp
+++ day334.cpp
@@ -22,7 +22,31 @@ Constraints:
 1 ≤ dictionary.size() ≤ 1000
 1 ≤ dictionary[i].size() ≤ 100
 
-
+class Solution {
+    public:
+      bool wordBreak(string &s, vector<string> &dic) {
+          // code here
+          int n=s.size(),m=dic.size();
+          vector<int>dp(n+1,0);
+          dp[n]=1;
+          for(int i=n-1;i>=0;i--){
+              for(int j=m-1;j>=0;j--){
+                  int k=0;
+                  for(int l=i;l<n&&k<dic[j].size();l++){
+                      if(s[l]==dic[j][k]){
+                          k++;
+                      }else{
+                          break;
+                      }
+                  }
+                  if(k==dic[j].size()){
+                      dp[i]|=dp[i+k];
+                  }
+              }
+          }
+          return dp[0];
+      }
+  };
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 26 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
