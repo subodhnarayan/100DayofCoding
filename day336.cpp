c8dab35d63ca8db9299c196fa2f3fb3b3a727970
ExploreAI351 <your.email@example.com>
2025-03-28 21:13:19 +0530

    Update day336.cpp

--- day336.cpp
+++ day336.cpp
@@ -19,6 +19,31 @@ Constraints:
 1 ≤ start.size() = finish.size() ≤ 2*105
 1 ≤ start[i] ≤ finish[i] ≤ 109
 
+class Solution {
+    public:
+      int activitySelection(vector<int> &start, vector<int> &finish) {
+          // code here
+          vector<vector<int>>vec;
+          for(int i=0;i<start.size();i++){
+              vec.push_back({finish[i], start[i]});
+          }
+          sort(vec.begin(), vec.end());
+          
+          //int ans=1;
+          int end=vec[0][0];
+          int maxi=1;
+          for(int i=1;i<vec.size();i++){
+              if(vec[i][1]>end){
+                  maxi++;
+                  end=vec[i][0];
+              }
+              
+  
+          }
+          return maxi;
+      }
+  };
+
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 28 / 03 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
