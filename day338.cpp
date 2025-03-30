d842581fa99b68040491fd8a46ca637ede944687
ExploreAI351 <your.email@example.com>
2025-03-30 22:51:02 +0530

    Update day338.cpp

--- day338.cpp
+++ day338.cpp
@@ -94,3 +94,25 @@ Constraints:
 1 <= s.length <= 500
 s consists of lowercase English letters.
 
+class Solution {
+public:
+    vector<int> partitionLabels(string s) {
+         unordered_map<char, int> last_occurrence;
+        for (int i = 0; i < s.size(); i++) {
+            last_occurrence[s[i]] = i;
+        }
+
+        vector<int> result;
+        int start = 0, end = 0;
+
+        for (int i = 0; i < s.size(); i++) {
+            end = max(end, last_occurrence[s[i]]);
+            if (i == end) {
+                result.push_back(end - start + 1);
+                start = i + 1;
+            }
+        }
+
+        return result;
+    }
+};
