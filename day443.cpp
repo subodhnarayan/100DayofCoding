5371bbac54fc0e8044ff19805e81db3573a07b09
ExploreAI351 <your.email@example.com>
2025-04-04 22:38:50 +0530

    Update day443.cpp

--- day443.cpp
+++ day443.cpp
@@ -22,6 +22,37 @@ Constraints:
 
 Time Complexity: O(V + E)Auxiliary Space: O(V)
 
+class Solution {
+    public:
+      bool isCycle(int V, vector<vector<int>>& edges) {
+          // Code here
+           vector<vector<int>> adj(V);
+          vector<bool> visited(V, false);        
+          visited[0] = true;
+          
+          for(auto &i : edges) {
+              int u = i[0], v = i[1];
+              adj[u].push_back(v);
+              adj[v].push_back(u);
+          }
+              
+          function<bool(int, int)> dfs = [&](int curr, int prev) -> bool {
+              bool res = false;
+              for(auto i : adj[curr]) {
+                  if(i != prev && visited[i] == true) return true;
+                  else if(i != prev) {
+                      visited[i] = true;
+                      res |= dfs(i, curr);
+                      if(res) return res;
+                  }
+              }
+              return res;
+          };
+          
+          return dfs(0, -1);
+      }
+  };
+
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 04 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
