b53858bad8dca0a0c03746098ba1a2e32109ae50
ExploreAI351 <your.email@example.com>
2025-04-04 22:38:02 +0530

    Update day443.cpp

--- day443.cpp
+++ day443.cpp
@@ -1,5 +1,26 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 04 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
+Undirected Graph Cycle
+Difficulty: MediumAccuracy: 30.13%Submissions: 548K+Points: 4Average Time: 20m
+Given an undirected graph with V vertices and E edges, represented as a 2D vector edges[][], where each entry edges[i] = [u, v] denotes an edge between vertices u and v, determine whether the graph contains a cycle or not.
+
+Examples:
+
+Input: V = 4, E = 4, edges[][] = [[0, 1], [0, 2], [1, 2], [2, 3]]
+Output: true
+Explanation: 
+ 
+1 -> 2 -> 0 -> 1 is a cycle.
+Input: V = 4, E = 3, edges[][] = [[0, 1], [1, 2], [2, 3]]
+Output: false
+Explanation: 
+ 
+No cycle in the graph.
+Constraints:
+1 ≤ V ≤ 105
+1 ≤ E = edges.size() ≤ 105
+
+
 
 
 
