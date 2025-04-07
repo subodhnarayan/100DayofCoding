9d036c1ac5a47a1717d0a58c8e72e957f73cba08
ExploreAI351 <your.email@example.com>
2025-04-07 23:37:39 +0530

    Update day446.cpp

--- day446.cpp
+++ day446.cpp
@@ -1,5 +1,27 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 07 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
+Directed Graph Cycle
+Difficulty: MediumAccuracy: 27.88%Submissions: 467K+Points: 4
+Given a Directed Graph with V vertices (Numbered from 0 to V-1) and E edges, check whether it contains any cycle or not.
+The graph is represented as a 2D vector edges[][], where each entry edges[i] = [u, v] denotes an edge from verticex u to v.
+
+Examples:
+
+Input: V = 4, edges[][] = [[0, 1], [1, 2], [2, 3], [3, 3]]
+
+
+
+Output: true
+Explanation: 3 -> 3 is a cycle
+Input: V = 3, edges[][] = [[0, 1], [1, 2]]
+
+
+
+Output: false
+Explanation: no cycle in the graph
+Constraints:
+1 ≤ V, E ≤ 105
+
 
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 07 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
