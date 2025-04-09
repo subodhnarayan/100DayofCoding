b57217a4471868c31338ac4b2253ae717168c695
ExploreAI351 <your.email@example.com>
2025-04-09 23:46:48 +0530

    Update day468.cpp

--- day468.cpp
+++ day468.cpp
@@ -143,3 +143,19 @@ Constraints:
 1 <= nums[i] <= 100
 1 <= k <= 100
 
+
+class Solution {
+    public:
+        int minOperations(vector<int>& nums, int k) {
+             unordered_set<int> st;
+            for (int x : nums) {
+                if (x < k) {
+                    return -1;
+                } else if (x > k) {
+                    st.insert(x);
+                }
+            }
+            return st.size();
+        }
+    };
+
