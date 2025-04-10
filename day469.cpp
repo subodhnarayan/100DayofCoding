88863736805f8810a24de2a555088205e74fd27c
ExploreAI351 <your.email@example.com>
2025-04-10 23:45:34 +0530

    Update day469.cpp

--- day469.cpp
+++ day469.cpp
@@ -29,6 +29,41 @@ Constraint:
 1 ≤ n ≤ 103
 0 ≤ houses[i][j] ≤ 103
 
+class Solution {
+    public:
+      int minCost(vector<vector<int>>& houses) {
+          // code here
+            // code here
+          int n = houses.size();
+          
+          priority_queue<pair<int,int>, vector<pair<int,int>>, greater<pair<int,int>>> pq;
+          
+          vector<int> vis(n, 0);
+          pq.push({0, 0});
+          int sum =0; 
+          
+          while(!pq.empty()){
+              auto node = pq.top();
+              pq.pop();
+              int wt = node.first;
+              int ind = node.second;
+              if (vis[ind]) continue;
+      
+              vis[ind] = 1;
+              sum += wt;
+      
+              for (int i = 0; i < n; i++) {
+                  if (!vis[i]) {
+                      int newWt = abs(houses[ind][0] - houses[i][0]) + abs(houses[ind][1] - houses[i][1]);
+                      pq.push({newWt, i});
+                  }
+              }
+          }
+          
+          return sum;
+      }
+  };
+
 
 -- -- -- -- -- -- -- -- -LeetCode - POTD - 10 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
