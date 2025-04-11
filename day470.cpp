e9b96fa80e944691f786a5df5787036141e2df5f
ExploreAI351 <your.email@example.com>
2025-04-11 23:53:38 +0530

    Update day470.cpp

--- day470.cpp
+++ day470.cpp
@@ -35,3 +35,27 @@ Explanation: There are 4 symmetric integers between 1200 and 1230: 1203, 1212, 1
 Constraints:
 
 1 <= low <= high <= 104
+
+
+
+
+class Solution {
+    public:
+        int countSymmetricIntegers(int low, int high) {
+             int res = 0;
+            for (int a = low; a <= high; ++a) {
+                if (a < 100 && a % 11 == 0) {
+                    res++;
+                } else if (1000 <= a && a < 10000) {
+                    int left = a / 1000 + (a % 1000) / 100;
+                    int right = (a % 100) / 10 + a % 10;
+                    if (left == right) {
+                        res++;
+                    }
+                }
+            }
+            return res;
+        }
+    };
+    
+    
