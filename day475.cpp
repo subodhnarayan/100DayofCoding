da564a9ab7c703278e28cee6fab91c8045af9cc8
ExploreAI351 <your.email@example.com>
2025-04-16 23:53:22 +0530

    Update day475.cpp

--- day475.cpp
+++ day475.cpp
@@ -31,3 +31,22 @@ Expected Complexities
 Time Complexity: O(n^3)
 Auxiliary Space: O(1)
 
+class Solution {
+    public:
+      void floydWarshall(vector<vector<int>> &dist) {
+          // Code here
+             int n = dist.size();
+          const int INF = 1e8;
+  
+          for (int k = 0; k < n; ++k) {
+              for (int i = 0; i < n; ++i) {
+                  for (int j = 0; j < n; ++j) {
+                      if (dist[i][k] < INF && dist[k][j] < INF) {
+                          dist[i][j] = min(dist[i][j], dist[i][k] + dist[k][j]);
+                      }
+                  }
+              }
+          }
+      }
+  };
+  
