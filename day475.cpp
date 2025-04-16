d00f846179b1f8c41050abc021afa131dbf53fd3
ExploreAI351 <your.email@example.com>
2025-04-16 23:55:08 +0530

    Update day475.cpp

--- day475.cpp
+++ day475.cpp
@@ -88,3 +88,26 @@ Constraints:
 
 1 <= nums.length <= 105
 1 <= nums[i], k <= 109
+
+class Solution {
+    public:
+        long long countGood(vector<int>& nums, int k) {
+            int n = nums.size();
+            int same = 0, right = -1;
+            unordered_map<int, int> cnt;
+            long long ans = 0;
+            for (int left = 0; left < n; ++left) {
+                while (same < k && right + 1 < n) {
+                    ++right;
+                    same += cnt[nums[right]];
+                    ++cnt[nums[right]];
+                }
+                if (same >= k) {
+                    ans += n - right;
+                }
+                --cnt[nums[left]];
+                same -= cnt[nums[left]];
+            }
+            return ans;
+        }
+};
