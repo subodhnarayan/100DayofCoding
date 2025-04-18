77c24c1c7703d91606d77b460bac014d8ef23e0f
ExploreAI351 <your.email@example.com>
2025-04-18 22:25:30 +0530

    Update day477.cpp

--- day477.cpp
+++ day477.cpp
@@ -19,5 +19,68 @@ Constraints:
 1 ≤ query.size() ≤ 104
 1 ≤ word.size() ≤ 103
 
+class Node{
+    public:
+    Node* children[26];
+    bool flag; // end of word flag
+    
+    Node(){
+        flag = false;
+        for(int i=0;i<26; i++){
+            children[i] = nullptr;
+        }
+    }
+};
+class Trie {
+    Node* root;
+  public:
+
+    Trie() {
+        // implement Trie
+         root = new Node();
+    }
+
+    void insert(string &word) {
+        // insert word into Trie
+         Node* node = root;
+        for(char c: word){
+            int index = c - 'a';
+            if(node->children[index] == nullptr){
+                node->children[index] = new Node();
+            }
+            node = node->children[index];
+        }
+        node->flag = true;
+    }
+
+    bool search(string &word) {
+        // search word in the Trie
+        Node* curr = root;
+        for(char ch:word){
+            int index = ch-'a';
+            if(curr->children[index] == nullptr){
+                return false;
+            }        
+            curr = curr->children[index];
+        }
+        
+        return curr->flag;
+    }
+
+    bool isPrefix(string &word) {
+        // search prefix word in the Trie
+         Node* curr = root;
+        for(char ch:word){
+            int index = ch-'a';
+            if(curr->children[index] == nullptr){
+                return false;
+            }        
+            curr = curr->children[index];
+        }
+        
+        return true;
+    }
+};
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 18 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
