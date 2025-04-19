48e71cd51d4567e1e1b336fb5f669188eaf45b31
ExploreAI351 <your.email@example.com>
2025-04-19 22:42:40 +0530

    Update day478.cpp

--- day478.cpp
+++ day478.cpp
@@ -1,4 +1,20 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 19 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
+Maximum XOR of two numbers in an array
+Difficulty: MediumAccuracy: 50.0%Submissions: 19K+Points: 4
+Given an array arr[] of non-negative integers of size n. Find the maximum possible XOR between two numbers present in the array.
+
+Examples:
+
+Input: arr[] = [25, 10, 2, 8, 5, 3]
+Output: 28
+Explanation: The maximum possible XOR is 5 ^ 25 = 28.
+Input: arr[] = [1, 2, 3, 4, 5, 6, 7]
+Output: 7
+Explanation : The maximum possible XOR is 1 ^ 6 = 7.
+Constraints:
+2 ≤ arr.size() ≤ 5*104
+1 ≤ arr[i] ≤ 106
+
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 19 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
