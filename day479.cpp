f5caa00974ed5b64bde7bb530a14299e48e81e89
ExploreAI351 <your.email@example.com>
2025-04-20 23:22:49 +0530

    Update day479.cpp

--- day479.cpp
+++ day479.cpp
@@ -1,5 +1,30 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 20 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
+Find Only Repetitive Element from 1 to n-1
+Difficulty: EasyAccuracy: 59.22%Submissions: 13K+Points: 2
+Given an array arr[] of size n, filled with numbers from 1 to n-1 in random order. The array has only one repetitive element. Your task is to find the repetitive element.
+
+Note: It is guaranteed that there is a repeating element present in the array.
+
+Examples:
+
+Input: arr[] = [1, 3, 2, 3, 4]
+Output: 3 
+Explanation: The number 3 is the only repeating element.
+Input: arr[] = [1, 5, 1, 2, 3, 4]
+Output: 1  
+Explanation: The number 1 is the only repeating element.
+Input: arr[] = [1, 1]  
+Output: 1
+Explanation: The array is of size 2 with both elements being 1, making 1 the repeating element.
+Constraints:
+2 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ n-1 
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(1)
+
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 20 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
