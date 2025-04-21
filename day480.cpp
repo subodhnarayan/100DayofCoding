c745dfbf1b30355d9149aa0055794963ab6e3c44
ExploreAI351 <your.email@example.com>
2025-04-21 23:49:28 +0530

    Update day480.cpp

--- day480.cpp
+++ day480.cpp
@@ -89,3 +89,20 @@ n == differences.length
 1 <= n <= 105
 -105 <= differences[i] <= 105
 -105 <= lower <= upper <= 105
+
+
+class Solution {
+    public:
+        int numberOfArrays(vector<int>& differences, int lower, int upper)   {
+             int x = 0, y = 0, cur = 0;
+            for (int d : differences) {
+                cur += d;
+                x = min(x, cur);
+                y = max(y, cur);
+                if (y - x > upper - lower) {
+                    return 0;
+                }
+            }
+            return (upper - lower) - (y - x) + 1;
+        }
+    };
