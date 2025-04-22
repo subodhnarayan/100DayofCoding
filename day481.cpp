f67445c8a877dcc8b5ec2e4cf1dda51001134199
ExploreAI351 <your.email@example.com>
2025-04-22 22:07:24 +0530

    Update day481.cpp

--- day481.cpp
+++ day481.cpp
@@ -20,3 +20,14 @@ Expected Complexities
 Time Complexity: O(n)
 Auxiliary Space: O(1)
 
+class Solution {
+    public:
+      int findUnique(vector<int> &arr) {
+          // code here
+          int ans=0;
+          for(int i=0;i<arr.size();i++){
+              ans^=arr[i];
+          }
+          return ans;
+      }
+  };
