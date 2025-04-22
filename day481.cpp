ddc0c62d9349562fcbfd08dd56cc946ca3d5e975
ExploreAI351 <your.email@example.com>
2025-04-22 22:06:25 +0530

    Create day481.cpp

---
+++ day481.cpp
@@ -0,0 +1,21 @@
+
+
+Unique Number I
+Difficulty: EasyAccuracy: 62.05%Submissions: 12K+Points: 2
+Given a unsorted array arr[] of positive integers having all the numbers occurring exactly twice, except for one number which will occur only once. Find the number occurring only once.
+
+Examples :
+
+Input: arr[] = [1, 2, 1, 5, 5]
+Output: 2
+Explanation: Since 2 occurs once, while other numbers occur twice, 2 is the answer.
+Input: arr[] = [2, 30, 2, 15, 20, 30, 15]
+Output: 20
+Explanation: Since 20 occurs once, while other numbers occur twice, 20 is the answer.
+Constraints
+1 ≤  arr.size()  ≤ 106
+0 ≤ arr[i] ≤ 109
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(1)
