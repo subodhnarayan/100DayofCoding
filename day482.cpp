7527fa5473cc894aa4c1a3afd1e5b86f5319d4bf
ExploreAI351 <your.email@example.com>
2025-04-23 23:28:18 +0530

    Update day482.cpp

--- day482.cpp
+++ day482.cpp
@@ -1,6 +1,28 @@
--- -- -- -- -- -- -- -- -GFG - POTD - 23 / 04 / 2025 -- -- -- -- -- -- -- -- -- -- -- -- -- -------
 
+-- -- -- -- -- -- -- -- -Leetcode - POTD - 23 / 04 / 2025 -----------------------------------
+Unique Number II
+Difficulty: MediumAccuracy: 36.9%Submissions: 165K+Points: 4Average Time: 15m
+Given an array arr[] containing 2*n + 2 positive numbers, out of which 2*n numbers exist in pairs whereas only two number occur exactly once and are distinct. Find the other two numbers. Return the answer in increasing order.
 
+Examples:
+
+Input: arr[] = [1, 2, 3, 2, 1, 4]
+Output: [3, 4] 
+Explanation: 3 and 4 occur exactly once.
+Input: arr[] = [2, 1, 3, 2]
+Output: [1, 3]
+Explanation: 1 and 3 occur exactly once.
+Input: arr[] = [2, 1, 3, 3]
+Output: [1, 2]
+Explanation: 1 and 2 occur exactly once.
+Constraints:
+2 ≤ arr.size() ≤ 106 
+1 ≤ arr[i] ≤ 5 * 106
+arr.size() is even
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(1)
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 23 / 04 / 2025 -----------------------------------
