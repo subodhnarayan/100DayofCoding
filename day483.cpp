e03cde005e6ca3bf38e5d411c176913e1c56198b
ExploreAI351 <your.email@example.com>
2025-04-24 22:53:55 +0530

    Update day483.cpp

--- day483.cpp
+++ day483.cpp
@@ -1,5 +1,27 @@
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 24 / 04 / 2025 -----------------------------------
+Unique Number III
+Difficulty: MediumAccuracy: 49.87%Submissions: 60K+Points: 4Average Time: 20m
+Given an array of integers arr[] where, every element appears thrice except for one which occurs once.
+Find that element which occurs once.
+
+Examples:
+
+Input: arr[] = [1, 10, 1, 1]
+Output: 10
+Explanation: 10 occurs once in the array while the other element 1 occurs thrice.
+Input: arr[] = [3, 2, 1, 34, 34, 1, 2, 34, 2, 1]
+Output: 3
+Explanation: All elements except 3 occurs thrice in the array.
+Constraints:
+1 ≤ arr.size() ≤ 105
+arr.size() % 3 = 1
+-109 ≤ arr[i] ≤ 109
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(1)
+
 
 
 
