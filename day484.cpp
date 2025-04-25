054646ef4e2e3caf44a26d861a517413dab52455
ExploreAI351 <your.email@example.com>
2025-04-26 00:00:37 +0530

    Update day484.cpp

--- day484.cpp
+++ day484.cpp
@@ -61,6 +61,23 @@ Constraints:
 1 <= modulo <= 109
 0 <= k < modulo
 
+class Solution {
+    public:
+        long long countInterestingSubarrays(vector<int>& nums, int modulo, int k) {
+            int n = nums.size();
+            unordered_map<int, int> cnt;
+            long long res = 0;
+            int prefix = 0;
+            cnt[0] = 1;
+            for (int i = 0; i < n; i++) {
+                prefix += nums[i] % modulo == k;
+                res += cnt[(prefix - k + modulo) % modulo];
+                cnt[prefix % modulo]++;
+            }
+            return res;
+        }
+    };
+
 
 
 
