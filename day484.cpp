b2330bd0aba0ad4eef8d2b264f1e3e4159022637
ExploreAI351 <your.email@example.com>
2025-04-25 23:59:16 +0530

    Update day484.cpp

--- day484.cpp
+++ day484.cpp
@@ -1,6 +1,29 @@
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 25 / 04 / 2025 -----------------------------------
+Majority Element
+Difficulty: MediumAccuracy: 27.82%Submissions: 714K+Points: 4Average Time: 59m
+Given an array arr[]. Find the majority element in the array. If no majority element exists, return -1.
 
+Note: A majority element in an array is an element that appears strictly more than arr.size()/2 times in the array.
+
+Examples:
+
+Input: arr[] = [1, 1, 2, 1, 3, 5, 1]
+Output: 1
+Explanation: Since, 1 is present more than 7/2 times, so it is the majority element.
+Input: arr[] = [7]
+Output: 7
+Explanation: Since, 7 is single element and present more than 1/2 times, so it is the majority element.
+Input: arr[] = [2, 13]
+Output: -1
+Explanation: Since, no element is present more than 2/2 times, so there is no majority element.
+Constraints:
+1 ≤ arr.size() ≤ 105
+0 ≤ arr[i] ≤ 105
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(1)
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 25 / 04 / 2025 -----------------------------------
