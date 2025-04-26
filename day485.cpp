e207135a0957d1368273c1f492d2e433f4c05ce9
ExploreAI351 <your.email@example.com>
2025-04-26 23:22:30 +0530

    Update day485.cpp

--- day485.cpp
+++ day485.cpp
@@ -26,6 +26,33 @@ Expected Complexities
 Time Complexity: O(n)
 Auxiliary Space: O(n)
 
+
+class Solution {
+    public:
+      bool isHeap(Node* tree) {
+          // code here
+          if(tree==nullptr) return true;
+          queue<Node*> q;
+          Node *root = tree;
+          q.push(root);
+          bool nullflag = false;
+          while(!q.empty()){
+              Node *node = q.front();
+              q.pop();
+              if(node==nullptr) nullflag = true;
+              else{
+                  if(nullflag==true) return false;
+                  if(node->left && node->left->data > node->data ||
+                  node->right && node->right->data > node->data) return false;
+              
+                  q.push(node->left);
+                  q.push(node->right);
+              }
+          }
+          return true;
+      }
+  };
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 26 / 04 / 2025 -----------------------------------
 
 
