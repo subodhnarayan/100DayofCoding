089a71c5df4aecfe2756ed53fdcf03f421244e41
ExploreAI351 <your.email@example.com>
2025-04-27 23:46:26 +0530

    Update day486.cpp

--- day486.cpp
+++ day486.cpp
@@ -1,5 +1,27 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 27 / 04 / 2025 -----------------------------------
+Multiply two strings
+Difficulty: MediumAccuracy: 20.06%Submissions: 233K+Points: 4Average Time: 20m
+Given two numbers as strings s1 and s2. Calculate their Product.
+Note: The numbers can be negative and You are not allowed to use any built-in function or convert the strings to integers. There can be zeros in the begining of the numbers. You don't need to specify '+' sign in the begining of positive numbers.
 
+Examples:
+
+Input: s1 = "0033", s2 = "2"
+Output: "66"
+Explanation: 33 * 2 = 66
+Input: s1 = "11", s2 = "23"
+Output: "253"
+Explanation: 11 * 23  = 253
+Input: s1 = "123", s2 = "0"
+Output: "0"
+Explanation: Anything multiplied by 0 is equal to 0.
+Constraints:
+1 ≤ s1.size() ≤ 103
+1 ≤ s2.size() ≤ 103
+
+Expected Complexities
+Time Complexity: O(n * m)
+Auxiliary Space: O(n * m)
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 27 / 04 / 2025 -----------------------------------
