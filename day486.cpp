2fe72b05f3e356e24c70bec4cf436daf43c767b3
ExploreAI351 <your.email@example.com>
2025-04-27 23:47:25 +0530

    Update day486.cpp

--- day486.cpp
+++ day486.cpp
@@ -96,3 +96,19 @@ Constraints:
 3 <= nums.length <= 100
 -100 <= nums[i] <= 100
 
+
+
+class Solution {
+    public:
+        int countSubarrays(vector<int>& nums) {
+             int n = nums.size();
+            int ans = 0;
+            for (int i = 1; i < n - 1; ++i) {
+                if (nums[i] == (nums[i - 1] + nums[i + 1]) * 2) {
+                    ++ans;
+                }
+            }
+            return ans;
+        }
+};
+
