5d70593bcccd617b4c3ebb41acfa72eae392399d
ExploreAI351 <your.email@example.com>
2025-04-28 23:10:28 +0530

    Update day487.cpp

--- day487.cpp
+++ day487.cpp
@@ -98,3 +98,19 @@ Constraints:
 1 <= k <= 1015
 
 
+class Solution {
+    public:
+        long long countSubarrays(vector<int>& nums, long long k) {
+             int n = nums.size();
+            long long res = 0, total = 0;
+            for (int i = 0, j = 0; j < n; j++) {
+                total += nums[j];
+                while (i <= j && total * (j - i + 1) >= k) {
+                    total -= nums[i];
+                    i++;
+                }
+                res += j - i + 1;
+            }
+            return res;
+        }
+    };
