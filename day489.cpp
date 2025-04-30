01387dd319b0b58fc0a067d934524e897d88b956
ExploreAI351 <your.email@example.com>
2025-04-30 23:33:27 +0530

    Update day489.cpp

--- day489.cpp
+++ day489.cpp
@@ -31,7 +31,30 @@ Expected Complexities
 Time Complexity: O(n)
 Auxiliary Space: O(1)
 
-
+class Solution {
+    public:
+      // Function to find the length of a loop in the linked list.
+      int countNodesinLoop(Node *head) {
+          // Code here
+          unordered_map<Node*, int> mp;
+          Node* ptr = head;
+          while(ptr){
+              if(mp[ptr]) break;
+              mp[ptr] = ptr->data;
+              ptr = ptr -> next;
+          }
+          int len = 0;
+          if(ptr){
+              Node* ptr2 = ptr;
+              while(ptr2){
+                  ptr2 = ptr2 -> next;
+                  len++;
+                  if(ptr2 == ptr) break;
+              }
+          }
+          return len;
+      }
+  };
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 30 / 04 / 2025 -----------------------------------
 
