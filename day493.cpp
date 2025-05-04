cdc610040d6f473c7ed113e5b8fcf9f74ab3cef5
ExploreAI351 <your.email@example.com>
2025-05-04 23:28:43 +0530

    Update day493.cpp

--- day493.cpp
+++ day493.cpp
@@ -1,4 +1,26 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 04 / 05 / 2025 -----------------------------------
+Smallest distinct window
+Difficulty: MediumAccuracy: 31.85%Submissions: 109K+Points: 4
+Given a string str, your task is to find the length of the smallest window that contains all the characters of the given string at least once.
+
+Example:
+
+Input: str = "aabcbcdbca"
+Output: 4
+Explanation: Sub-String "dbca" has the smallest length that contains all the characters of str.
+Input: str = "aaab"
+Output: 2
+Explanation: Sub-String "ab" has the smallest length that contains all the characters of str.
+Input: str = "geeksforgeeks"
+Output: 8
+Explanation: There are multiple substring with smallest length that contains all characters of str, "geeksfor" and "forgeeks". 
+Constraints:
+1 ≤ str.size() ≤ 105
+str contains only lower-case english alphabets.
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(1)
 
 
 
