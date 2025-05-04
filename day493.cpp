b1854d4faaab781e7642fa10c4bea537827d3bb9
ExploreAI351 <your.email@example.com>
2025-05-04 23:29:02 +0530

    Update day493.cpp

--- day493.cpp
+++ day493.cpp
@@ -22,6 +22,54 @@ Expected Complexities
 Time Complexity: O(n)
 Auxiliary Space: O(1)
 
+class Solution {
+    public:
+      int findSubString(string& str) {
+          // code here
+         int first=0,second=0,diff=0,len=str.size();
+          vector<int> count(256,0);
+          while(first<str.size())
+          {
+              if(count[str[first]]==0)
+              diff++;
+              
+              count[str[first]]++;
+              first++;
+          }
+          
+          for(int i=0;i<256;i++)
+          {
+              count[i]=0;
+          }
+          first=0;
+          while(second<str.size())
+          {
+              while(diff && second<str.size())
+          {
+              if(count[str[second]]==0)
+              diff--;
+              
+              count[str[second]]++;
+              second++;
+          }
+              
+              len=min(len,second-first);
+              
+              while(diff!=1)
+              {
+                   
+              len=min(len,second-first);
+              count[str[first]]--;
+              
+              if(count[str[first]]==0)
+              diff++;
+              
+              first++;
+              }
+          }
+          return len; 
+      }
+  };
 
 
 
