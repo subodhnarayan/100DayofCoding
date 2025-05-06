3504c5dd028d63bba00463786fe6a33c9e89c1db
ExploreAI351 <your.email@example.com>
2025-05-06 23:53:42 +0530

    Update day495.cpp

--- day495.cpp
+++ day495.cpp
@@ -103,3 +103,18 @@ The elements in nums are distinct.
  
 
 Follow-up: Can you solve it without using an extra space (i.e., O(1) memory)?
+
+
+
+class Solution {
+    public:
+        vector<int> buildArray(vector<int>& nums) {
+              for (int i = 0; i < nums.size(); i++) 
+                nums[i] += (1024 * (nums[nums[i]] % 1024));
+            
+            for (int i = 0; i < nums.size(); i++) 
+                nums[i] /= 1024;
+            
+            return nums;
+        }
+    };
