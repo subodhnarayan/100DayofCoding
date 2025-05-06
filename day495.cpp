6f8d25b5d74145f618a7cbcb453b9f3c80bfdb2a
ExploreAI351 <your.email@example.com>
2025-05-06 23:52:39 +0530

    Update day495.cpp

--- day495.cpp
+++ day495.cpp
@@ -1,4 +1,33 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 06 / 05 / 2025 -----------------------------------
+Left View of Binary Tree
+Difficulty: EasyAccuracy: 33.74%Submissions: 556K+Points: 2Average Time: 20m
+You are given the root of a binary tree. Your task is to return the left view of the binary tree. The left view of a binary tree is the set of nodes visible when the tree is viewed from the left side.
+
+If the tree is empty, return an empty list.
+
+Examples :
+
+Input: root[] = [1, 2, 3, 4, 5, N, N]
+
+Output: [1, 2, 4]
+Explanation: From the left side of the tree, only the nodes 1, 2, and 4 are visible.
+
+Input: root[] = [1, 2, 3, N, N, 4, N, N, 5, N, N]
+
+Output: [1, 2, 4, 5]
+Explanation: From the left side of the tree, the nodes 1, 2, 4, and 5 are visible.
+
+Input: root[] = [N]
+Output: []
+Constraints:
+0 <= number of nodes <= 106
+0 <= node -> data <= 105
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(n)
+
+
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 06 / 05 / 2025 -----------------------------------
