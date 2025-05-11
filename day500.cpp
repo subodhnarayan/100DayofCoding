244e5f7f66a5745721f48cca4dbe9a58cbf182bb
ExploreAI351 <your.email@example.com>
2025-05-11 22:48:31 +0530

    Update day500.cpp

--- day500.cpp
+++ day500.cpp
@@ -21,6 +21,25 @@ Expected Complexities
 Time Complexity: O(n^2 log k)
 Auxiliary Space: O(log k)
 
+class Solution {
+  public:
+    int kthLargest(vector<int> &arr, int k) {
+        // code here
+        priority_queue<int,vector<int>,greater<>>p;
+        int n=arr.size();
+        int count=0;
+        for(int i=0;i<n;i++){
+            int x=0;
+            for(int j=i;j<n;j++){
+                x+=arr[j];
+                if(p.size()<k) p.push(x);
+                else if(p.top()<x) p.pop(),p.push(x);
+                
+            }
+        }
+        return p.top();
+    }
+};
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 11 / 05 / 2025 -----------------------------------
