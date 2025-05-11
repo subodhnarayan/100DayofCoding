0cbec5d6cadf4d5034d4a87baa715e29d635da24
ExploreAI351 <your.email@example.com>
2025-05-11 22:49:24 +0530

    Update day500.cpp

--- day500.cpp
+++ day500.cpp
@@ -69,3 +69,17 @@ Constraints:
 
 1 <= arr.length <= 1000
 1 <= arr[i] <= 1000
+
+class Solution {
+public:
+    bool threeConsecutiveOdds(vector<int>& arr) {
+        int n=arr.size();
+        
+        for(int i=0;i<n-2;i++){
+           if((arr[i]%2==1) && (arr[i+1]%2==1) && (arr[i+2]%2==1)){
+            return true;
+           }
+        }
+        return false;
+    }
+};
