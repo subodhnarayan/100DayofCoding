df9927f3dd9b2c90b2a540ec2708fdef87ff39a8
ExploreAI351 <your.email@example.com>
2025-05-12 23:54:10 +0530

    Update day501.cpp

--- day501.cpp
+++ day501.cpp
@@ -96,3 +96,24 @@ class Solution {
 
 -- -- -- -- -- -- -- -- -GFG - POTD - 13 / 05 / 2025 ----------------------------------------
 
+class Solution {
+public:
+    vector<int> findEvenNumbers(vector<int>& digits) {
+        set<int> st;
+        int n=digits.size();
+        sort(digits.begin(),digits.end());
+        for(int i=0;i<n;i++){
+            if(digits[i]==0) continue;
+                for(int j=0;j<n;j++){
+                    if(i==j) continue;
+                      for(int k=0;k<n;k++){
+                            if(i==k || j==k) continue;
+                            if (digits[k] % 2 != 0) continue;
+                            int num=digits[i]*100+digits[j]*10+digits[k];
+                            st.insert(num);
+                        }
+                }
+        }
+        return vector<int>(st.begin(),st.end());
+    }
+};
