7bde88a94cc116cbbf8fbf6c58021fe8260ded80
ExploreAI351 <your.email@example.com>
2025-05-13 23:59:00 +0530

    Update day502.cpp

--- day502.cpp
+++ day502.cpp
@@ -23,7 +23,33 @@ Constraints:
 1 ≤ n ≤ 100
 0 ≤ r ≤ 100
 
-
+class Solution {
+  public:
+  const int mod = 1e9+7;
+  int binaryexp(int x, int n) {
+        if(n == 0) return 1;
+        int temp = binaryexp(x, n/2);
+        if(n%2 == 0) return (1LL *temp * temp)%mod;
+        else return (((1LL*temp*temp)%mod)*x)%mod;
+    }
+    int nCr(int n, int r) {
+        // code here
+        if(r > n) return 0;
+        if(r == n) return 1;
+        long long ans = 1;
+        //first calculating the numerator
+        for(int i = 1; i<=r; i++) {
+            ans = (ans*1LL*(i+(n-r)))%mod;
+        }
+        //now calculating the final ans by dividing the ans with the terms in denominator 
+        for(int i = 1; i<=r; i++) {
+            int inv = binaryexp(i, mod-2);
+            ans = ((ans%mod) * (inv%mod))%mod;
+        }
+        return ans;
+        
+    }
+};
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 13 / 05 / 2025 ----------------------------------------
 
