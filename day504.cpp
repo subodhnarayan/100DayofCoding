ab59ab00d8d68e49070c6e9e4826cc2bc1511994
ExploreAI351 <your.email@example.com>
2025-05-15 23:50:52 +0530

    Update day504.cpp

--- day504.cpp
+++ day504.cpp
@@ -1,3 +1,21 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 15 / 05 / 2025 ----------------------------------------
 
+Substrings with same first and last characters
+Difficulty: EasyAccuracy: 50.0%Submissions: 26K+Points: 2
+Given a string s consisting of lowercase characters, the task is to find the count of all substrings that start and end with the same character.
+
+Examples:
+
+Input: s = "abcab"
+Output: 7
+Explanation: There are 7 substrings where the first and last characters are the same: "a", "abca", "b", "bcab", "c", "a", and "b"
+Input: s = "aba"
+Output: 4
+Explanation: There are 4 substrings where the first and last characters are the same: "a", "aba", "b", "a"
+Constraints:
+1 <= |s| <= 104
+s contains lower case english alphabets
+
+
+
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 15 / 05 / 2025 ----------------------------------------
