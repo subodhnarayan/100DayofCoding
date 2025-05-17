b03276588fb96e23703444b0a475b91ac1cbbd19
ExploreAI351 <your.email@example.com>
2025-05-17 23:24:32 +0530

    Update day506.cpp

--- day506.cpp
+++ day506.cpp
@@ -1,6 +1,24 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 17 / 05 / 2025 ----------------------------------------
 
+Sort the given array after applying the given equation
+Difficulty: MediumAccuracy: 36.21%Submissions: 24K+Points: 4
+Given an integer array arr[] sorted in ascending order, along with three integers: A, B, and C. The task is to transform each element x in the array using the quadratic function A*(x2) + B*x + C. After applying this transformation to every element, return the modified array in sorted order.
 
+Examples:
+
+Input: arr[] = [-4, -2, 0, 2, 4], A = 1, B = 3, C = 5
+Output: [3, 5, 9, 15, 33]
+Explanation: After applying f(x) = 1*(x2)+ 3*x + 5 to each x, we get [9, 3, 5, 15, 33]. After sorting this array, the array becomes [3, 5, 9, 15, 33].
+Input: arr[] = [-3, -1, 2, 4], A = -1, B = 0, C = 0
+Output: [-16, -9, -4, -1]
+Explanation: After applying f(x) = -1*(x2) + 0*x + 0 to each x, we get [ -9, -1, -4, -16 ]. After sorting this array, the array becomes  [-16, -9, -4, -1].
+Constraints:
+1 ≤ arr.size() ≤ 106
+-103 ≤ arr[i], A, B, C ≤ 103
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(n)
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 17 / 05 / 2025 ----------------------------------------
 
