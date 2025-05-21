1556ef53d7a9e685214416eb33052affe702fc5c
ExploreAI351 <your.email@example.com>
2025-05-21 23:36:13 +0530

    Update day510.cpp

--- day510.cpp
+++ day510.cpp
@@ -1,5 +1,26 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 21 / 05 / 2025 ----------------------------------------
 
+Kth Smallest Number in Multiplication Table
+Difficulty: HardAccuracy: 50.15%Submissions: 17K+Points: 8
+Given three integers m, n and k. Consider a grid of m * n, where mat[i][j] = i * j (1 based index). The task is to return the kth smallest element in the m * n multiplication table.
+
+Examples :
+
+Input: m = 3, n = 3, k = 5
+Output: 3
+Explanation: 
+
+The 5th smallest element is 3. 
+Input: m = 2, n = 3, k = 6
+Output: 6 
+Explanation: [1, 2, 3][2, 4, 6]. The 6th smallest element is 6.
+Constraints:
+1 <= m, n <= 3 * 104
+1 <= k <= m * n
+
+Expected Complexities
+Time Complexity: O(m * log (m*n))
+Auxiliary Space: O(1)
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 21 / 05 / 2025 ----------------------------------------
 
