046c0846035fde7393306120108797b1b0291030
ExploreAI351 <your.email@example.com>
2025-05-22 23:49:34 +0530

    Update day511.cpp

--- day511.cpp
+++ day511.cpp
@@ -105,3 +105,30 @@ Constraints:
 1 <= queries.length <= 105
 queries[i].length == 2
 0 <= li <= ri < nums.length
+
+
+class Solution {
+public:
+    int maxRemoval(vector<int>& nums, vector<vector<int>>& queries) {
+       sort(queries.begin(), queries.end());
+        priority_queue<int> available;
+        priority_queue<int, vector<int>, greater<int>> assigned;
+        int count = 0;
+
+        for (int time = 0, k = 0; time < nums.size(); time++) {
+            while (!assigned.empty() && assigned.top() < time)
+                assigned.pop();
+            while (k < queries.size() && queries[k][0] <= time) 
+                available.push(queries[k++][1]);
+            while (assigned.size() < nums[time] && 
+                  !available.empty() && available.top() >= time) {
+                assigned.push(available.top());
+                available.pop();
+                count++;
+            }
+            if (assigned.size() < nums[time])
+                return -1;
+        }
+        return queries.size() - count; 
+    }
+};
