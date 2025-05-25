cc1050d356e1b3186b2eac141dbf6175a2e4adf9
ExploreAI351 <your.email@example.com>
2025-05-25 22:57:47 +0530

    Update day514.cpp

--- day514.cpp
+++ day514.cpp
@@ -21,6 +21,38 @@ Expected Complexities
 Time Complexity: O(n + max(arr[i])^2)
 Auxiliary Space: O(max(arr[i]))
 
+Time Complexity: O(n + max(arr[i])^2)
+Auxiliary Space: O(max(arr[i]))
+
+
+class Solution {
+  public:
+    bool pythagoreanTriplet(vector<int>& arr) {
+        // code here
+       int n=arr.size();
+        int maxi=*max_element(arr.begin(),arr.end());
+        vector<int>elements(maxi+1,0);
+        for(int i:arr){
+            elements[i]++;
+        }
+        
+        for(int i=1;i<=maxi;i++){
+            for(int j=i+1;j<=maxi;j++){
+                // check if i and j exists in arr
+                if(elements[i] && elements[j]){
+                    int a=i,b=j;
+                    int right=(a*a) + (b*b);
+                    int c=sqrt(right);
+                    if(c<=maxi && (c*c)==right && elements[c]) 
+                        return true;
+                }
+            }
+        }
+        
+        return false;
+    }
+};
+
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 25 / 05 / 2025 ----------------------------------------
