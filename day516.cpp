d048effd096ab255efa5783c70f1c38654cea375
ExploreAI351 <your.email@example.com>
2025-05-27 23:39:41 +0530

    Update day516.cpp

--- day516.cpp
+++ day516.cpp
@@ -30,6 +30,41 @@ Expected Complexities
 Time Complexity: O(n)
 Auxiliary Space: O(n)
 
+class Solution {
+  public:
+    vector<int> leafNodes(vector<int>& preorder) {
+        // code here
+        queue<vector<int>>q;
+        q.push(preorder);
+        vector<int>ans;
+        while(!q.empty())
+        {
+            vector<int>t=q.front();
+            q.pop();
+            vector<int>small;
+            vector<int>large;
+            if(t.size()==1)
+            ans.push_back(t[0]);
+            else
+            {
+                for(int i=1;i<t.size();i++)
+                {
+                    if(t[i]>t[0])
+                    large.push_back(t[i]);
+                    else
+                    small.push_back(t[i]);
+                    
+                }
+                if(!small.empty())
+                q.push(small);
+                if(!large.empty())
+                q.push(large);
+            }
+        }
+        sort(ans.begin(),ans.end());
+        return ans;
+    }
+};
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 27 / 05 / 2025 ----------------------------------------
 
