0d9d1a041a43f8de6b3547efb67a464930335ff2
ExploreAI351 <your.email@example.com>
2025-05-27 23:39:06 +0530

    Update day516.cpp

--- day516.cpp
+++ day516.cpp
@@ -1,4 +1,35 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 27 / 05 / 2025 ----------------------------------------
+Print leaf nodes from preorder traversal of BST
+Difficulty: MediumAccuracy: 47.26%Submissions: 34K+Points: 4
+Given a preorder traversal of a BST, find the leaf nodes of the tree without building the tree.
+
+
+Examples:
+
+Input: preorder[] = [5, 2, 10]
+Output: [2, 10]
+Explaination: 
+
+2 and 10 are the leaf nodes as shown in the figure.
+Input: preorder[] = [4, 2, 1, 3, 6, 5]
+Output: [1, 3, 5]
+Explaination: 
+
+1, 3 and 5 are the leaf nodes as shown in the figure.
+Input: preorder[] = [8, 2, 5, 10, 12]
+Output: [5, 12]
+Explaination: 
+
+5 and 12 are the leaf nodes as shown in the figure.
+
+Constraints:
+1 ≤ preorder.size() ≤ 103
+1 ≤ preorder[i] ≤ 103
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(n)
+
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 27 / 05 / 2025 ----------------------------------------
 
