97f62b747674ac3b21d0545f5241b37a4fd35eaf
ExploreAI351 <your.email@example.com>
2025-05-30 23:22:14 +0530

    Update day518.cpp

--- day518.cpp
+++ day518.cpp
@@ -1,4 +1,33 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 30 / 05 / 2025 ----------------------------------------
+Closest Neighbour in BST
+Difficulty: EasyAccuracy: 36.98%Submissions: 60K+Points: 2
+Given the root of a binary search tree and a number k, find the greatest number in the binary search tree that is less than or equal to k.
+
+Examples:
+
+ 
+
+Input: root = [10, 7, 15, 2, 8, 11, 16], k = 14
+
+Output: 11
+Explanation: The greatest element in the tree which is less than or equal to 14, is 11.
+Input: root = [5, 2, 12, 1, 3, 9, 21, N, N, N, N, N, N, 19, 25], k = 24
+
+Output: 21
+Explanation: The greatest element in the tree which is less than or equal to 24, is 21. 
+Input: root = [5, 2, 12, 1, 3, 9, 21, N, N, N, N, N, N, 19, 25], k = 4
+
+Output: 3
+Explanation: The greatest element in the tree which is less than or equal to 4, is 3.
+Constraints:
+1 <= number of nodes <= 105
+1 <= node->data, k <= 105
+All nodes are unique in the BST
+
+Expected Complexities
+Time Complexity: O(h)
+Auxiliary Space: O(h)
+
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 30 / 05 / 2025 ----------------------------------------
