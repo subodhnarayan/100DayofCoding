c169ea2dd67610488a19ed098797b1dcc5601e25
ExploreAI351 <your.email@example.com>
2025-06-02 20:30:54 +0530

    Update day521.cpp

--- day521.cpp
+++ day521.cpp
@@ -85,3 +85,26 @@ n == ratings.length
 1 <= n <= 2 * 104
 0 <= ratings[i] <= 2 * 104
 
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int cnt=0;
+        int n=ratings.size();
+        vector<int> candies(n,1);
+
+        for(int i=1;i<n;i++)
+            if(ratings[i]>ratings[i-1])
+             candies[i]=candies[i-1]+1;
+
+        
+        for(int i=n-1;i>0;i--){
+            if(ratings[i-1]>ratings[i])
+              candies[i-1]=max(candies[i]+1,candies[i-1]);
+            cnt+=candies[i-1];
+        }
+
+       cnt+=candies[n-1];
+
+       return cnt;
+    }
+};
