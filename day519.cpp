2e3aaf35a39c2d9959d48f1236d4288715d84ffb
ExploreAI351 <your.email@example.com>
2025-06-02 20:25:58 +0530

    Update day519.cpp

--- day519.cpp
+++ day519.cpp
@@ -92,3 +92,31 @@ n == board.length == board[i].length
 2 <= n <= 20
 board[i][j] is either -1 or in the range [1, n2].
 The squares labeled 1 and n2 are not the starting points of any snake or ladder.
+
+
+class Solution {
+public:
+    int snakesAndLadders(vector<vector<int>>& board) {
+        int n = board.size();
+        vector<int> min_rolls(n * n + 1, -1);
+        queue<int> q;
+        min_rolls[1] = 0;
+        q.push(1);
+        while (!q.empty()) {
+            int x = q.front();
+            q.pop();
+            for (int i = 1; i <= 6 && x + i <= n * n; i++) {
+                int t = x + i, row = (t - 1) / n, col = (t - 1) % n;
+                int v = board[n - 1 - row][row % 2 ? n - 1 - col : col];
+                int y = v > 0 ? v : t; // follow ladder / snake if present
+                if (y == n * n)
+                    return min_rolls[x] + 1;
+                if (min_rolls[y] == -1) {
+                    min_rolls[y] = min_rolls[x] + 1;
+                    q.push(y);
+                }
+            }
+        }
+        return -1;
+    }
+};
