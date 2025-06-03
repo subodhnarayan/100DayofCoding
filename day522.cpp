30f2c0c7e89bd68b641d04089782b0f61902c91d
ExploreAI351 <your.email@example.com>
2025-06-03 23:11:15 +0530

    Update day522.cpp

--- day522.cpp
+++ day522.cpp
@@ -109,3 +109,27 @@ All values of containedBoxes[i] are unique.
 Each box is contained in one box at most.
 0 <= initialBoxes.length <= n
 0 <= initialBoxes[i] < n
+
+class Solution {
+public:
+    int maxCandies(vector<int>& vis, vector<int>& candies, vector<vector<int>>& keys, vector<vector<int>>& containedBoxes, vector<int>& initialBoxes) {
+        bool foundOpenable = true;
+        int totalCandies = 0;
+        while (!initialBoxes.empty() && foundOpenable) {
+            foundOpenable = false;
+            vector<int> nextBoxes;
+            for (int boxId : initialBoxes) {
+                if (vis[boxId]) {
+                    foundOpenable = true;
+                    nextBoxes.insert(end(nextBoxes), begin(containedBoxes[boxId]), end(containedBoxes[boxId]));
+                    for (int keyId : keys[boxId]) vis[keyId] = 1;
+                    totalCandies += candies[boxId];
+                } else {
+                    nextBoxes.push_back(boxId);
+                }
+            }
+            swap(initialBoxes, nextBoxes);
+        }
+        return totalCandies;
+    }
+};
