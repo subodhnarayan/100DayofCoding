23d9f2be0401772c5594339695bc28e09190209f
ExploreAI351 <your.email@example.com>
2025-06-06 22:38:29 +0530

    Update day525.cpp

--- day525.cpp
+++ day525.cpp
@@ -1,5 +1,31 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 06 / 06 / 2025 ----------------------------------------
+Search Pattern (Rabin-Karp Algorithm)
+Difficulty: MediumAccuracy: 34.53%Submissions: 81K+Points: 4Average Time: 20m
+Given two strings:
 
+A text string in which you want to search.
+
+A pattern string that you are looking for within the text.
+
+Return all positions (1-based indexing) where the pattern occurs as a substring in the text. If the pattern does not occur, return an empty list.
+
+All characters in both strings are lowercase English letters (a to z).
+
+Examples:
+
+Input: text = "birthdayboy", pattern = "birth"
+Output: [1]
+Explanation: The string "birth" occurs at index 1 in text.
+Input: text = "geeksforgeeks", pattern = "geek"
+Output: [1, 9]
+Explanation: The string "geek" occurs twice in text, one starts are index 1 and the other at index 9.
+Constraints:
+1<= text.size() <=5*105
+1<= pattern.size() <= text.size()
+
+Expected Complexities
+Time Complexity: O(n + m)
+Auxiliary Space: O(1)
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 06 / 06 / 2025 ----------------------------------------
