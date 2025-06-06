bb9e5b4cd44515cc26dc69e7c8cc71d0c1f92069
ExploreAI351 <your.email@example.com>
2025-06-06 22:40:29 +0530

    Update day525.cpp

--- day525.cpp
+++ day525.cpp
@@ -114,3 +114,29 @@ Constraints:
 1 <= s.length <= 105
 s consists of only English lowercase letters.
 
+class Solution {
+public:
+    string robotWithString(string s) {
+        unordered_map<char, int> cnt;
+        for (char c : s) {
+            cnt[c]++;
+        }
+
+        stack<char> stk;
+        string res;
+        char minCharacter = 'a';
+        for (char c : s) {
+            stk.emplace(c);
+            cnt[c]--;
+            while (minCharacter != 'z' && cnt[minCharacter] == 0) {
+                minCharacter++;
+            }
+            while (!stk.empty() && stk.top() <= minCharacter) {
+                res.push_back(stk.top());
+                stk.pop();
+            }
+        }
+
+        return res;
+    }
+};
