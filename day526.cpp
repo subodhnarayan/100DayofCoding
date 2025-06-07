ee11706d0cba43bb1f3979d052f12fa01649c4c4
ExploreAI351 <your.email@example.com>
2025-06-07 22:33:23 +0530

    Update day526.cpp

--- day526.cpp
+++ day526.cpp
@@ -19,6 +19,29 @@ Expected Complexities
 Time Complexity: O(n)
 Auxiliary Space: O(n)
 
-
+class Solution {
+  public:
+    int longestCommonSum(vector<int> &a1, vector<int> &a2) {
+        // Code here.
+        int n=a1.size();
+        unordered_map<int,int> mpp;
+        
+        int sum=0;
+        int maxi=0;
+        
+        mpp[0]=-1;
+        
+        for(int i=0;i<n;i++){
+            sum+=(a1[i]-a2[i]);
+            if(mpp.find(sum)!=mpp.end()){
+                maxi=max(maxi,i-mpp[sum]);
+            }
+            else{
+                mpp[sum]=i;
+            }
+        }
+        return maxi;
+    }
+};
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 07 / 06 / 2025 ----------------------------------------
 
