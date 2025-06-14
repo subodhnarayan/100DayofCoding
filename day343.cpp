eb68bc7f533d387dd44ab535b2fa1635483dac97
@MeSubodh <88626001+subodhnarayan@users.noreply.github.com>
2025-06-14 23:26:54 +0545

    Update day343.cpp

--- day343.cpp
+++ day343.cpp
@@ -23,3 +23,79 @@ Constraints:
 Expected Complexities
 Time Complexity: O(n)
 Auxiliary Space: O(h)
+
+   class Solution {
+  public:
+  bool solve(Node* r1, Node* r2){
+        if(r1==NULL && r2==NULL) return 1;
+        if((r1==NULL && r2!=NULL) || (r1!=NULL && r2==NULL)) return 0;
+        return ((r1->data==r2->data) && solve(r1->left, r2->right) 
+        && solve(r1->right, r2->left));
+    }
+    
+    bool isSymmetric(Node* root) {
+        if(root==NULL) return true;
+        return solve(root->left,root->right);
+    }
+};
+
+
+
+
+
+2566. Maximum Difference by Remapping a Digit
+Solved
+Easy
+Topics
+premium lock icon
+Companies
+Hint
+You are given an integer num. You know that Bob will sneakily remap one of the 10 possible digits (0 to 9) to another digit.
+
+Return the difference between the maximum and minimum values Bob can make by remapping exactly one digit in num.
+
+Notes:
+
+When Bob remaps a digit d1 to another digit d2, Bob replaces all occurrences of d1 in num with d2.
+Bob can remap a digit to itself, in which case num does not change.
+Bob can remap different digits for obtaining minimum and maximum values respectively.
+The resulting number after remapping can contain leading zeroes.
+ 
+
+Example 1:
+
+Input: num = 11891
+Output: 99009
+Explanation: 
+To achieve the maximum value, Bob can remap the digit 1 to the digit 9 to yield 99899.
+To achieve the minimum value, Bob can remap the digit 1 to the digit 0, yielding 890.
+The difference between these two numbers is 99009.
+Example 2:
+
+Input: num = 90
+Output: 99
+Explanation:
+The maximum value that can be returned by the function is 99 (if 0 is replaced by 9) and the minimum value that can be returned by the function is 0 (if 9 is replaced by 0).
+Thus, we return 99.
+ 
+
+Constraints:
+
+1 <= num <= 108
+
+
+   class Solution {
+public:
+    int minMaxDifference(int num) {
+        string s=to_string(num);
+        string t=s;
+        size_t pos=s.find_first_not_of('9');
+        if(pos!=string::npos){
+            char a = s[pos];
+            replace(s.begin(),s.end(),a,'9');
+        }
+        char b=t[0];
+        replace(t.begin(),t.end(),b,'0');
+        return stoi(s)-stoi(t);
+    }
+};
