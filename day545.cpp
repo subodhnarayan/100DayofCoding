e59abd47286f31908d200986af9fc23431d4d1f6
ExploreAI351 <your.email@example.com>
2025-06-25 22:13:31 +0530

    Update day545.cpp

--- day545.cpp
+++ day545.cpp
@@ -1,5 +1,28 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 25 / 06 / 2025 ----------------------------------------
 
+Check if frequencies can be equal
+Difficulty: MediumAccuracy: 16.67%Submissions: 134K+Points: 4
+Given a string s consisting only lowercase alphabetic characters, check whether it is possible to remove at most one character such that the  frequency of each distinct character in the string becomes same. Return true if it is possible; otherwise, return false.
+
+Examples:
+
+Input: s = "xyyz"
+Output: true 
+Explanation: Removing one 'y' will make frequency of each distinct character to be 1.
+Input: s = "xyyzz"
+Output: true
+Explanation: Removing one 'x' will make frequency of each distinct character to be 2.
+Input: s = "xxxxyyzz"
+Output: false
+Explanation: Frequency can not be made same by removing at most one character.
+Constraints:
+1 ≤ s.size() ≤ 105
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(1)
+
+
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 25 / 06 / 2025 ----------------------------------------
