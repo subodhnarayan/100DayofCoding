2ec52ab8fa9ce99f7fe0c25c8141de08873c1edf
ExploreAI351 <your.email@example.com>
2025-06-27 22:31:36 +0530

    Update day547.cpp

--- day547.cpp
+++ day547.cpp
@@ -26,7 +26,52 @@ Expected Complexities
 Time Complexity: O(n)
 Auxiliary Space: O(1)
 
+class Solution {
+  public:
+    int getCount(int n) {
+        // code here
+        if (n == 1) {
+            return 10;
+        }
 
+        int arr[10][5] = {
+            {0, 8, -1, -1, -1},   // 0 can move to 8
+            {1, 2, 4, -1, -1},    // 1 can move to 2, 4
+            {1, 2, 3, 5, -1},     // 2 can move to 1, 3, 5
+            {2, 3, 6, -1, -1},    // 3 can move to 2, 6
+            {1, 4, 5, 7, -1},     // 4 can move to 1, 5, 7
+            {2, 4, 5, 6, 8},      // 5 can move to 2, 4, 6, 8
+            {3, 5, 6, 9, -1},     // 6 can move to 3, 5, 9
+            {4, 7, 8, -1, -1},    // 7 can move to 4, 8
+            {0, 5, 7, 8, 9},      // 8 can move to 0, 5, 7, 9
+            {6, 8, 9, -1, -1}     // 9 can move to 6, 8
+        };
+
+        long long dp[n+1][10];
+
+        for (int i = 0; i < 10; i++) {
+            dp[1][i] = 1;
+        }
+
+        for (int i = 2; i <= n; i++) {
+            for (int j = 0; j < 10; j++) {
+                dp[i][j] = 0;
+                for (int k = 0; k < 5; k++) {
+                    if (arr[j][k] != -1) {
+                        dp[i][j] += dp[i - 1][arr[j][k]];
+                    }
+                }
+            }
+        }
+
+        long long ans = 0;
+        for (int i = 0; i <= 9; i++) {
+            ans += dp[n][i];
+        }
+
+        return ans;
+    }
+};
 
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 27 / 06 / 2025 ----------------------------------------
