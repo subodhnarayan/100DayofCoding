13fb7b7f14dde873a97f8bd05af66fffd718f474
ExploreAI351 <your.email@example.com>
2025-06-28 21:56:57 +0530

    Update day578.cpp

--- day578.cpp
+++ day578.cpp
@@ -25,7 +25,18 @@ Expected Complexities
 Time Complexity: O(n + m + max(b[i]))
 Auxiliary Space: O(max(b[i]))
 
-
+class Solution {
+  public:
+    vector<int> countLessEq(vector<int>& a, vector<int>& b) {
+        // code here
+        sort(b.begin(), b.end());
+        vector<int> ans;
+        for(auto &e : a){
+            ans.push_back(upper_bound(b.begin(), b.end(), e) - b.begin());
+        }
+        return ans;
+    }
+};
 
 
 
