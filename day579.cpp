00b25317f0477c821aec18551e2f3cea44a934d2
ExploreAI351 <your.email@example.com>
2025-06-29 21:53:03 +0530

    Update day579.cpp

--- day579.cpp
+++ day579.cpp
@@ -16,7 +16,36 @@ Constraints:
 1 ≤ k ≤ arr.size() ≤ 105
 1 ≤ arr[i] ≤ 104
 
-
+class Solution {
+  public:
+   bool canSplit(vector<int>& arr, int k, int mid){
+        int subarray=0, currsum=0;
+        for(int n:arr){
+            currsum+=n;
+            if(currsum>mid){
+                currsum=n;
+                subarray+=1;
+            }
+        }
+        return subarray+1<=k;
+    }
+    int splitArray(vector<int>& arr, int k) {
+        // code here
+         int l=*max_element(arr.begin(), arr.end());
+        int r=accumulate(arr.begin(), arr.end(), 0);
+        int result=r;
+        while(l<=r){
+            int mid=l+(r-l)/2;
+            if(canSplit(arr, k, mid)){
+                result=mid;
+                r=mid-1;
+            }else{
+                l=mid+1;
+            }
+        }
+        return result;
+    }
+};
 
 -- -- -- -- -- -- -- -- -Leetcode - POTD - 29 / 06 / 2025 ----------------------------------------
 
