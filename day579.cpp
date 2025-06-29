948accaa2bf701c6edad1ffe3cac720d2d45eeec
ExploreAI351 <your.email@example.com>
2025-06-29 21:52:32 +0530

    Update day579.cpp

--- day579.cpp
+++ day579.cpp
@@ -1,5 +1,20 @@
 -- -- -- -- -- -- -- -- -GFG - POTD - 29/ 06 / 2025 ----------------------------------------
 
+Split Array Largest Sum
+Difficulty: HardAccuracy: 58.9%Submissions: 57K+Points: 8
+Given an array arr[] and an integer k, divide the array into k contiguous subarrays such that the maximum sum among these subarrays is minimized. Find this minimum possible maximum sum.
+
+Examples:
+
+Input: arr[] = [1, 2, 3, 4], k = 3
+Output: 4
+Exaplanation: Optimal Split is [1, 2], [3], [4]. Maximum sum of all subarrays is 4, which is minimum possible for 3 splits.
+Input: arr[] = [1, 1, 2], k = 2
+Output: 2
+Exaplanation: Splitting the array as [1, 1] and [2] is optimal. This results in a maximum sum subarray of 2.
+Constraints:
+1 ≤ k ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 104
 
 
 
