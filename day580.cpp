7bceff9abd792048d1b7f5582952a2f9fd2233b3
ExploreAI351 <your.email@example.com>
2025-06-30 21:22:41 +0530

    Update day580.cpp

--- day580.cpp
+++ day580.cpp
@@ -115,3 +115,20 @@ Constraints:
 1 <= nums.length <= 2 * 104
 -109 <= nums[i] <= 109
 
+class Solution {
+public:
+    int findLHS(vector<int>& nums) {
+         sort(nums.begin(), nums.end());
+        int j = 0, maxLength = 0;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            while (nums[i] - nums[j] > 1) {
+                ++j;
+            }
+            if (nums[i] - nums[j] == 1) {
+                maxLength = max(maxLength, i - j + 1);
+            }
+        }
+        return maxLength;
+    }
+};
