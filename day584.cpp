189c17a632fde37bce6beb2cef1fbccc44537714
ExploreAI351 <your.email@example.com>
2025-07-04 22:19:17 +0530

    Update day584.cpp

--- day584.cpp
+++ day584.cpp
@@ -22,7 +22,29 @@ Constraints:
 1 ≤ k ≤ 2*104
 1 ≤ arr[i] ≤ 109
 
-
+class Solution {
+  public:
+    int countAtMostK(vector<int> &arr, int k) {
+        // code here
+         map<int, int> mp;
+        int st=0;
+        int end=0;
+        int total=0;
+        while(end< arr.size()){
+            mp[arr[end]]++;
+            while(mp.size()>k){
+                mp[arr[st]]--;
+                if(mp[arr[st]]==0){
+                    mp.erase(arr[st]);
+                }
+                st++;
+            }
+            total= total+ (end-st+1);
+            end++;
+        }
+        return total;
+    }
+};
 
 
 
