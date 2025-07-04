0b2d7727ba9f76de494a82c9b0f6902750ae5e1f
ExploreAI351 <your.email@example.com>
2025-07-04 22:20:31 +0530

    Update day584.cpp

--- day584.cpp
+++ day584.cpp
@@ -107,3 +107,15 @@ Constraints:
 operations[i] is either 0 or 1.
 The input is generated such that word has at least k characters after all operations.
 
+
+class Solution {
+public:
+    char kthCharacter(long long k, vector<int>& arr) {
+        int c = 0; 
+        k--;
+        for (int i = 0; k != 0; i++, k >>= 1) {
+            c += ((k & 1) & arr[i]);
+        }
+        return (char)((c % 26) + 'a');
+    }
+};
