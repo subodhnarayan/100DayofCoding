331fc28b492c938c8ddee9ebd60c5898ce958c16
ExploreAI351 <your.email@example.com>
2025-07-05 23:35:05 +0530

    Create day585.cpp

---
+++ day585.cpp
@@ -0,0 +1,33 @@
+-- -- -- -- -- -- -- -- -GFG - POTD - 05/ 07 / 2025 ----------------------------------------
+
+Max Score from Subarray Mins
+Difficulty: MediumAccuracy: 43.26%Submissions: 130K+Points: 4
+You are given an array arr[] of integers. Your task is to find the maximum sum of the smallest and second smallest elements across all subarrays (of size >= 2) of the given array.
+
+Examples :
+
+Input: arr[] = [4, 3, 5, 1]
+Output: 8
+Explanation: All subarrays with at least 2 elements and find the two smallest numbers in each:
+[4, 3] → 3 + 4 = 7
+[4, 3, 5] → 3 + 4 = 7
+[4, 3, 5, 1] → 1 + 3 = 4
+[3, 5] → 3 + 5 = 8
+[3, 5, 1] → 1 + 3 = 4
+[5, 1] → 1 + 5 = 6
+Maximum Score is 8.
+Input: arr[] = [1, 2, 3]
+Output: 5
+Explanation: All subarray with at least 2 elements and find the two smallest numbers in each:
+[1, 2] → 1 + 2 = 3
+[1, 2, 3] → 1 + 2 = 3
+[2, 3] → 2 + 3 = 5
+Maximum Score is 5
+Constraints:
+2 ≤ arr.size() ≤ 105
+1 ≤ arr[i] ≤ 106
+
+Expected Complexities
+Time Complexity: O(n)
+Auxiliary Space: O(1)
+-- -- -- -- -- -- -- -- -Leetcode - POTD - 05/ 07 / 2025 ----------------------------------------
