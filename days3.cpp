af6e5f954581fc8864a81cf6f11cc6d7999e0551
ExploreAI351 <your.email@example.com>
2025-07-11 21:56:14 +0530

    Update days3.cpp

--- days3.cpp
+++ days3.cpp
@@ -21,4 +21,23 @@ Constraints:
 Expected Complexities
 Time Complexity: O(n)
 Auxiliary Space: O(1)
+
+
+class Solution {
+  public:
+  int fib(int n){
+      int first=1,second=1;
+      int sum=0;
+      for(int i=3;i<=n;i++){
+          sum=first+second;
+          first=second;
+          second=sum;
+      }
+      return sum;
+  }
+    int countConsec(int n) {
+        // code here
+         return (1<<n) - fib(n+2);
+    }
+};
 --------------------------------Leetcode POTD 11/07/2025---------------------------
