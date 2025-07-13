213a345ac9595d77c2a7bea8621c014cfed269a7
ExploreAI351 <your.email@example.com>
2025-07-13 21:47:07 +0530

    Update days5.cpp

--- days5.cpp
+++ days5.cpp
@@ -1,4 +1,24 @@
 --------------------------------GFG POTD 13/07/2025-------------------------------
+Maximum sum of elements not part of LIS
+Difficulty: MediumAccuracy: 48.67%Submissions: 16K+Points: 4
+Given an array arr[] of positive integers, your task is to find the maximum possible sum of all elements that are not part of the Longest Increasing Subsequence (LIS).
+
+Examples:
+
+Input: arr[] = [4, 6, 1, 2, 3, 8]
+Output: 10
+Explanation: The elements which are not in LIS is 4 and 6.
+Input: arr[] = [5, 4, 3, 2, 1]
+Output: 14
+Explanation: The elements which are not in LIS is 5, 4, 3 and 2.
+Constraints:
+1 ≤ arr.size() ≤ 103
+1 ≤ arr[i] ≤ 105
+
+Expected Complexities
+Time Complexity: O(n log n)
+Auxiliary Space: O(n)
+
 
 
 
